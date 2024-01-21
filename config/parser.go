5918a7bb17e5cf95d1358b9de42323cb63aa8258
care bear <carebera@berachain.com>
2024-01-21 17:07:50 -0500

    cleanups and TODOs

--- config/parser.go
+++ config/parser.go
@@ -39,6 +39,8 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
+// TODO: Move this to a common package for parsing utils.
+
 // baseTen is for the big.Int string conversation.
 const baseTen = 10
 
