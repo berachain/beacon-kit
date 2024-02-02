2c92b024c6c01f23af9cbcf43c5118c0de4f9dc2
itsdevbear <itsdevbear@berachain.com>
2024-02-02 02:28:55 -0500

    bump solidity

--- types/consensus/v1/alias.go
+++ types/consensus/v1/alias.go
@@ -37,7 +37,7 @@ type (
 	// Gwei is from github.com/prysmaticlabs/prysm/v4/consensus-types/primitives.Gwei
 	// We have to do this to keep `sszgen` happy.
 	Gwei = github_com_prysmaticlabs_prysm_v4_math.Gwei
-	// Gwei is from github.com/prysmaticlabs/prysm/v4/consensus-types/primitives.Wei
+	// Wei is from github.com/prysmaticlabs/prysm/v4/consensus-types/primitives.Wei
 	// We have to do this to keep `sszgen` happy.
 	Wei = github_com_prysmaticlabs_prysm_v4_math.Wei
 )
