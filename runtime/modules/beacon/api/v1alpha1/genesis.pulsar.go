db2588388c3fa0c4ca4d4cbaaf69c3a2a429ba12
itsdevbear <itsdevbear@berachain.com>
2024-02-07 01:42:06 -0500

    fix lint

--- runtime/modules/beacon/api/v1alpha1/genesis.pulsar.go
+++ runtime/modules/beacon/api/v1alpha1/genesis.pulsar.go
@@ -18,8 +18,8 @@ var (
 )
 
 func init() {
-	file_x_beacon_v1alpha1_genesis_proto_init()
-	md_GenesisState = File_x_beacon_v1alpha1_genesis_proto.Messages().ByName("GenesisState")
+	file_runtime_modules_beacon_v1alpha1_genesis_proto_init()
+	md_GenesisState = File_runtime_modules_beacon_v1alpha1_genesis_proto.Messages().ByName("GenesisState")
 	fd_GenesisState_eth1_genesis_hash = md_GenesisState.Fields().ByName("eth1_genesis_hash")
 }
 
@@ -32,7 +32,7 @@ func (x *GenesisState) ProtoReflect() protoreflect.Message {
 }
 
 func (x *GenesisState) slowProtoReflect() protoreflect.Message {
-	mi := &file_x_beacon_v1alpha1_genesis_proto_msgTypes[0]
+	mi := &file_runtime_modules_beacon_v1alpha1_genesis_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -109,13 +109,13 @@ func (x *fastReflection_GenesisState) Range(f func(protoreflect.FieldDescriptor,
 // a repeated field is populated if it is non-empty.
 func (x *fastReflection_GenesisState) Has(fd protoreflect.FieldDescriptor) bool {
 	switch fd.FullName() {
-	case "x.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
+	case "runtime.modules.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
 		return x.Eth1GenesisHash != ""
 	default:
 		if fd.IsExtension() {
-			panic(fmt.Errorf("proto3 declared messages do not support extensions: x.beacon.v1alpha1.GenesisState"))
+			panic(fmt.Errorf("proto3 declared messages do not support extensions: runtime.modules.beacon.v1alpha1.GenesisState"))
 		}
-		panic(fmt.Errorf("message x.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
+		panic(fmt.Errorf("message runtime.modules.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
 	}
 }
 
@@ -127,13 +127,13 @@ func (x *fastReflection_GenesisState) Has(fd protoreflect.FieldDescriptor) bool 
 // Clear is a mutating operation and unsafe for concurrent use.
 func (x *fastReflection_GenesisState) Clear(fd protoreflect.FieldDescriptor) {
 	switch fd.FullName() {
-	case "x.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
+	case "runtime.modules.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
 		x.Eth1GenesisHash = ""
 	default:
 		if fd.IsExtension() {
-			panic(fmt.Errorf("proto3 declared messages do not support extensions: x.beacon.v1alpha1.GenesisState"))
+			panic(fmt.Errorf("proto3 declared messages do not support extensions: runtime.modules.beacon.v1alpha1.GenesisState"))
 		}
-		panic(fmt.Errorf("message x.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
+		panic(fmt.Errorf("message runtime.modules.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
 	}
 }
 
@@ -145,14 +145,14 @@ func (x *fastReflection_GenesisState) Clear(fd protoreflect.FieldDescriptor) {
 // of the value; to obtain a mutable reference, use Mutable.
 func (x *fastReflection_GenesisState) Get(descriptor protoreflect.FieldDescriptor) protoreflect.Value {
 	switch descriptor.FullName() {
-	case "x.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
+	case "runtime.modules.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
 		value := x.Eth1GenesisHash
 		return protoreflect.ValueOfString(value)
 	default:
 		if descriptor.IsExtension() {
-			panic(fmt.Errorf("proto3 declared messages do not support extensions: x.beacon.v1alpha1.GenesisState"))
+			panic(fmt.Errorf("proto3 declared messages do not support extensions: runtime.modules.beacon.v1alpha1.GenesisState"))
 		}
-		panic(fmt.Errorf("message x.beacon.v1alpha1.GenesisState does not contain field %s", descriptor.FullName()))
+		panic(fmt.Errorf("message runtime.modules.beacon.v1alpha1.GenesisState does not contain field %s", descriptor.FullName()))
 	}
 }
 
@@ -168,13 +168,13 @@ func (x *fastReflection_GenesisState) Get(descriptor protoreflect.FieldDescripto
 // Set is a mutating operation and unsafe for concurrent use.
 func (x *fastReflection_GenesisState) Set(fd protoreflect.FieldDescriptor, value protoreflect.Value) {
 	switch fd.FullName() {
-	case "x.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
+	case "runtime.modules.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
 		x.Eth1GenesisHash = value.Interface().(string)
 	default:
 		if fd.IsExtension() {
-			panic(fmt.Errorf("proto3 declared messages do not support extensions: x.beacon.v1alpha1.GenesisState"))
+			panic(fmt.Errorf("proto3 declared messages do not support extensions: runtime.modules.beacon.v1alpha1.GenesisState"))
 		}
-		panic(fmt.Errorf("message x.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
+		panic(fmt.Errorf("message runtime.modules.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
 	}
 }
 
@@ -190,13 +190,13 @@ func (x *fastReflection_GenesisState) Set(fd protoreflect.FieldDescriptor, value
 // Mutable is a mutating operation and unsafe for concurrent use.
 func (x *fastReflection_GenesisState) Mutable(fd protoreflect.FieldDescriptor) protoreflect.Value {
 	switch fd.FullName() {
-	case "x.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
-		panic(fmt.Errorf("field eth1_genesis_hash of message x.beacon.v1alpha1.GenesisState is not mutable"))
+	case "runtime.modules.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
+		panic(fmt.Errorf("field eth1_genesis_hash of message runtime.modules.beacon.v1alpha1.GenesisState is not mutable"))
 	default:
 		if fd.IsExtension() {
-			panic(fmt.Errorf("proto3 declared messages do not support extensions: x.beacon.v1alpha1.GenesisState"))
+			panic(fmt.Errorf("proto3 declared messages do not support extensions: runtime.modules.beacon.v1alpha1.GenesisState"))
 		}
-		panic(fmt.Errorf("message x.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
+		panic(fmt.Errorf("message runtime.modules.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
 	}
 }
 
@@ -205,13 +205,13 @@ func (x *fastReflection_GenesisState) Mutable(fd protoreflect.FieldDescriptor) p
 // For lists, maps, and messages, this returns a new, empty, mutable value.
 func (x *fastReflection_GenesisState) NewField(fd protoreflect.FieldDescriptor) protoreflect.Value {
 	switch fd.FullName() {
-	case "x.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
+	case "runtime.modules.beacon.v1alpha1.GenesisState.eth1_genesis_hash":
 		return protoreflect.ValueOfString("")
 	default:
 		if fd.IsExtension() {
-			panic(fmt.Errorf("proto3 declared messages do not support extensions: x.beacon.v1alpha1.GenesisState"))
+			panic(fmt.Errorf("proto3 declared messages do not support extensions: runtime.modules.beacon.v1alpha1.GenesisState"))
 		}
-		panic(fmt.Errorf("message x.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
+		panic(fmt.Errorf("message runtime.modules.beacon.v1alpha1.GenesisState does not contain field %s", fd.FullName()))
 	}
 }
 
@@ -221,7 +221,7 @@ func (x *fastReflection_GenesisState) NewField(fd protoreflect.FieldDescriptor) 
 func (x *fastReflection_GenesisState) WhichOneof(d protoreflect.OneofDescriptor) protoreflect.FieldDescriptor {
 	switch d.FullName() {
 	default:
-		panic(fmt.Errorf("%s is not a oneof field in x.beacon.v1alpha1.GenesisState", d.FullName()))
+		panic(fmt.Errorf("%s is not a oneof field in runtime.modules.beacon.v1alpha1.GenesisState", d.FullName()))
 	}
 	panic("unreachable")
 }
@@ -461,7 +461,7 @@ func (x *fastReflection_GenesisState) ProtoMethods() *protoiface.Methods {
 // versions:
 // 	protoc-gen-go v1.27.0
 // 	protoc        (unknown)
-// source: x/beacon/v1alpha1/genesis.proto
+// source: runtime/modules/beacon/v1alpha1/genesis.proto
 
 const (
 	// Verify that this generated code is sufficiently up-to-date.
@@ -484,7 +484,7 @@ type GenesisState struct {
 func (x *GenesisState) Reset() {
 	*x = GenesisState{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_x_beacon_v1alpha1_genesis_proto_msgTypes[0]
+		mi := &file_runtime_modules_beacon_v1alpha1_genesis_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -498,7 +498,7 @@ func (*GenesisState) ProtoMessage() {}
 
 // Deprecated: Use GenesisState.ProtoReflect.Descriptor instead.
 func (*GenesisState) Descriptor() ([]byte, []int) {
-	return file_x_beacon_v1alpha1_genesis_proto_rawDescGZIP(), []int{0}
+	return file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *GenesisState) GetEth1GenesisHash() string {
@@ -508,48 +508,55 @@ func (x *GenesisState) GetEth1GenesisHash() string {
 	return ""
 }
 
-var File_x_beacon_v1alpha1_genesis_proto protoreflect.FileDescriptor
+var File_runtime_modules_beacon_v1alpha1_genesis_proto protoreflect.FileDescriptor
 
-var file_x_beacon_v1alpha1_genesis_proto_rawDesc = []byte{
-	0x0a, 0x1f, 0x78, 0x2f, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2f, 0x76, 0x31, 0x61, 0x6c, 0x70,
-	0x68, 0x61, 0x31, 0x2f, 0x67, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x12, 0x11, 0x78, 0x2e, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2e, 0x76, 0x31, 0x61, 0x6c,
-	0x70, 0x68, 0x61, 0x31, 0x22, 0x3a, 0x0a, 0x0c, 0x47, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73, 0x53,
-	0x74, 0x61, 0x74, 0x65, 0x12, 0x2a, 0x0a, 0x11, 0x65, 0x74, 0x68, 0x31, 0x5f, 0x67, 0x65, 0x6e,
-	0x65, 0x73, 0x69, 0x73, 0x5f, 0x68, 0x61, 0x73, 0x68, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x0f, 0x65, 0x74, 0x68, 0x31, 0x47, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73, 0x48, 0x61, 0x73, 0x68,
-	0x42, 0xbe, 0x01, 0x0a, 0x15, 0x63, 0x6f, 0x6d, 0x2e, 0x78, 0x2e, 0x62, 0x65, 0x61, 0x63, 0x6f,
-	0x6e, 0x2e, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x42, 0x0c, 0x47, 0x65, 0x6e, 0x65,
-	0x73, 0x69, 0x73, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x50, 0x01, 0x5a, 0x31, 0x63, 0x6f, 0x73, 0x6d,
-	0x6f, 0x73, 0x73, 0x64, 0x6b, 0x2e, 0x69, 0x6f, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x78, 0x2f, 0x62,
-	0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2f, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x3b, 0x62,
-	0x65, 0x61, 0x63, 0x6f, 0x6e, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0xa2, 0x02, 0x03,
-	0x58, 0x42, 0x58, 0xaa, 0x02, 0x11, 0x58, 0x2e, 0x42, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2e, 0x56,
-	0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0xca, 0x02, 0x11, 0x58, 0x5c, 0x42, 0x65, 0x61, 0x63,
-	0x6f, 0x6e, 0x5c, 0x56, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0xe2, 0x02, 0x1d, 0x58, 0x5c,
-	0x42, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x5c, 0x56, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x5c,
-	0x47, 0x50, 0x42, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0xea, 0x02, 0x13, 0x58, 0x3a,
-	0x3a, 0x42, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x3a, 0x3a, 0x56, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61,
-	0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDesc = []byte{
+	0x0a, 0x2d, 0x72, 0x75, 0x6e, 0x74, 0x69, 0x6d, 0x65, 0x2f, 0x6d, 0x6f, 0x64, 0x75, 0x6c, 0x65,
+	0x73, 0x2f, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2f, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61,
+	0x31, 0x2f, 0x67, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
+	0x1f, 0x72, 0x75, 0x6e, 0x74, 0x69, 0x6d, 0x65, 0x2e, 0x6d, 0x6f, 0x64, 0x75, 0x6c, 0x65, 0x73,
+	0x2e, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2e, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31,
+	0x22, 0x3a, 0x0a, 0x0c, 0x47, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73, 0x53, 0x74, 0x61, 0x74, 0x65,
+	0x12, 0x2a, 0x0a, 0x11, 0x65, 0x74, 0x68, 0x31, 0x5f, 0x67, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73,
+	0x5f, 0x68, 0x61, 0x73, 0x68, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0f, 0x65, 0x74, 0x68,
+	0x31, 0x47, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73, 0x48, 0x61, 0x73, 0x68, 0x42, 0x93, 0x02, 0x0a,
+	0x23, 0x63, 0x6f, 0x6d, 0x2e, 0x72, 0x75, 0x6e, 0x74, 0x69, 0x6d, 0x65, 0x2e, 0x6d, 0x6f, 0x64,
+	0x75, 0x6c, 0x65, 0x73, 0x2e, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2e, 0x76, 0x31, 0x61, 0x6c,
+	0x70, 0x68, 0x61, 0x31, 0x42, 0x0c, 0x47, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73, 0x50, 0x72, 0x6f,
+	0x74, 0x6f, 0x50, 0x01, 0x5a, 0x3f, 0x63, 0x6f, 0x73, 0x6d, 0x6f, 0x73, 0x73, 0x64, 0x6b, 0x2e,
+	0x69, 0x6f, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x72, 0x75, 0x6e, 0x74, 0x69, 0x6d, 0x65, 0x2f, 0x6d,
+	0x6f, 0x64, 0x75, 0x6c, 0x65, 0x73, 0x2f, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2f, 0x76, 0x31,
+	0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x3b, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x76, 0x31, 0x61,
+	0x6c, 0x70, 0x68, 0x61, 0x31, 0xa2, 0x02, 0x03, 0x52, 0x4d, 0x42, 0xaa, 0x02, 0x1f, 0x52, 0x75,
+	0x6e, 0x74, 0x69, 0x6d, 0x65, 0x2e, 0x4d, 0x6f, 0x64, 0x75, 0x6c, 0x65, 0x73, 0x2e, 0x42, 0x65,
+	0x61, 0x63, 0x6f, 0x6e, 0x2e, 0x56, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0xca, 0x02, 0x1f,
+	0x52, 0x75, 0x6e, 0x74, 0x69, 0x6d, 0x65, 0x5c, 0x4d, 0x6f, 0x64, 0x75, 0x6c, 0x65, 0x73, 0x5c,
+	0x42, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x5c, 0x56, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0xe2,
+	0x02, 0x2b, 0x52, 0x75, 0x6e, 0x74, 0x69, 0x6d, 0x65, 0x5c, 0x4d, 0x6f, 0x64, 0x75, 0x6c, 0x65,
+	0x73, 0x5c, 0x42, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x5c, 0x56, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61,
+	0x31, 0x5c, 0x47, 0x50, 0x42, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0xea, 0x02, 0x22,
+	0x52, 0x75, 0x6e, 0x74, 0x69, 0x6d, 0x65, 0x3a, 0x3a, 0x4d, 0x6f, 0x64, 0x75, 0x6c, 0x65, 0x73,
+	0x3a, 0x3a, 0x42, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x3a, 0x3a, 0x56, 0x31, 0x61, 0x6c, 0x70, 0x68,
+	0x61, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_x_beacon_v1alpha1_genesis_proto_rawDescOnce sync.Once
-	file_x_beacon_v1alpha1_genesis_proto_rawDescData = file_x_beacon_v1alpha1_genesis_proto_rawDesc
+	file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDescOnce sync.Once
+	file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDescData = file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDesc
 )
 
-func file_x_beacon_v1alpha1_genesis_proto_rawDescGZIP() []byte {
-	file_x_beacon_v1alpha1_genesis_proto_rawDescOnce.Do(func() {
-		file_x_beacon_v1alpha1_genesis_proto_rawDescData = protoimpl.X.CompressGZIP(file_x_beacon_v1alpha1_genesis_proto_rawDescData)
+func file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDescGZIP() []byte {
+	file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDescOnce.Do(func() {
+		file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDescData = protoimpl.X.CompressGZIP(file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDescData)
 	})
-	return file_x_beacon_v1alpha1_genesis_proto_rawDescData
+	return file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDescData
 }
 
-var file_x_beacon_v1alpha1_genesis_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_x_beacon_v1alpha1_genesis_proto_goTypes = []interface{}{
-	(*GenesisState)(nil), // 0: x.beacon.v1alpha1.GenesisState
+var file_runtime_modules_beacon_v1alpha1_genesis_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_runtime_modules_beacon_v1alpha1_genesis_proto_goTypes = []interface{}{
+	(*GenesisState)(nil), // 0: runtime.modules.beacon.v1alpha1.GenesisState
 }
-var file_x_beacon_v1alpha1_genesis_proto_depIdxs = []int32{
+var file_runtime_modules_beacon_v1alpha1_genesis_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -557,13 +564,13 @@ var file_x_beacon_v1alpha1_genesis_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_x_beacon_v1alpha1_genesis_proto_init() }
-func file_x_beacon_v1alpha1_genesis_proto_init() {
-	if File_x_beacon_v1alpha1_genesis_proto != nil {
+func init() { file_runtime_modules_beacon_v1alpha1_genesis_proto_init() }
+func file_runtime_modules_beacon_v1alpha1_genesis_proto_init() {
+	if File_runtime_modules_beacon_v1alpha1_genesis_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_x_beacon_v1alpha1_genesis_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_runtime_modules_beacon_v1alpha1_genesis_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*GenesisState); i {
 			case 0:
 				return &v.state
@@ -580,18 +587,18 @@ func file_x_beacon_v1alpha1_genesis_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_x_beacon_v1alpha1_genesis_proto_rawDesc,
+			RawDescriptor: file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_x_beacon_v1alpha1_genesis_proto_goTypes,
-		DependencyIndexes: file_x_beacon_v1alpha1_genesis_proto_depIdxs,
-		MessageInfos:      file_x_beacon_v1alpha1_genesis_proto_msgTypes,
+		GoTypes:           file_runtime_modules_beacon_v1alpha1_genesis_proto_goTypes,
+		DependencyIndexes: file_runtime_modules_beacon_v1alpha1_genesis_proto_depIdxs,
+		MessageInfos:      file_runtime_modules_beacon_v1alpha1_genesis_proto_msgTypes,
 	}.Build()
-	File_x_beacon_v1alpha1_genesis_proto = out.File
-	file_x_beacon_v1alpha1_genesis_proto_rawDesc = nil
-	file_x_beacon_v1alpha1_genesis_proto_goTypes = nil
-	file_x_beacon_v1alpha1_genesis_proto_depIdxs = nil
+	File_runtime_modules_beacon_v1alpha1_genesis_proto = out.File
+	file_runtime_modules_beacon_v1alpha1_genesis_proto_rawDesc = nil
+	file_runtime_modules_beacon_v1alpha1_genesis_proto_goTypes = nil
+	file_runtime_modules_beacon_v1alpha1_genesis_proto_depIdxs = nil
 }
