afee14a957929d3f625b17639bbc2ba33ab8257a
rustybera <157746232+rustybera@users.noreply.github.com>
2024-02-08 23:18:32 -0500

    feat(brc): Fix storage and selector collision, search timestamp from block number (#30)
    * Fix storage and selector collision, search timestamp from block number
    
    * Test and fix `BeaconRootsContract`
    
    Added a setStorage function to the BeaconRootsContractTest for initializing the storage for a series of tests. These tests ensure that: timestamps, beacon roots, and coinbases are stored correctly in the circular buffers. Changes also comply with EIP-4788 i.e., depending on the caller and its calldata, the contract follows certain execution paths. Amendments have been done in the setBeaconRoot, get and binarySearch functions in the BeaconRootsContract ensuring the storage and retrieval of beacon block root and coinbase are correctly managed with direct storage access.
    
    * Add tests for out of range timestamp, invalid timestamp and partially initialized buffer
    
    * Refactor `BeaconRootsContract` for more efficient storage
    
    This commit combines the `setBeaconRoot()` and `setCoinbase()` functions into one function named `set()`. This change optimizes direct storage access by using assembly language. Additionally, redundant code comments have been removed for simplicity and clarity.
    
    * Address review comments and make tests more comprehensive
    
    This commit refactors the BeaconRootsContract by implementing circular buffers for better gas efficiency. It adds extra checks to ensure correct storage and retrieval of timestamps, beacon roots, and coinbases. Also, it includes additional fuzzing tests for the circular buffers and the functionality of partially initialized buffers. Moreover, new gas snapshots are included to observe the impact on gas consumption.
    
    ---------
    
    Co-authored-by: Devon Bear <itsdevbear@berachain.com>

--- contracts/src/eip4788/BeaconRootsContract.sol
+++ contracts/src/eip4788/BeaconRootsContract.sol
@@ -16,56 +16,43 @@ contract BeaconRootsContract {
 
     // HISTORY_BUFFER_LENGTH is the length of the circular buffer for
     // storing beacon roots and coinbases.
-    uint256 constant HISTORY_BUFFER_LENGTH = 256;
+    uint256 private constant HISTORY_BUFFER_LENGTH = 256;
+    uint256 private constant BEACON_ROOT_OFFSET = HISTORY_BUFFER_LENGTH;
+    uint256 private constant COINBASE_OFFSET = BEACON_ROOT_OFFSET + HISTORY_BUFFER_LENGTH;
 
     // SYSTEM_ADDRESS is the address that is allowed to call the set function
     // as defined in EIP-4788: https://eips.ethereum.org/EIPS/eip-4788
-    address constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
+    address private constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
+
+    /// @dev The selector for "getCoinbase(uint256)".
+    bytes4 private constant GET_COINBASE_SELECTOR = 0xe8e284b9;
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                        ENTRYPOINT                          */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
+    /// @notice Conforming to EIP-4788, this contract follows two execution paths:
+    /// 1. If it is called by the SYSTEM_ADDRESS, the calldata is the 32-byte encoded beacon block root.
+    /// 2. If it is called by any other address, there are two possible scenarios:
+    ///    a. If the calldata is the 32-byte encoded timestamp, the function will return the beacon block root.
+    ///    b. If the calldata is the 4-bytes selector for "getCoinbase(uint256)" appended with the 32-byte encoded
+    ///       block number, the function will return the coinbase for the given block number.
     fallback() external {
-        if (msg.sender == SYSTEM_ADDRESS) {
-            // Call set function
-            set();
+        if (msg.sender != SYSTEM_ADDRESS) {
+            if (msg.data.length == 36 && bytes4(msg.data) == GET_COINBASE_SELECTOR) {
+                getCoinbase(uint256(bytes32(msg.data[4:36])));
+            } else {
+                // if the first 32 bytes is a timestamp, the first 4 bytes must be 0
+                get();
+            }
         } else {
-            // Call get function
-            get();
+            set();
         }
-    }
-
-    // From: https://eips.ethereum.org/EIPS/eip-4788
-    //
-    // def set():
-    //     timestamp_idx = to_uint256_be(evm.timestamp) % HISTORY_BUFFER_LENGTH
-    //     root_idx = timestamp_idx + HISTORY_BUFFER_LENGTH
-
-    //     storage.set(timestamp_idx, evm.timestamp)
-    //     storage.set(root_idx, evm.calldata)
-    //
-    /// @dev Sets the beacon root and coinbase for the current block.
-    /// This function is called internally and utilizes assembly for direct storage access.
-    function set() internal {
-        setBeaconRoot();
-        setCoinbase();
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                       BEACON ROOT                          */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-
-    /// @dev Sets the beacon root and coinbase for the current block.
-    /// This function is called internally and utilizes assembly for direct storage access.
-    function setBeaconRoot() internal {
-        assembly {
-            let timestamp_idx := mod(timestamp(), HISTORY_BUFFER_LENGTH)
-            let root_idx := add(timestamp_idx, HISTORY_BUFFER_LENGTH)
-            sstore(timestamp_idx, timestamp())
-            sstore(root_idx, calldataload(0))
-        }
-    }
 
     // From: https://eips.ethereum.org/EIPS/eip-4788
     //
@@ -89,18 +76,46 @@ contract BeaconRootsContract {
     //
     /// @dev Retrieves the beacon root for a given timestamp.
     /// This function is called internally and utilizes assembly for direct storage access.
+    /// Reverts if the calldata is not a 32-byte timestamp or if the timestamp is 0.
+    /// Reverts if the timestamp is not within the circular buffer.
     /// @return The beacon root associated with the given timestamp.
     function get() internal view returns (bytes32) {
-        assembly {
-            if iszero(eq(calldatasize(), 32)) { revert(0, 0) }
-            if iszero(calldataload(0)) { revert(0, 0) }
-            let timestamp_idx := mod(calldataload(0), HISTORY_BUFFER_LENGTH)
-            let _timestamp := sload(timestamp_idx)
+        assembly ("memory-safe") {
+            if iszero(and(eq(calldatasize(), 0x20), gt(calldataload(0), 0))) { revert(0, 0) }
+        }
+        uint256 block_idx = binarySearch();
+        assembly ("memory-safe") {
+            let _timestamp := sload(block_idx)
             if iszero(eq(_timestamp, calldataload(0))) { revert(0, 0) }
-            let root_idx := add(timestamp_idx, HISTORY_BUFFER_LENGTH)
-            let root := sload(root_idx)
-            mstore(0, root)
-            return(0, 32)
+            let root_idx := add(block_idx, BEACON_ROOT_OFFSET)
+            mstore(0, sload(root_idx))
+            return(0, 0x20)
+        }
+    }
+
+    // From: https://eips.ethereum.org/EIPS/eip-4788
+    //
+    // def set():
+    //     timestamp_idx = to_uint256_be(evm.timestamp) % HISTORY_BUFFER_LENGTH
+    //     root_idx = timestamp_idx + HISTORY_BUFFER_LENGTH
+
+    //     storage.set(timestamp_idx, evm.timestamp)
+    //     storage.set(root_idx, evm.calldata)
+    //
+    /// @dev Sets the beacon root and coinbase for the current block.
+    /// This function is called internally and utilizes assembly for direct storage access.
+    function set() internal {
+        uint256 _COINBASE_OFFSET = COINBASE_OFFSET;
+        assembly ("memory-safe") {
+            let block_idx := mod(number(), HISTORY_BUFFER_LENGTH)
+            // set the timestamp
+            sstore(block_idx, timestamp())
+            let root_idx := add(block_idx, BEACON_ROOT_OFFSET)
+            // set the beacon root
+            sstore(root_idx, calldataload(0))
+            let coinbase_idx := add(block_idx, _COINBASE_OFFSET)
+            // set the coinbase
+            sstore(coinbase_idx, coinbase())
         }
     }
 
@@ -108,24 +123,44 @@ contract BeaconRootsContract {
     /*                         COINBASE                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /// @dev Sets the coinbase for the current block in storage.
-    /// This function is called internally and utilizes assembly for direct storage access.
-    function setCoinbase() internal {
-        assembly {
-            sstore(mod(number(), HISTORY_BUFFER_LENGTH), coinbase())
-        }
-    }
-
-    /// @dev Retrieves the coinbase for a given block number.
+    /// @notice Retrieves the coinbase for a given block number.
     /// @dev if called with a block number that is before the history buffer
     /// it will return the coinbase for blockNumber + HISTORY_BUFFER_LENGTH * A
     /// Where A is the number of times the buffer has cycled since the blockNumber
     /// @param blockNumber The block number for which to retrieve the coinbase.
     /// @return The coinbase for the given block number.
-    function getCoinbase(uint256 blockNumber) external view returns (address) {
-        assembly {
-            mstore(0, sload(mod(blockNumber, HISTORY_BUFFER_LENGTH))) // fix collision
-            return(0, 0x20) // or 32
+    function getCoinbase(uint256 blockNumber) internal view returns (address) {
+        uint256 _COINBASE_OFFSET = COINBASE_OFFSET;
+        assembly ("memory-safe") {
+            let block_idx := mod(blockNumber, HISTORY_BUFFER_LENGTH)
+            let coinbase_idx := add(block_idx, _COINBASE_OFFSET)
+            mstore(0, sload(coinbase_idx))
+            return(0, 0x20)
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                 TIMESTAMP TO BLOCK NUMBER                  */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Retrieves the block index for a given timestamp using binary search on the circular buffer.
+    /// @dev Precondition: Any two consecutive timestamps in the circular buffer are strictly increasing.
+    function binarySearch() internal view returns (uint256 block_idx) {
+        assembly ("memory-safe") {
+            let high := mod(number(), HISTORY_BUFFER_LENGTH)
+            let low := mod(add(number(), 1), HISTORY_BUFFER_LENGTH)
+            // revert if the timestamp is not within the circular buffer
+            if or(lt(calldataload(0), sload(low)), gt(calldataload(0), sload(high))) { revert(0, 0) }
+            for {} 1 {} {
+                let high_adjusted := add(high, mul(lt(high, low), HISTORY_BUFFER_LENGTH))
+                block_idx := mod(shr(1, add(low, high_adjusted)), HISTORY_BUFFER_LENGTH)
+                if eq(low, high) { break }
+                if lt(sload(block_idx), calldataload(0)) {
+                    low := mod(add(block_idx, 1), HISTORY_BUFFER_LENGTH)
+                    continue
+                }
+                high := block_idx
+            }
         }
     }
 }
--- contracts/src/eip4788/BeaconRootsContract.t.sol
+++ contracts/src/eip4788/BeaconRootsContract.t.sol
@@ -2,15 +2,177 @@
 pragma solidity ^0.8.24;
 
 import "../../lib/solady/test/utils/SoladyTest.sol";
+import "../../lib/solady/src/utils/FixedPointMathLib.sol";
 import "../eip4788/BeaconRootsContract.sol";
 
 /// @title BeaconRootsContractTest
 /// @dev This contract is used for testing the BeaconRootsContract.
 contract BeaconRootsContractTest is SoladyTest {
-    BeaconRootsContract beaconRootsContract;
+    uint256 private constant HISTORY_BUFFER_LENGTH = 256;
+    uint256 private constant BEACON_ROOT_OFFSET = HISTORY_BUFFER_LENGTH;
+    uint256 private constant COINBASE_OFFSET = BEACON_ROOT_OFFSET + HISTORY_BUFFER_LENGTH;
+    address private constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
+    bytes4 private constant GET_COINBASE_SELECTOR = 0xe8e284b9;
+    uint256 private constant BLOCK_INTERVAL = 5;
+    uint256 private constant TIMESTAMP = 1707425462;
+    uint256[HISTORY_BUFFER_LENGTH] private _timestamps;
+
+    BeaconRootsContract internal beaconRootsContract;
+    bytes32 private beaconRoot;
 
     /// @dev Set up the test environment by deploying a new BeaconRootsContract.
     function setUp() public {
         beaconRootsContract = new BeaconRootsContract();
+        setStorage(0, TIMESTAMP, HISTORY_BUFFER_LENGTH);
+    }
+
+    /// @dev Set the storage of the BeaconRootsContract by calling from the SYSTEM_ADDRESS.
+    function setStorage(uint256 startBlock, uint256 startTimestamp, uint256 length)
+        internal
+        returns (
+            uint256[] memory blockNumbers,
+            uint256[] memory timestamps,
+            bytes32[] memory beaconRoots,
+            address[] memory coinbases
+        )
+    {
+        blockNumbers = new uint256[](length);
+        timestamps = new uint256[](length);
+        beaconRoots = new bytes32[](length);
+        coinbases = new address[](length);
+        vm.startPrank(SYSTEM_ADDRESS);
+        for (uint256 i; i < length; ++i) {
+            blockNumbers[i] = startBlock + i;
+            timestamps[i] = startTimestamp + i * BLOCK_INTERVAL
+            // a random number between 1 and BLOCK_INTERVAL such that the timestamp is ever increasing
+            + FixedPointMathLib.min(1, FixedPointMathLib.fullMulDiv(_random(), BLOCK_INTERVAL, type(uint256).max));
+            _timestamps[i % HISTORY_BUFFER_LENGTH] = timestamps[i];
+            beaconRoots[i] = bytes32(_random());
+            coinbases[i] = _randomNonZeroAddress();
+            vm.roll(blockNumbers[i]);
+            vm.warp(timestamps[i]);
+            vm.coinbase(coinbases[i]);
+            (bool success,) = address(beaconRootsContract).call(abi.encode(beaconRoots[i]));
+            assertTrue(success, "setStorage: set failed");
+        }
+        beaconRoot = beaconRoots[length - 1];
+        vm.stopPrank();
+    }
+
+    function callGet(uint256 timestamp) internal returns (bool success, bytes memory data) {
+        (success, data) = address(beaconRootsContract).call(abi.encode(timestamp));
+    }
+
+    function validateBeaconRoots(uint256[] memory timestamps, bytes32[] memory beaconRoots) internal {
+        // loop over the last `HISTORY_BUFFER_LENGTH` indices
+        uint256 i = timestamps.length - 1;
+        for (uint256 j; j < HISTORY_BUFFER_LENGTH; ++j) {
+            (bool success, bytes memory data) = callGet(timestamps[i]);
+            assertTrue(success, "get: failed");
+            assertEq(data.length, 32, "get: invalid length");
+            assertEq(bytes32(data), beaconRoots[i], "get: invalid beacon root");
+            if (i == 0) {
+                break;
+            }
+            --i;
+        }
+    }
+
+    /// @dev Test the timestamps, beacon roots, and coinbases are stored correctly in the circular buffers.
+    function test_Set() public {
+        testFuzz_Set(0, 0, HISTORY_BUFFER_LENGTH);
+    }
+
+    /// @dev Fuzzing test the timestamps, beacon roots, and coinbases are stored correctly in the circular buffers.
+    function testFuzz_Set(uint64 startBlock, uint32 startTimestamp, uint256 length) public {
+        // may wrap around the circular buffer
+        length = _bound(length, 1, HISTORY_BUFFER_LENGTH * 4);
+        (, uint256[] memory timestamps, bytes32[] memory beaconRoots, address[] memory coinbases) =
+            setStorage(startBlock, startTimestamp, length);
+        // loop over the last `HISTORY_BUFFER_LENGTH` indices
+        uint256 i = length - 1;
+        for (uint256 j; j < HISTORY_BUFFER_LENGTH; ++j) {
+            uint256 blockIdx = (startBlock + i) % HISTORY_BUFFER_LENGTH;
+            bytes32 data = vm.load(address(beaconRootsContract), bytes32(blockIdx));
+            assertEq(uint256(data), timestamps[i], "set: invalid timestamp");
+            data = vm.load(address(beaconRootsContract), bytes32(blockIdx + BEACON_ROOT_OFFSET));
+            assertEq(data, beaconRoots[i], "set: invalid beacon root");
+            data = vm.load(address(beaconRootsContract), bytes32(blockIdx + COINBASE_OFFSET));
+            assertEq(uint256(data), uint160(coinbases[i]), "set: invalid coinbase");
+            if (i == 0) {
+                break;
+            }
+            --i;
+        }
+    }
+
+    /// @dev Test the beacon root is retrieved correctly from the circular buffer.
+    function test_Get() public {
+        (bool success, bytes memory data) = callGet(block.timestamp);
+        assertTrue(success, "get: failed");
+        assertEq(data.length, 32, "get: invalid length");
+        assertEq(bytes32(data), beaconRoot, "get: invalid beacon root");
+    }
+
+    function test_InvalidCalldataLength() public {
+        bytes memory data = abi.encode(block.timestamp);
+        (bool success,) = address(beaconRootsContract).call(bytes.concat(data, data));
+        assertFalse(success, "get: found invalid calldata length");
+    }
+
+    function test_GetOutOfRangeTimestamp() public {
+        (bool success,) = callGet(TIMESTAMP - 1);
+        assertFalse(success, "get: found out of range timestamp");
+    }
+
+    function test_GetInvalidTimestamp(uint256 timestamp) public {
+        timestamp = _bound(timestamp, _timestamps[0] + 1, _timestamps[HISTORY_BUFFER_LENGTH - 1] - 1);
+        for (uint256 i; i < HISTORY_BUFFER_LENGTH; ++i) {
+            vm.assume(timestamp != _timestamps[i]);
+        }
+        (bool success,) = callGet(timestamp);
+        assertFalse(success, "get: found invalid timestamp");
+    }
+
+    /// @dev Fuzzing test the beacon root is retrieved correctly from the circular buffer.
+    function testFuzz_Get(uint64 startBlock, uint32 startTimestamp, uint256 length) public {
+        vm.assume(startTimestamp > 0);
+        // may wrap around the circular buffer
+        length = _bound(length, 1, HISTORY_BUFFER_LENGTH * 4);
+        beaconRootsContract = new BeaconRootsContract();
+        (, uint256[] memory timestamps, bytes32[] memory beaconRoots,) = setStorage(startBlock, startTimestamp, length);
+        // The timestamp encoded in the calldata may be in the past.
+        // But the block number and timestamp in the EVM must be the latest.
+        validateBeaconRoots(timestamps, beaconRoots);
+    }
+
+    /// @dev Fuzzing test the coinbase is retrieved correctly from the circular buffer.
+    function testFuzz_GetCoinbase(uint64 startBlock, uint32 startTimestamp, uint256 length) public {
+        vm.assume(startTimestamp > 0);
+        // may wrap around the circular buffer
+        length = _bound(length, 1, HISTORY_BUFFER_LENGTH * 4);
+        beaconRootsContract = new BeaconRootsContract();
+        (uint256[] memory blockNumbers,,, address[] memory coinbases) = setStorage(startBlock, startTimestamp, length);
+        // loop over the last `HISTORY_BUFFER_LENGTH` indices
+        uint256 i = length - 1;
+        for (uint256 j; j < HISTORY_BUFFER_LENGTH; ++j) {
+            (bool success, bytes memory data) =
+                address(beaconRootsContract).call(abi.encodeWithSelector(GET_COINBASE_SELECTOR, blockNumbers[i]));
+            assertTrue(success, "getCoinbase: failed");
+            assertEq(uint256(bytes32(data)), uint160(coinbases[i]), "get: invalid coinbase");
+            if (i == 0) {
+                break;
+            }
+            --i;
+        }
+    }
+
+    /// @dev Fuzzing test the beacon root is retrieved correctly from a partially initialized buffer.
+    function testFuzz_PartiallyInitializedBuffer(uint256 length) public {
+        beaconRootsContract = new BeaconRootsContract();
+        length = _bound(length, 1, HISTORY_BUFFER_LENGTH - 1);
+        // The block number starts from 1.
+        (, uint256[] memory timestamps, bytes32[] memory beaconRoots,) = setStorage(1, TIMESTAMP, length);
+        validateBeaconRoots(timestamps, beaconRoots);
     }
 }
