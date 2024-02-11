6b77bf69760014a48d479656358101fe28642935
itsdevbear <itsdevbear@berachain.com>
2024-02-11 16:16:27 -0500

    moretypes

--- types/engine/v2/execution_payload.pb.go
+++ types/engine/v2/execution_payload.pb.go
@@ -222,6 +222,64 @@ func (x *ExecutionPayloadCapella) GetWithdrawals() []*v1.Withdrawal {
 	return nil
 }
 
+// ExecutionPayloadCapellaWithValue wraps the ExecutionPayloadCapella with an additional value field.
+type ExecutionPayloadCapellaWithValue struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// payload is the ExecutionPayloadCapella message.
+	Payload *ExecutionPayloadCapella `protobuf:"bytes,1,opt,name=payload,proto3" json:"payload,omitempty"`
+	// value is an additional field to store arbitrary bytes.
+	Value []byte `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"`
+}
+
+func (x *ExecutionPayloadCapellaWithValue) Reset() {
+	*x = ExecutionPayloadCapellaWithValue{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_types_engine_v2_execution_payload_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ExecutionPayloadCapellaWithValue) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ExecutionPayloadCapellaWithValue) ProtoMessage() {}
+
+func (x *ExecutionPayloadCapellaWithValue) ProtoReflect() protoreflect.Message {
+	mi := &file_types_engine_v2_execution_payload_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ExecutionPayloadCapellaWithValue.ProtoReflect.Descriptor instead.
+func (*ExecutionPayloadCapellaWithValue) Descriptor() ([]byte, []int) {
+	return file_types_engine_v2_execution_payload_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *ExecutionPayloadCapellaWithValue) GetPayload() *ExecutionPayloadCapella {
+	if x != nil {
+		return x.Payload
+	}
+	return nil
+}
+
+func (x *ExecutionPayloadCapellaWithValue) GetValue() []byte {
+	if x != nil {
+		return x.Value
+	}
+	return nil
+}
+
 var File_types_engine_v2_execution_payload_proto protoreflect.FileDescriptor
 
 var file_types_engine_v2_execution_payload_proto_rawDesc = []byte{
@@ -276,11 +334,19 @@ var file_types_engine_v2_execution_payload_proto_rawDesc = []byte{
 	0x79, 0x70, 0x65, 0x73, 0x2e, 0x65, 0x6e, 0x67, 0x69, 0x6e, 0x65, 0x2e, 0x76, 0x31, 0x2e, 0x57,
 	0x69, 0x74, 0x68, 0x64, 0x72, 0x61, 0x77, 0x61, 0x6c, 0x42, 0x13, 0x92, 0xb5, 0x18, 0x0f, 0x77,
 	0x69, 0x74, 0x68, 0x64, 0x72, 0x61, 0x77, 0x61, 0x6c, 0x2e, 0x73, 0x69, 0x7a, 0x65, 0x52, 0x0b,
-	0x77, 0x69, 0x74, 0x68, 0x64, 0x72, 0x61, 0x77, 0x61, 0x6c, 0x73, 0x42, 0x2f, 0x5a, 0x2d, 0x67,
-	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x69, 0x74, 0x73, 0x64, 0x65, 0x76,
-	0x62, 0x65, 0x61, 0x72, 0x2f, 0x62, 0x6f, 0x6c, 0x61, 0x72, 0x69, 0x73, 0x2f, 0x74, 0x79, 0x70,
-	0x65, 0x73, 0x2f, 0x65, 0x6e, 0x67, 0x69, 0x6e, 0x65, 0x2f, 0x76, 0x32, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x77, 0x69, 0x74, 0x68, 0x64, 0x72, 0x61, 0x77, 0x61, 0x6c, 0x73, 0x22, 0x7c, 0x0a, 0x20, 0x45,
+	0x78, 0x65, 0x63, 0x75, 0x74, 0x69, 0x6f, 0x6e, 0x50, 0x61, 0x79, 0x6c, 0x6f, 0x61, 0x64, 0x43,
+	0x61, 0x70, 0x65, 0x6c, 0x6c, 0x61, 0x57, 0x69, 0x74, 0x68, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x12,
+	0x42, 0x0a, 0x07, 0x70, 0x61, 0x79, 0x6c, 0x6f, 0x61, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b,
+	0x32, 0x28, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x65, 0x6e, 0x67, 0x69, 0x6e, 0x65, 0x2e,
+	0x76, 0x32, 0x2e, 0x45, 0x78, 0x65, 0x63, 0x75, 0x74, 0x69, 0x6f, 0x6e, 0x50, 0x61, 0x79, 0x6c,
+	0x6f, 0x61, 0x64, 0x43, 0x61, 0x70, 0x65, 0x6c, 0x6c, 0x61, 0x52, 0x07, 0x70, 0x61, 0x79, 0x6c,
+	0x6f, 0x61, 0x64, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01,
+	0x28, 0x0c, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x42, 0x2f, 0x5a, 0x2d, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x69, 0x74, 0x73, 0x64, 0x65, 0x76, 0x62, 0x65,
+	0x61, 0x72, 0x2f, 0x62, 0x6f, 0x6c, 0x61, 0x72, 0x69, 0x73, 0x2f, 0x74, 0x79, 0x70, 0x65, 0x73,
+	0x2f, 0x65, 0x6e, 0x67, 0x69, 0x6e, 0x65, 0x2f, 0x76, 0x32, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -295,18 +361,20 @@ func file_types_engine_v2_execution_payload_proto_rawDescGZIP() []byte {
 	return file_types_engine_v2_execution_payload_proto_rawDescData
 }
 
-var file_types_engine_v2_execution_payload_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_types_engine_v2_execution_payload_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_types_engine_v2_execution_payload_proto_goTypes = []interface{}{
-	(*ExecutionPayloadCapella)(nil), // 0: types.engine.v2.ExecutionPayloadCapella
-	(*v1.Withdrawal)(nil),           // 1: types.engine.v1.Withdrawal
+	(*ExecutionPayloadCapella)(nil),          // 0: types.engine.v2.ExecutionPayloadCapella
+	(*ExecutionPayloadCapellaWithValue)(nil), // 1: types.engine.v2.ExecutionPayloadCapellaWithValue
+	(*v1.Withdrawal)(nil),                    // 2: types.engine.v1.Withdrawal
 }
 var file_types_engine_v2_execution_payload_proto_depIdxs = []int32{
-	1, // 0: types.engine.v2.ExecutionPayloadCapella.withdrawals:type_name -> types.engine.v1.Withdrawal
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	2, // 0: types.engine.v2.ExecutionPayloadCapella.withdrawals:type_name -> types.engine.v1.Withdrawal
+	0, // 1: types.engine.v2.ExecutionPayloadCapellaWithValue.payload:type_name -> types.engine.v2.ExecutionPayloadCapella
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_types_engine_v2_execution_payload_proto_init() }
@@ -327,6 +395,18 @@ func file_types_engine_v2_execution_payload_proto_init() {
 				return nil
 			}
 		}
+		file_types_engine_v2_execution_payload_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ExecutionPayloadCapellaWithValue); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -334,7 +414,7 @@ func file_types_engine_v2_execution_payload_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_types_engine_v2_execution_payload_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
