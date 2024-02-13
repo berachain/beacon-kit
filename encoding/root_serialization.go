43d69737253c774a611f7468c0af9d02cab80b44
ocnc <anocpublic@gmail.com>
2024-02-12 22:14:08 -0500

    more clean

--- encoding/root_serialization.go
+++ encoding/root_serialization.go
@@ -2,7 +2,6 @@ package encoding
 
 import (
 	"errors"
-	"unsafe"
 
 	_ "github.com/minio/sha256-simd"
 	"github.com/protolambda/ztyp/tree"
@@ -19,10 +18,6 @@ func TransactionsRoot(txs [][]byte) ([32]byte, error) {
 	for i := 0; i < len(txs); i++ {
 		rt := tree.GetHashFn().ByteListHTR(txs[i], fieldparams.MaxBytesPerTxLength) // getting the transaction root here
 		txRoots = append(txRoots, rt)
-	}
-
-	if uint64(len(txRoots)) > fieldparams.MaxTxsPerPayloadLength {
-		return [32]byte{}, errors.New("merkleizing list that is too large, over limit")
 	}
 
 	byteRoots, err := SafeMerkleizeVector(
@@ -56,37 +51,9 @@ func UnsafeMerkleizeVector(roots []tree.Root, length uint64) tree.Root {
 			roots = append(roots, x)
 		}
 
-		// map htr.VectorizedSha256 to roots result
-		res := htr.VectorizedSha256(*(*[][32]byte)(unsafe.Pointer(&roots)))
-		roots = *(*[]tree.Root)(unsafe.Pointer(&res))
+		// TODO: move this because gpl
+		res := htr.VectorizedSha256(convertTreeRootsToBytes(roots))
+		roots = convertBytesToTreeRoots(res)
 	}
 	return roots[0]
-}
-
-// Hashable is an interface representing objects that implement HashTreeRoot()
-type Hashable interface {
-	HashTreeRoot() ([32]byte, error)
-}
-
-// MerkleizeVectorSSZ hashes each element in the list and then returns the HTR
-// of the corresponding list of roots
-func MerkleizeVectorSSZ[T Hashable](elements []T, length uint64) ([32]byte, error) {
-	roots := make([][32]byte, len(elements))
-	var err error
-	for i, el := range elements {
-		roots[i], err = el.HashTreeRoot()
-		if err != nil {
-			return [32]byte{}, err
-		}
-	}
-
-	return UnsafeMerkleizeVector(convertBytesToTreeRoots(roots), length), nil
-}
-
-func convertTreeRootsToBytes(roots []tree.Root) [][32]byte {
-	return *(*[][32]byte)(unsafe.Pointer(&roots))
-}
-
-func convertBytesToTreeRoots(bytes [][32]byte) []tree.Root {
-	return *(*[]tree.Root)(unsafe.Pointer(&bytes))
 }
