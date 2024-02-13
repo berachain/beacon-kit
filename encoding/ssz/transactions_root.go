ea656d854810402a03c9c490969c9fd36bedb354
itsdevbear <itsdevbear@berachain.com>
2024-02-12 22:50:21 -0500

    full strong typing

--- encoding/ssz/transactions_root.go
+++ encoding/ssz/transactions_root.go
@@ -44,7 +44,7 @@ func (bz Bytes) HashTreeRoot(h tree.HashFn) tree.Root {
 // TransactionsRoot computes the HTR for the Transactions' property of the ExecutionPayload
 // The code was largely copy/pasted from the code generated to compute the HTR of the entire
 // ExecutionPayload.
-func TransactionsRoot(txs []Bytes) ([32]byte, error) {
+func TransactionsRoot(txs []Bytes) (tree.Root, error) {
 	fn := tree.GetHashFn()
 	txRoots := make([]tree.Root, 0)
 	for i := 0; i < len(txs); i++ {
@@ -58,7 +58,7 @@ func TransactionsRoot(txs []Bytes) ([32]byte, error) {
 		txRoots, lenTxRoots, primitives.MaxTxsPerPayloadLength,
 	)
 	if err != nil {
-		return [32]byte{}, err
+		return tree.Root{}, err
 	}
 	return fn.Mixin(byteRoots, lenTxRoots), nil
 }
