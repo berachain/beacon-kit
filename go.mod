710be2757828d0d9b7e88de22e85797ececfe438
po-bera <156114927+po-bera@users.noreply.github.com>
2024-02-16 15:14:09 -0500

    feat(collections): Queue (#154)
    * add queue
    
    * Update collections/queue.go
    
    Co-authored-by: Devon Bear <itsdevbear@berachain.com>
    Signed-off-by: po-bera <156114927+po-bera@users.noreply.github.com>
    
    * mv
    
    * using name for prefix
    
    * add fuzz tests
    
    * fix(queue): Fuzz testing bug?  (#163)
    
    * queue bug
    
    * off by 1
    
    * Update queue.go
    
    Co-authored-by: po-bera <156114927+po-bera@users.noreply.github.com>
    Signed-off-by: Devon Bear <itsdevbear@berachain.com>
    
    * Update queue.go
    
    Co-authored-by: po-bera <156114927+po-bera@users.noreply.github.com>
    Signed-off-by: Devon Bear <itsdevbear@berachain.com>
    
    * refactor
    
    * refactor
    
    * refactor
    
    ---------
    
    Signed-off-by: Devon Bear <itsdevbear@berachain.com>
    Co-authored-by: po-bera <156114927+po-bera@users.noreply.github.com>
    Co-authored-by: Po Bera <po@berachain.com>
    
    * cleanup
    
    * go mod
    
    * fix lint
    
    * cleanup tests
    
    * remove stuff
    
    * comment
    
    * rm comment
    
    ---------
    
    Signed-off-by: po-bera <156114927+po-bera@users.noreply.github.com>
    Signed-off-by: Devon Bear <itsdevbear@berachain.com>
    Co-authored-by: Devon Bear <itsdevbear@berachain.com>

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ replace github.com/syndtr/goleveldb => github.com/syndtr/goleveldb v1.0.1-0.2021
 require (
 	cosmossdk.io/api v0.7.2
 	cosmossdk.io/client/v2 v2.0.0-20230818115413-c402c51a1508
+	cosmossdk.io/collections v0.4.0
 	cosmossdk.io/core v0.11.0
 	cosmossdk.io/depinject v1.0.0-alpha.4
 	cosmossdk.io/errors v1.0.1
@@ -58,7 +59,6 @@ require (
 	cloud.google.com/go/storage v1.35.1 // indirect
 	connectrpc.com/connect v1.14.0 // indirect
 	connectrpc.com/otelconnect v0.7.0 // indirect
-	cosmossdk.io/collections v0.4.0 // indirect
 	cosmossdk.io/math v1.2.0 // indirect
 	cosmossdk.io/x/tx v0.13.0 // indirect
 	filippo.io/edwards25519 v1.0.0 // indirect
---
+++ lib/store/collections/queue_test.go
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2024 Berachain Foundation
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+package collections_test
+
+import (
+	"context"
+	"testing"
+
+	sdk "cosmossdk.io/collections"
+	"cosmossdk.io/core/store"
+	dba "cosmossdk.io/store/dbadapter"
+	db "github.com/cosmos/cosmos-db"
+	"github.com/stretchr/testify/require"
+
+	"github.com/itsdevbear/bolaris/lib/store/collections"
+)
+
+func Test_Queue(t *testing.T) {
+	t.Run("should return correct items and lengths", func(t *testing.T) {
+		sk, ctx := deps()
+		sb := sdk.NewSchemaBuilder(sk)
+		q := collections.NewQueue[uint64](sb, "queue", sdk.Uint64Value)
+
+		// Test initial length of the queue
+		qlen, err := q.Len(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), qlen, "Queue should be empty initially")
+
+		_, err = q.Peek(ctx)
+		require.Equal(t, sdk.ErrNotFound, err)
+
+		_, err = q.Pop(ctx)
+		require.Equal(t, sdk.ErrNotFound, err)
+
+		err = q.Push(ctx, 1)
+		require.NoError(t, err)
+
+		// Test length after first push
+		qlen, err = q.Len(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint64(1), qlen, "Queue should have 1 item after first push")
+
+		err = q.Push(ctx, 2)
+		require.NoError(t, err)
+
+		// Test length after second push
+		qlen, err = q.Len(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint64(2), qlen, "Queue should have 2 items after second push")
+
+		v, err := q.Pop(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint64(1), v)
+
+		// Test length after first pop
+		qlen, err = q.Len(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint64(1), qlen, "Queue should have 1 item after first pop")
+
+		v, err = q.Pop(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint64(2), v)
+
+		// Test length after clearing the queue
+		qlen, err = q.Len(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), qlen, "Queue should be empty after clearing")
+
+		// Attempt to peek at the top value of the queue, expecting an error since the queue
+		// should now be empty
+		_, err = q.Peek(ctx)
+		require.Equal(t, sdk.ErrNotFound, err)
+
+		// Attempt to pop an item from the queue, expecting an error since the queue is empty
+		_, err = q.Pop(ctx)
+		require.Equal(t, sdk.ErrNotFound, err)
+	})
+}
+
+// MockStore wraps the dba.Store to implement additional functionalities.
+type MockStore struct {
+	dba.Store
+}
+
+// OpenKVStore returns the underlying KVStore from the Store.
+func (s MockStore) OpenKVStore(ctx context.Context) store.KVStore {
+	return s.DB
+}
+
+// deps initializes dependencies for testing, returning a KVStoreService and a context.
+func deps() (store.KVStoreService, context.Context) {
+	return &MockStore{Store: dba.Store{DB: db.NewMemDB()}}, context.Background()
+}
