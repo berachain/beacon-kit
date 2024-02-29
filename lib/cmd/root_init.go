a775a2c5ff40c16edc2f51ba1c8ff4d753345d05
itsdevbear <itsdevbear@berachain.com>
2024-02-29 14:50:09 -0500

    re-add jwt command

--- lib/cmd/root_init.go
+++ lib/cmd/root_init.go
@@ -128,6 +128,7 @@ func AddCommands[T servertypes.Application](
 	rootCmd.AddCommand(
 		startCmd,
 		cometCmd,
+		beaconconfig.BeaconKitCommands(),
 		version.NewVersionCommand(),
 		server.NewRollbackCmd(appCreator),
 	)
