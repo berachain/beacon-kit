1e094207f51419877abef400651e98ce5ecf3c32
neverDefined <hk-nft@protonmail.com>
2024-02-29 16:34:12 -0500

    grammar nits

--- contracts/src/staking/IDepositContract.sol
+++ contracts/src/staking/IDepositContract.sol
@@ -27,7 +27,7 @@ pragma solidity 0.8.24;
 
 /// @title IDepositContract
 /// @author Berachain Team.
-/// @dev This contract is used to deposit (create or top up validator), redirect and withdraw stake from the Beaconchain.
+/// @dev This contract is used to create validator, deposit, redirect and withdraw stake from the Beaconchain.
 interface IDepositContract {
     // /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     // /*                        EVENTS                              */
@@ -35,8 +35,8 @@ interface IDepositContract {
 
     /**
      * @dev Emitted when a deposit is made, which could mean a new validator or a top up of an existing one.
-     * @param validatorPubKey the public key of the validator who is being deposited for (if not a new validator).
-     * @param stakingCredentials the public key of the operator(if new validator) or the delegator(if top up).
+     * @param validatorPubKey the public key of the validator who is being deposited for if not a new validator.
+     * @param stakingCredentials the public key of the operator if new validator or the depositor if top up.
      * @param amount the amount of stake being deposited.
      * @param signature the signature of the deposit message, only checked when creating a new validator.
      */
@@ -49,12 +49,16 @@ interface IDepositContract {
 
     /**
      * @dev Emitted when a redirect of stake is made.
+     * @param stakingCredentials The public key of the account redirecting their stake.
      * @param fromPubKey The public key of the validator that is being redirected from.
      * @param toPubKey The public key of the validator that is being redirected to.
      * @param amount The amount of stake be redirected.
      */
     event Redirect(
-        bytes indexed fromPubKey, bytes indexed toPubKey, uint64 amount
+        bytes indexed stakingCredentials,
+        bytes indexed fromPubKey,
+        bytes indexed toPubKey,
+        uint64 amount
     );
 
     /**
@@ -82,10 +86,10 @@ interface IDepositContract {
 
     /**
      * @notice Submit a deposit message to the Beaconchain.
-     * @notice This will be used to create a new validator or to top up an existing one, increasing delegation.
+     * @notice This will be used to create a new validator or to top up an existing one, increasing stake.
      * @param validatorPubKey is the consensus public key of the validator. If subsequent deposit, its ignored.
      * @param stakingCredentials is the staking credentials of the validator. If this is the first deposit it is
-     * validator operator public key, if subsequent deposit it is the delegators public key.
+     * validator operator public key, if subsequent deposit it is the depositors public key.
      * @param amount is the amount of stake native/ERC20 token to be deposited.
      * @param signiture is the signiture used only on the first deposit.
      */
@@ -99,8 +103,8 @@ interface IDepositContract {
         payable;
 
     /**
-     * @notice Submit a redirect stake message, this allows delegators to move their stake from one validator to another.
-     * @notice This function is only callable by the owner of the stake. hence the signature is not required.
+     * @notice Submit a redirect stake message, this allows depositors to move their stake from one validator to another.
+     * @notice This function is only callable by the owner of the stake. Hence the signature is not required.
      * @param fromPubKey is the public key of the source validator where we are removing the stake from.
      * @param toPubKey is the public key of the destination validator where we are adding the stake to.
      * @param amount is the amount of stake to be redirected, this amount needs to be calculated offchain.
@@ -115,15 +119,15 @@ interface IDepositContract {
 
     /**
      * @notice Submit a withdrawal message to the Beaconchain.
-     * @notice this function is callable by the account with the stake.
+     * @notice This function is callable by the account with the stake.
      * @param validatorPubKey is the public key of the validator we are withdrawing from.
-     * @param withdrawCredentials is the public key of the account that will receive the withdrawal.
+     * @param withdrawalCredentials is the public key of the account that will receive the withdrawal.
      * @param amount is the amount of stake to be withdrawn, the amount needs to be calculated offchain since
      * validator tokens are not fungible, and their shares -> stake amount can differ if there is a slashing event.
      */
     function withdraw(
         bytes calldata validatorPubKey,
-        bytes calldata withdrawCredentials,
+        bytes calldata withdrawalCredentials,
         uint64 amount
     )
         external;
