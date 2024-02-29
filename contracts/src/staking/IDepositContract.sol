4d161c3ea9702639a88e8f19fa1330ccea5f6419
neverDefined <hk-nft@protonmail.com>
2024-02-29 15:52:56 -0500

    feat: improve interface and docs

--- contracts/src/staking/IDepositContract.sol
+++ contracts/src/staking/IDepositContract.sol
@@ -27,95 +27,100 @@ pragma solidity 0.8.24;
 
 /// @title IDepositContract
 /// @author Berachain Team.
-/// @dev This contract is the interdface of the deposit contract that the beaconkit uses to handle its
-/// delegate proof of stake system. It is derived from the Ethereum 2.0 specification but with an arbitrary
-/// staking backend for the chain, hence it is very flexible and can be used with ERC20s or the native token.
+/// @dev This contract is used to deposit (create or top up validator), redirect and withdraw stake from the Beaconchain.
 interface IDepositContract {
-    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-    /*                        EVENTS                              */
-    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    // /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    // /*                        EVENTS                              */
+    // /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /**
-     * @dev Emitted when a deposit is made.
-     * @notice the withdrawCredentials can be left empty if its not the first deposit.
-     * @param amount The amount of the deposit.
-     * @param pubKey The public key of the validator.
-     * @param withdrawalCredentials The withdrawal credentials of the validator.
-     * @param signature The signature of the depositor.
+     * @dev Emitted when a deposit is made, which could mean a new validator or a top up of an existing one.
+     * @param validatorPubKey the public key of the validator who is being deposited for (if not a new validator).
+     * @param stakingCredentials the public key of the operator(if new validator) or the delegator(if top up).
+     * @param amount the amount of stake being deposited.
+     * @param signature the signature of the deposit message, only checked for creating a new validator.
      */
     event Deposit(
+        bytes validatorPubKey,
+        bytes stakingCredentials,
         uint64 amount,
-        bytes pubKey,
-        bytes withdrawalCredentials,
         bytes signature
     );
 
     /**
-     * @dev Emitted when a redirection is made.
-     * @param srcPub The public key of the source validator.
-     * @param dstPub The public key of the destination validator.
-     * @param amount The amount to be redirected.
-     * @param signature The signature of the redirector with the stake.
+     * @dev Emitted when a redirect of stake is made.
+     * @param fromPubKey The public key of the validator that is being redirected from.
+     * @param toPubKey The public key of the validator that is being redirected to.
+     * @param amount The amount of stake be redirected.
      */
-    event Redirect(bytes srcPub, bytes dstPub, uint64 amount, bytes signature);
+    event Redirect(bytes fromPubKey, bytes toPubKey, uint64 amount);
 
     /**
-     * @dev Emitted when a withdrawal is made.
-     * @param pubKey The public key of the validator.
+     * @dev Emitted when a deposit is made.
+     * @param fromPubKey The public key of the validator that is being withdrawn from.
+     * @param withdrawalCredentials The public key of the account that will receive the withdrawal.
      * @param amount The amount to be withdrawn from the validator.
-     * @param signature The signature of the withdrawer.
      */
-    event Withdraw(bytes pubKey, uint64 amount, bytes signature);
+    event Withdraw(
+        bytes fromPubKey, bytes withdrawalCredentials, uint64 amount
+    );
 
-    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-    /*                        WRITES                              */
-    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    // /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    // /*                        ERRORS                              */
+    // /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Error thrown when the deposit amount is too small, to prevent dust deposits.
+    error InsufficientDeposit();
+
+    // /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    // /*                        WRITES                              */
+    // /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /**
-     * @notice Submit a stake message to the Beaconchain.
-     * @param amount The amount of the deposit.
-     * @param pubkey A BLS12-381 public key.
-     * @param withdrawal_credentials Commitment to a public key for withdrawals.
-     * @param signature A BLS12-381 signature from the depositor.
+     * @notice Submit a deposit message to the Beaconchain.
+     * @notice This will be used to create a new validator or to top up an existing one, increasing delegation.
+     * @param validatorPubKey is the consensus public key of the validator. If subsequent deposit, its ignored.
+     * @param stakingCredentials is the staking credentials of the validator. If this is the first deposit it is
+     * validator operator public key, if subsequent deposit it is the delegators public key.
+     * @param amount is the amount of stake native/ERC20 token to be deposited.
+     * @param signiture is the signiture used only on the first deposit.
      */
     function deposit(
-        uint64 amount, // in Gwei even if an ERC20 token.
-        bytes calldata pubkey,
-        bytes calldata withdrawal_credentials,
-        bytes calldata signature
+        bytes calldata validatorPubKey,
+        bytes calldata stakingCredentials,
+        uint64 amount,
+        bytes calldata signiture
     )
         external
         payable;
 
     /**
-     * @notice Submit a redirect stake message.
-     * @notice This function is only callable by the owner of the stake.
-     * @param srcPub A BLS12-381 public key of the source validator.
-     * @param dstPub A BLS12-381 public key of the destination validator.
-     * @param amount The amount of the deposit.
-     * @param signature A BLS12-381 signature from the redirector.
+     * @notice Submit a redirect stake message, this allows delegators to move their stake from one validator to another.
+     * @notice This function is only callable by the owner of the stake. hence the signature is not required.
+     * @param fromPubKey is the public key of the source validator where we are removing the stake from.
+     * @param toPubKey is the public key of the destination validator where we are adding the stake to.
+     * @param amount is the amount of stake to be redirected, this amount needs to be calculated offchain.
+     *   since validator tokens are not fungible, and their shares -> stake amount can differ.
      */
     function redirect(
-        bytes calldata srcPub,
-        bytes calldata dstPub,
-        uint64 amount,
-        bytes calldata signature
+        bytes calldata fromPubKey,
+        bytes calldata toPubKey,
+        uint64 amount
     )
-        external
-        payable;
+        external;
 
     /**
      * @notice Submit a withdrawal message to the Beaconchain.
-     * @notice This function is only callable by the owner of the stake.
-     * @param pubkey A BLS12-381 public key.
-     * @param amount The amount of the deposit to be withdrawn.
-     * @param signature A BLS12-381 signature of the withdrawer.
+     * @notice this function is callable by the account with the stake.
+     * @param validatorPubKey is the public key of the validator we are withdrawing from.
+     * @param withdrawCredentials is the public key of the account that will receive the withdrawal.
+     * @param amount is the amount of stake to be withdrawn, the amount needs to be calculated offchain since
+     * validator tokens are not fungible, and their shares -> stake amount can differ if there is a slashing event.
      */
     function withdraw(
-        bytes calldata pubkey,
-        uint64 amount,
-        bytes calldata signature
+        bytes calldata validatorPubKey,
+        bytes calldata withdrawCredentials,
+        uint64 amount
     )
-        external
-        payable;
+        external;
 }
