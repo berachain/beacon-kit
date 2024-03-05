c5a379c471468c616b22f34a1d9cea1565fcac3e
codingwithmanny <manny@berachain.com>
2024-03-06 00:12:34 +0300

    feat: 🎸 Docs Added Initial Sitemap Structure

---
+++ docs/vocs.theme.beaconkit.tsx
@@ -0,0 +1,76 @@
+// Imports
+// ========================================================
+import { Theme } from 'vocs';
+
+// Constants
+// ========================================================
+// const Colors = {
+//   white: '#FFFFFF',
+//   black: '#000000',
+//   deepTeal: "#39534E",
+//   darkSlate: "#28302B",
+//   richEspresso: "#2C251F",
+//   warmMahogany: "#2B1C17",
+//   almostBlack: "#0E0803",
+//   burntSienna: "#512313",
+//   darkChestnut: "#5D2D19",
+//   rustyCopper: "#8E4719",
+//   vibrantTangerine: "#F47226",
+//   brightOrange: "#FF8425",
+//   sunnyYellow: "#FDB72D",
+//   lightGold: "#FACC62",
+//   paleSunshine: "#FBE38A",
+//   softCream: "#FBEEB9",
+//   deepCyan: "#223F3F",
+//   oceanGreen: "#275758",
+//   tealBlue: "#396566",
+//   softIvory: '#F5F5F4',
+//   darkCoffee: '#1F1512',
+//   deepCharcoal: '#1E1B1A',
+//   mistyPewter: '#9F9591',
+//   midnightExpresso: '#160905',
+//   velvetOnyx: '#1E1B19',
+//   brandGradient: "-webkit-linear-gradient(117deg, #FF8A00F5, #FFC738)"
+// }
+
+// Main App Component
+// ========================================================
+export const theme: Theme = {
+  accentColor: {
+    light: '#FF0101',
+    dark: '#61CEFD'
+  },
+  // accentColor: {
+  //   light: `${Colors.almostBlack}`,
+  //   dark: `${Colors.sunnyYellow}`
+  // },
+  // variables: {
+  //   fontFamily: {
+  //     default: '"IBM Plex Sans", sans-serif',
+  //     mono: '"IBM Plex Mono", monospace'
+  //   },
+  //   color: {
+  //     // var(--vocs-color_text)
+  //     // text: {
+  //     //   light: `rgba(60, 60, 67)`,
+  //     //   dark: `${Colors.white}`
+  //     // },
+  //     heading: { 
+  //       light: '#202020', 
+  //       dark: `${Colors.white}`,
+  //     },
+  //     background: {
+  //       light: `${Colors.white}`,
+  //       dark: `${Colors.almostBlack}`
+  //     },
+  //     backgroundDark: {
+  //       light: `${Colors.softIvory}`,
+  //       dark: `${Colors.velvetOnyx}`
+  //     },
+  //     text3: { 
+  //       light: `${Colors.darkSlate}`,
+  //       dark: `${Colors.mistyPewter}`
+  //     }
+  //   }
+  // }
+};
