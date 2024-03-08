064fb810c4898794a414b801d8878f1adb67b9bb
itsdevbear <itsdevbear@berachain.com>
2024-03-08 00:15:24 -0500

    feat(client): update isTimeout

--- engine/client/errors.go
+++ engine/client/errors.go
@@ -227,12 +227,26 @@ func (s *EngineClient) handleRPCError(err error) error {
 	}
 }
 
+// httpTimeoutError defines an interface for timeout errors.
+// It includes methods for error message retrieval and timeout status checking.
 type httpTimeoutError interface {
+	// Error returns the error message.
 	Error() string
+	// Timeout indicates whether the error is a timeout error.
 	Timeout() bool
 }
 
+// isTimeout checks if the given error is a timeout error.
+// It asserts the error to the httpTimeoutError interface and checks its Timeout
+// status.
+// Returns true if the error is a timeout error, false otherwise.
 func isTimeout(e error) bool {
-	t, ok := e.(httpTimeoutError) //nolint:errorlint // from prysm.
-	return ok && t.Timeout()
+	if e == nil {
+		return false
+	}
+	var t httpTimeoutError
+	if errors.As(e, &t) {
+		return t.Timeout()
+	}
+	return false
 }
