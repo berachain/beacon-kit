1304af4ecad2b7dd3570137db879152f2d688d5e
itsdevbear <itsdevbear@berachain.com>
2024-03-10 23:57:04 -0400

    godoc body

--- beacon/core/types/body.go
+++ beacon/core/types/body.go
@@ -30,36 +30,49 @@ import (
 	"github.com/cockroachdb/errors"
 )
 
+// BeaconBlockBodyDeneb represents the body of a beacon block in the Deneb
+// chain.
 type BeaconBlockBodyDeneb struct {
-	RandaoReveal       [96]byte   `ssz-size:"96"`
-	Graffiti           [32]byte   `ssz-size:"32"`
-	Deposits           []*Deposit `                ssz-max:"16"`
-	ExecutionPayload   *enginetypes.ExecutableDataDeneb
+	// RandaoReveal is the reveal of the RANDAO.
+	RandaoReveal [96]byte `ssz-size:"96"`
+	// Graffiti is for a fun message or meme.
+	Graffiti [32]byte `ssz-size:"32"`
+	// Deposits is the list of deposits included in the body.
+	Deposits []*Deposit `                ssz-max:"16"`
+	// ExecutionPayload is the execution payload of the body.
+	ExecutionPayload *enginetypes.ExecutableDataDeneb
+	// BlobKzgCommitments is the list of KZG commitments for the EIP-4844 blobs.
 	BlobKzgCommitments [][48]byte `ssz-size:"?,48" ssz-max:"16"`
 }
 
+// IsNil checks if the BeaconBlockBodyDeneb is nil.
 func (b *BeaconBlockBodyDeneb) IsNil() bool {
 	return b == nil
 }
 
+// GetBlobKzgCommitments returns the BlobKzgCommitments of the Body.
 func (b *BeaconBlockBodyDeneb) GetBlobKzgCommitments() [][48]byte {
 	return b.BlobKzgCommitments
 }
 
+// GetExecutionPayload returns the ExecutionPayload of the Body.
 //
 //nolint:lll
 func (b *BeaconBlockBodyDeneb) GetExecutionPayload() enginetypes.ExecutionPayload {
 	return b.ExecutionPayload
 }
 
+// GetDeposits returns the Deposits of the BeaconBlockBodyDeneb.
 func (b *BeaconBlockBodyDeneb) GetDeposits() []*Deposit {
 	return b.Deposits
 }
 
+// SetDeposits sets the Deposits of the BeaconBlockBodyDeneb.
 func (b *BeaconBlockBodyDeneb) SetDeposits(deposits []*Deposit) {
 	b.Deposits = deposits
 }
 
+// SetExecutionData sets the ExecutionData of the BeaconBlockBodyDeneb.
 func (b *BeaconBlockBodyDeneb) SetExecutionData(
 	executionData enginetypes.ExecutionPayload,
 ) error {
