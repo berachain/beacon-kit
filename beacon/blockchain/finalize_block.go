2f298da84c8b37f8fc1778a1c8e7744480ae1803
itsdevbear <itsdevbear@berachain.com>
2024-03-14 16:24:48 -0400

    vm

--- beacon/blockchain/finalize_block.go
+++ beacon/blockchain/finalize_block.go
@@ -93,12 +93,6 @@ func (s *Service) FinalizeBeaconBlock(
 		s.Logger().Error("failed to process logs", "error", err)
 		return err
 	}
-
-	// TODO: put into an actual function / flow
-	_, err = s.BeaconState(ctx).DequeueDeposits(
-		uint64(len(blk.GetBody().GetDeposits())))
-
-	// TODO: Actually Process Staking Deposits and Withdraws.
 	return err
 }
 
