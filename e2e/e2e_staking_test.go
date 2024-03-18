24118f17c694326908a9ffd6db77692b49becea3
ocnc <anocpublic@gmail.com>
2024-03-18 03:06:11 -0400

    check for power increase

--- e2e/e2e_staking_test.go
+++ e2e/e2e_staking_test.go
@@ -35,37 +35,50 @@ import (
 	coretypes "github.com/ethereum/go-ethereum/core/types"
 )
 
-// TestForgeScriptExecution tests the execution of a forge script
-// against the beacon-kit network.
+const (
+	// DepositContractAddress is the address of the deposit contract.
+	DepositContractAddress = "0x00000000219ab540356cbb839cbe05303d7705fa"
+)
+
+// TestDepositContract tests the deposit contract to attempt staking and
+// increasing a validator's consensus power.
 func (s *BeaconKitE2ESuite) TestDepositContract() {
+	// Get the consensus client.
 	client := s.ConsensusClients()["cl-validator-beaconkit-0"]
 	s.Require().NotNil(client)
 
+	// Get the public key.
 	pubkey, err := client.GetPubKey(s.Ctx())
 	s.Require().NoError(err)
 	s.Require().Len(pubkey, 48)
 
-	_, err = client.GetConsensusPower(s.Ctx())
+	// Get the consensus power.
+	power, err := client.GetConsensusPower(s.Ctx())
 	s.Require().NoError(err)
 
+	// Bind the deposit contract.
 	dc, err := stakingabi.NewBeaconDepositContract(
-		common.HexToAddress("0x00000000219ab540356cbb839cbe05303d7705fa"),
+		common.HexToAddress(DepositContractAddress),
 		s.JSONRPCBalancer(),
 	)
 	s.Require().NoError(err)
 
+	// Generate the credentials.
 	credentials := byteslib.PrependExtendToSize(
 		s.GenesisAccount().Address().Bytes(),
 		32,
 	)
 	credentials[0] = 0x01
 
+	// Generate the signature.
 	signature := [96]byte{}
 	s.Require().Len(signature[:], 96)
 
+	// Get the chain ID.
 	chainID, err := s.JSONRPCBalancer().ChainID(s.Ctx())
 	s.Require().NoError(err)
 
+	// Create a deposit transaction.
 	val, _ := big.NewFloat(32e18).Int(nil)
 	tx, err := dc.Deposit(&bind.TransactOpts{
 		From:   s.GenesisAccount().Address(),
@@ -74,8 +87,15 @@ func (s *BeaconKitE2ESuite) TestDepositContract() {
 	}, pubkey, credentials, 0, signature[:])
 	s.Require().NoError(err)
 
+	// Wait for the transaction to be mined.
 	var receipt *coretypes.Receipt
 	receipt, err = bind.WaitMined(s.Ctx(), s.JSONRPCBalancer(), tx)
 	s.Require().NoError(err)
 	s.Require().Equal(uint64(1), receipt.Status)
+	s.Require().True(s.CheckForSuccessfulTx(receipt.TxHash))
+
+	// Check that the consensus power has increased.
+	newPower, err := client.GetConsensusPower(s.Ctx())
+	s.Require().NoError(err)
+	s.Require().Greater(newPower, power)
 }
