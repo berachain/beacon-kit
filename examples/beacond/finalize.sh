a944db48d0e7d91a60157627dde18b8fcb393641
itsdevbear <itsdevbear@berachain.com>
2024-03-23 00:04:09 -0400

    more

--- examples/beacond/finalize.sh
+++ examples/beacond/finalize.sh
@@ -27,7 +27,7 @@
 set -x
 
 # Collect genesis tx
-/usr/bin/beacond genesis collect-gentxs --home "$BEACOND_HOME" > /dev/null 2>&1
+/usr/bin/beacond genesis customcollect-gentxs --home "$BEACOND_HOME" > /dev/null 2>&1
 
-# Run this to ensure everything worked and that the genesis file is setup correctly
-/usr/bin/beacond genesis validate-genesis --home "$BEACOND_HOME" > /dev/null 2>&1
+# # Run this to ensure everything worked and that the genesis file is setup correctly
+# /usr/bin/beacond genesis validate-genesis --home "$BEACOND_HOME" > /dev/null 2>&1
