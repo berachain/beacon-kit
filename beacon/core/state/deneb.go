edb07f6d46b4dbd2ced9f82dbc65b1f1d7d8022b
Devon Bear <itsdevbear@berachain.com>
2024-03-24 17:57:59 -0400

    feat(protobuf): A-B-C-D-E, F-U And your mom and your sister and your job And your broke-ass car and that shit you call art Fuck you and your friends that I'll never see again Everybody but your dog, you can all fuck off (#599)
    * beacon state
    
    * fix randao marshalling
    
    * format

--- beacon/core/state/deneb.go
+++ beacon/core/state/deneb.go
@@ -23,18 +23,35 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-package types
+package state
 
-import "github.com/berachain/beacon-kit/beacon/core/randao/types"
+import (
+	"github.com/berachain/beacon-kit/beacon/core/randao/types"
+	"github.com/berachain/beacon-kit/primitives"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
 
-func DefaultGenesis() *GenesisState {
-	return &GenesisState{
-		//nolint:lll
-		Eth1GenesisHash: "0xb938bfdfcbf7f840b5cd93c5c9899335018f64a846c153db2c80b957b127d56f",
-		RandaoMix:       make([]byte, types.MixLength),
+// DefaultBeaconStateDeneb returns a default BeaconStateDeneb.
+func DefaultBeaconStateDeneb() *BeaconStateDeneb {
+	return &BeaconStateDeneb{
+		Eth1GenesisHash: common.HexToHash(
+			"0xb938bfdfcbf7f840b5cd93c5c9899335018f64a846c153db2c80b957b127d56f",
+		),
+		RandaoMix: make([]byte, types.MixLength),
 	}
 }
 
-func (gs *GenesisState) Mix() types.Mix {
-	return types.Mix(gs.RandaoMix)
+// TODO setup this properly.
+//
+//go:generate go run github.com/fjl/gencodec -type BeaconStateDeneb -field-override beaconStateDenebJSONMarshaling -out deneb.json.go
+type BeaconStateDeneb struct {
+	Eth1GenesisHash primitives.ExecutionHash `json:"eth1GenesisHash" ssz-size:"32"`
+	RandaoMix       []byte                   `json:"randaoMix"       ssz-size:"32"`
+}
+
+// beaconStateDenebJSONMarshaling is a type used to marshal/unmarshal
+// BeaconStateDeneb.
+type beaconStateDenebJSONMarshaling struct {
+	RandaoMix hexutil.Bytes
 }
