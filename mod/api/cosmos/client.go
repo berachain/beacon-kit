62e996c293a5493793827be67169585ee3ae60ec
Randy Grok <randygrok@gmail.com>
2024-04-05 17:51:55 +0200

    add cosmos based querier

--- mod/api/cosmos/client.go
+++ mod/api/cosmos/client.go
@@ -1,11 +1,13 @@
 package cosmos
 
 import (
+	"github.com/berachain/beacon-kit/mod/node-builder/service"
 	"github.com/cosmos/cosmos-sdk/server/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type ChainQuerier struct {
 	ContextGetter func(height int64, prove bool) (sdk.Context, error)
+	Service       service.BeaconStorageBackend
 	ABCI          types.ABCI
 }
---
+++ mod/api/cosmos/randao.go
@@ -0,0 +1,55 @@
+package cosmos
+
+import (
+	"github.com/berachain/beacon-kit/mod/api/beaconnode"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"strconv"
+)
+
+func (c ChainQuerier) GetStateRandao(params beaconnode.GetStateRandaoParams) beaconnode.GetStateRandaoRes {
+	stateId := params.StateID
+	if stateId == "" {
+		return &beaconnode.GetStateRandaoBadRequest{
+			Code:        1,
+			Message:     "state_id is required in URL params",
+			Stacktraces: nil,
+		}
+	}
+
+	stateIdAsInt, err := strconv.ParseUint(stateId, 10, 64)
+	if err != nil {
+		return &beaconnode.GetStateRandaoBadRequest{
+			Code:        2,
+			Message:     "state_id must be a number",
+			Stacktraces: nil,
+		}
+	}
+
+	ctx, err := c.ContextGetter(int64(stateIdAsInt), false)
+	if err != nil {
+		return &beaconnode.GetStateRandaoInternalServerError{
+			Code:        3,
+			Message:     err.Error(),
+			Stacktraces: nil,
+		}
+	}
+
+	randao, err := c.Service.BeaconState(ctx).GetRandaoMixAtIndex(stateIdAsInt)
+	if err != nil {
+		return &beaconnode.GetStateRandaoInternalServerError{
+			Code:        4,
+			Message:     err.Error(),
+			Stacktraces: nil,
+		}
+	}
+
+	resp := &beaconnode.GetStateRandaoOK{
+		ExecutionOptimistic: false,
+		Finalized:           true,
+		Data: beaconnode.GetStateRandaoOKData{
+			Randao: hexutil.Encode(randao[:]),
+		},
+	}
+
+	return resp
+}
