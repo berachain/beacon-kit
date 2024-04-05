132b8e0c5d5af029c6a8934259b65b0d81c2f47d
Devon Bear <itsdevbear@berachain.com>
2024-04-05 14:57:09 -0400

    feat(4844): GoKZG Implementation of `BlobVerifier` (#686)
    * bet
    
    * bet
    
    * bet
    
    * bet
    
    * hardtype
    
    * hardtype
    
    * hardtype
    
    * switch to ptr
    
    * move blob to pointer
    
    * fix

--- mod/da/types/sidecar.go
+++ mod/da/types/sidecar.go
@@ -46,11 +46,11 @@ type BlobSidecar struct {
 	// Index represents the index of the blob in the block.
 	Index uint64
 	// Blob represents the blob data.
-	Blob []byte `ssz-size:"131072"`
+	Blob *kzg.Blob
 	// KzgCommitment is the KZG commitment of the blob.
 	KzgCommitment kzg.Commitment `ssz-size:"48"`
 	// Kzg proof allows folr the verification of the KZG commitment.
-	KzgProof []byte `ssz-size:"48"`
+	KzgProof kzg.Proof `ssz-size:"48"`
 	// BeaconBlockHeader represents the beacon block header for which this blob
 	// is being included.
 	BeaconBlockHeader *primitives.BeaconBlockHeader
