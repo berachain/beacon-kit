229ff2b9950e9f2f33f562df9be5d05c0cf49b79
itsdevbear <itsdevbear@berachain.com>
2024-04-07 13:54:35 -0400

    capella validator predicates

--- mod/core/types/withdrawal_credentials.go
+++ mod/core/types/withdrawal_credentials.go
@@ -9,7 +9,7 @@
 // copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following
-// conditions:
+// conditions
 //
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
@@ -30,7 +30,7 @@ import (
 )
 
 // EthSecp256k1CredentialPrefix is the prefix for an Ethereum secp256k1.
-const EthSecp256k1CredentialPrefix = iota + 1
+const EthSecp256k1CredentialPrefix = byte(iota + 1)
 
 // WithdrawalCredentials is a staking credential that is used to identify a
 // validator.
@@ -52,7 +52,7 @@ func (wc WithdrawalCredentials) ToExecutionAddress() (
 	primitives.ExecutionAddress,
 	error,
 ) {
-	if wc[0] != byte(EthSecp256k1CredentialPrefix) {
+	if wc[0] != EthSecp256k1CredentialPrefix {
 		return primitives.ExecutionAddress{}, ErrInvalidWithdrawalCredentials
 	}
 	return primitives.ExecutionAddress(wc[12:]), nil
