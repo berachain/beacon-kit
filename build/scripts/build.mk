558f80a9a639106ad8fdc0fdd781e6624a681b7e
itsdevbear <itsdevbear@berachain.com>
2024-04-09 12:05:03 -0400

    bet

--- build/scripts/build.mk
+++ build/scripts/build.mk
@@ -87,26 +87,23 @@ $(BUILD_TARGETS): $(OUT_DIR)/
 
 $(OUT_DIR)/:
 	mkdir -p $(OUT_DIR)/
+
 	# Variables
-
-
+ARCH ?= $(shell uname -m)
+ifeq ($(ARCH),)
+	ARCH = arm64
+endif
 GO_VERSION ?= 1.22.1
 IMAGE_NAME ?= beacond
 
 # Docker Paths
 DOCKERFILE = ./Dockerfile
 
-
-DOCKER_ARCH ?= $(shell uname -m)
-ifeq ($(DOCKER_ARCH),)
-	DOCKER_ARCH = arm64
-endif
-
 build-docker: ## build a docker image containing `beacond`
 	@echo "Build a release docker image for the Cosmos SDK chain..."
 	docker build \
 	--build-arg GO_VERSION=$(GO_VERSION) \
-	--platform linux/$(DOCKER_ARCH) \
+	--platform linux/$(ARCH) \
 	--build-arg GIT_COMMIT=$(shell git rev-parse HEAD) \
 	--build-arg GIT_VERSION=$(shell git describe --tags --always --dirty) \
 	--build-arg GIT_BRANCH=$(shell git rev-parse --abbrev-ref HEAD) \
