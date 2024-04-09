f819559812f232c2d802ac83062b09d3ed37922f
itsdevbear <itsdevbear@berachain.com>
2024-04-09 11:35:13 -0400

    helper

--- build/scripts/release.mk
+++ build/scripts/release.mk
@@ -2,19 +2,18 @@
 
 RELEASE_TARGETS := build-linux-amd64 build-linux-arm64 build-darwin-arm64
 
-build-release: $(RELEASE_TARGETS)
+define build_release
+	GOOS=$(1) GOARCH=$(2) CGO_ENABLED=$(3) \
+	cd ${CURRENT_DIR}/$(TESTAPP_CMD_DIR) && \
+	go build -mod=readonly $(BUILD_FLAGS) -o $(OUT_DIR)/beacond-$(1)-$(2) ./.
+endef
 
 build-linux-amd64:
-	GOOS=linux GOARCH=amd64 \
-	cd ${CURRENT_DIR}/$(TESTAPP_CMD_DIR) && \
-	go build -mod=readonly $(BUILD_FLAGS) -o $(OUT_DIR)/beacond-linux-amd64  ./.
+	$(call build_release,linux,amd64,0)
 
 build-linux-arm64:
-	GOOS=linux GOARCH=arm64 \
-	cd ${CURRENT_DIR}/$(TESTAPP_CMD_DIR) && \
-	go build -mod=readonly $(BUILD_FLAGS) -o $(OUT_DIR)/beacond-linux-arm64  ./.
+	$(call build_release,linux,arm64,0)
 
 build-darwin-arm64:
-	CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 \
-	cd ${CURRENT_DIR}/$(TESTAPP_CMD_DIR) && \
-	go build -mod=readonly $(BUILD_FLAGS) -o $(OUT_DIR)/beacond-darwin-arm64  ./.
+	$(call build_release,darwin,arm64,1)
+
