54882d8b02f94933ec9acafde3ed3810df658bbb
itsdevbear <itsdevbear@berachain.com>
2024-04-09 23:58:46 -0400

    feat(primitives): Add Eth1Data

---
+++ mod/primitives/eth1data.go
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2024 Berachain Foundation
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+package primitives
+
+import "github.com/davecgh/go-spew/spew"
+
+//go:generate go run github.com/ferranbt/fastssz/sszgen -path eth1data.go -objs Eth1Data -include execution.go,primitives.go,bytes.go,$GETH_PKG_INCLUDE/common -output eth1data.ssz.go
+type Eth1Data struct {
+	DepositRoot  Root          `json:"depositRoot"  ssz-size:"32"`
+	DepositCount uint64        `json:"depositCount"`
+	BlockHash    ExecutionHash `json:"blockHash"    ssz-size:"32"`
+}
+
+// String returns a string representation of the Eth1Data.
+func (e *Eth1Data) String() string {
+	return spew.Sdump(e)
+}
