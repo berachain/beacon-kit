d18353895f749d7b64df4a62743aa4372845bfdd
ocnc <97242826+ocnc@users.noreply.github.com>
2024-04-11 16:53:06 -0400

    chore(make): bet (#725)
    bet

--- build/scripts/devtools.mk
+++ build/scripts/devtools.mk
@@ -12,6 +12,14 @@ sync:
 yap: ## the yap cave
 	@go run ./mod/node-builder/utils/yap/yap.go
 
+
+.PHONY: format build test-unit bet
+bet:
+	$(MAKE) format build test-unit
+	@git add .
+	@git commit -m 'bet'
+	@git push
+
 checkpoint: format  ## checkpoint and push to remote
 	@git add -A
 	@git commit -m "yo bet"
