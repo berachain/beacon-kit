cda7b8bbae2f176ed8c72548a08aa4214472da6f
nidhi-singh02 <trippin@berachain.com>
2024-04-12 20:45:56 +0530

    Package for genesis generation geth
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>

---
+++ mod/generate-genesis/genesis/genesis.go
@@ -0,0 +1,91 @@
+package genesis
+
+import (
+	"encoding/json"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+	"log"
+	"math/big"
+	"os"
+)
+
+type Genesis struct {
+	alloc types.GenesisAlloc
+}
+
+// NewGenesis creates a new Genesis instance with an initialized alloc field.
+func NewGenesis() *Genesis {
+	return &Genesis{
+		alloc: make(types.GenesisAlloc),
+	}
+}
+
+// TO-DO : Remove core package and use the internal package "primtives" instead
+func (g *Genesis) AddAccount(genesis *core.Genesis, address common.Address, balance *big.Int) {
+	g.alloc[address] = types.Account{
+		Balance: balance,
+	}
+	genesis.Alloc = g.alloc
+}
+
+func (g *Genesis) AddPredeploy(genesis *core.Genesis, address common.Address, code []byte, storage map[common.Hash]common.Hash, balance *big.Int, nonce uint64) {
+	g.alloc[address] = types.Account{
+		Code:    code,
+		Storage: storage,
+		Balance: balance,
+		Nonce:   nonce,
+	}
+	genesis.Alloc = g.alloc
+}
+
+func (g *Genesis) ToGethGenesis() *core.Genesis {
+	chainConfig := &params.ChainConfig{
+		ChainID:                       big.NewInt(80087), // 80087 is the chain ID for Berachain
+		HomesteadBlock:                big.NewInt(0),
+		DAOForkBlock:                  big.NewInt(0),
+		DAOForkSupport:                true,
+		EIP150Block:                   big.NewInt(0),
+		EIP155Block:                   big.NewInt(0),
+		EIP158Block:                   big.NewInt(0),
+		ByzantiumBlock:                big.NewInt(0),
+		ConstantinopleBlock:           big.NewInt(0),
+		PetersburgBlock:               big.NewInt(0),
+		IstanbulBlock:                 big.NewInt(0),
+		MuirGlacierBlock:              big.NewInt(0),
+		BerlinBlock:                   big.NewInt(0),
+		LondonBlock:                   big.NewInt(0),
+		ArrowGlacierBlock:             big.NewInt(0),
+		GrayGlacierBlock:              big.NewInt(0),
+		MergeNetsplitBlock:            big.NewInt(0),
+		ShanghaiTime:                  new(uint64),
+		CancunTime:                    new(uint64),
+		TerminalTotalDifficulty:       big.NewInt(0),
+		TerminalTotalDifficultyPassed: true,
+	}
+
+	return &core.Genesis{
+		Config:     chainConfig,
+		Nonce:      0x0000000000000000,
+		Timestamp:  0x0,
+		ExtraData:  common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000000658bdf435d810c91414ec09147daa6db624063790000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"),
+		GasLimit:   0x1c9c380,
+		Difficulty: big.NewInt(0x0),
+		Alloc:      g.alloc,
+		Coinbase:   common.Address{},
+	}
+}
+func (g *Genesis) WriteFileToJSON(genesis *core.Genesis, fileName string) []byte {
+	// Convert the genesis to JSON with indentation
+	genesisJSON, err := json.MarshalIndent(genesis, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal genesis: %v", err)
+	}
+	// Write the JSON data to a file
+	err = os.WriteFile(fileName, genesisJSON, 0644)
+	if err != nil {
+		log.Fatalf("Failed to write genesis.json: %v", err)
+	}
+	return genesisJSON
+}
---
+++ mod/generate-genesis/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"github.com/berachain/beacon-kit/mod/generate-genesis/genesis"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/spf13/cobra"
+	"math/big"
+	"strconv"
+)
+
+// CreateGenesisFileCmd Commands creates a new genesis file
+func CreateGenesisFileCmd() *cobra.Command {
+	var accountAddresses []string
+	var accountBalances []string
+	var predeployAddresses []string
+	var predeployCodes []string
+	var predeployBalances []string
+	var predeployNonces []string
+	var genesisFormat string
+	var outputFileName string
+
+	cmd := &cobra.Command{
+		Use:   "generate-genesis",
+		Short: "Generate a genesis.json file",
+		Run: func(cmd *cobra.Command, args []string) {
+			g := genesis.NewGenesis()
+			gen := g.ToGethGenesis()
+
+			if genesisFormat != "nethermind" && genesisFormat != "geth" {
+				cmd.PrintErrf("invalid genesis format %v\n", genesisFormat)
+			}
+			for i, address := range accountAddresses {
+				balance := accountBalances[i]
+				balanceBigInt, success := new(big.Int).SetString(balance, 10)
+				if !success {
+					cmd.PrintErrf("failed to convert balance to big.Int %v\n", balance)
+				}
+				g.AddAccount(gen, common.HexToAddress(address), balanceBigInt)
+			}
+
+			for i := range predeployAddresses {
+				predeployAddress := common.HexToAddress(predeployAddresses[i])
+				predeployCode := common.FromHex(predeployCodes[i])
+				balance := new(big.Int)
+				balance.SetString(predeployBalances[i], 10)
+
+				nonce, err := strconv.ParseUint(predeployNonces[i], 10, 64) // convert string to uint64
+				if err != nil {
+					cmd.PrintErrf("failed to nonce to uint64 %v\n", err)
+				}
+
+				g.AddPredeploy(gen, predeployAddress, predeployCode, nil, balance, nonce)
+			}
+			g.WriteFileToJSON(gen, outputFileName)
+		},
+	}
+
+	cmd.Flags().StringSliceVarP(&accountAddresses, "account", "a", []string{}, "Account address to add")
+	cmd.Flags().StringSliceVarP(&accountBalances, "balance", "b", []string{}, "Balance for the account")
+	cmd.Flags().StringSliceVarP(&predeployAddresses, "predeployAddress", "p", []string{}, "Predeploy contract addresses")
+	cmd.Flags().StringSliceVarP(&predeployCodes, "code", "c", []string{}, "Codes for the predeploy contract")
+	cmd.Flags().StringSliceVarP(&predeployBalances, "predeploybalance", "i", []string{}, "Balances for the predeploy contract")
+	cmd.Flags().StringSliceVarP(&predeployNonces, "nonce", "n", []string{}, "Nonces for the predeploy contracts")
+	cmd.Flags().StringVarP(&genesisFormat, "format", "f", "geth", "Format of the genesis file (geth or nethermind)")
+	cmd.Flags().StringVarP(&outputFileName, "output", "o", "genesis-go-ethereum.json", "Output file name for the genesis file")
+
+	return cmd
+}
+
+func main() {
+	err := CreateGenesisFileCmd().Execute()
+	if err != nil {
+		return
+	}
+}
