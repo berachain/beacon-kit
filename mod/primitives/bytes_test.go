0bbeb40453608db442107323d0dd48e6f314416a
itsdevbear <itsdevbear@berachain.com>
2024-04-14 22:19:53 -0400

    feat(bytes): Tests through Bytes48

--- mod/primitives/bytes_test.go
+++ mod/primitives/bytes_test.go
@@ -23,9 +23,11 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
+//nolint:lll // long hex strings.
 package primitives_test
 
 import (
+	"encoding/json"
 	"reflect"
 	"testing"
 
@@ -188,8 +190,7 @@ func TestBytes32UnmarshalText(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "valid input",
-
+			name:  "valid input",
 			input: "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20",
 			want: primitives.Bytes32{
 				0x01,
@@ -227,8 +228,13 @@ func TestBytes32UnmarshalText(t *testing.T) {
 			},
 		},
 		{
+			name:    "invalid input - not hex",
+			input:   "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20",
+			wantErr: true,
+		},
+		{
 			name:    "invalid input - wrong length",
-			input:   "0x01020304",
+			input:   "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
 			wantErr: true,
 		},
 	}
@@ -252,6 +258,326 @@ func TestBytes32UnmarshalText(t *testing.T) {
 	}
 }
 
+func TestBytes32UnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    primitives.Bytes32
+		wantErr bool
+	}{
+		{
+			name:  "valid input",
+			input: `"0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20"`,
+			want: primitives.Bytes32{
+				0x01,
+				0x02,
+				0x03,
+				0x04,
+				0x05,
+				0x06,
+				0x07,
+				0x08,
+				0x09,
+				0x0a,
+				0x0b,
+				0x0c,
+				0x0d,
+				0x0e,
+				0x0f,
+				0x10,
+				0x11,
+				0x12,
+				0x13,
+				0x14,
+				0x15,
+				0x16,
+				0x17,
+				0x18,
+				0x19,
+				0x1a,
+				0x1b,
+				0x1c,
+				0x1d,
+				0x1e,
+				0x1f,
+				0x20,
+			},
+		},
+		{
+			name:    "invalid input - not hex",
+			input:   `"0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20"`,
+			wantErr: true,
+		},
+		{
+			name:    "invalid input - wrong length",
+			input:   `"0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"`,
+			wantErr: true,
+		},
+		{
+			name:    "invalid input - extra characters",
+			input:   `"0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122"`,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got primitives.Bytes32
+			err := json.Unmarshal([]byte(tt.input), &got)
+			if (err != nil) != tt.wantErr {
+				t.Errorf(
+					"Bytes32.UnmarshalJSON() error = %v, wantErr %v",
+					err,
+					tt.wantErr,
+				)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Bytes32.UnmarshalJSON() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBytes32MarshalText(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   primitives.Bytes32
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "valid input",
+			input: primitives.Bytes32{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+				0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12,
+				0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
+				0x1e, 0x1f, 0x20},
+			want: "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20",
+		},
+		{
+			name:  "empty input",
+			input: primitives.Bytes32{},
+			want:  "0x0000000000000000000000000000000000000000000000000000000000000000",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.input.MarshalText()
+			if (err != nil) != tt.wantErr {
+				t.Errorf(
+					"Bytes32.MarshalText() error = %v, wantErr %v",
+					err,
+					tt.wantErr,
+				)
+				return
+			}
+			if string(got) != tt.want {
+				t.Errorf(
+					"Bytes32.MarshalText() = %v, want %v",
+					string(got),
+					tt.want,
+				)
+			}
+		})
+	}
+}
+
+func TestBytes32String(t *testing.T) {
+	tests := []struct {
+		name  string
+		input primitives.Bytes32
+		want  string
+	}{
+		{
+			name: "valid input",
+			input: primitives.Bytes32{0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+				0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
+				0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a,
+				0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20},
+			want: "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20",
+		},
+		{
+			name:  "empty input",
+			input: primitives.Bytes32{},
+			want:  "0x0000000000000000000000000000000000000000000000000000000000000000",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.input.String(); got != tt.want {
+				t.Errorf("Bytes32.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestBytes48String(t *testing.T) {
+	tests := []struct {
+		name  string
+		input primitives.Bytes48
+		want  string
+	}{
+		{
+			name: "valid input",
+			input: primitives.Bytes48{
+				0x01,
+				0x02,
+				0x03,
+				0x04,
+				0x05,
+				0x06,
+				0x07,
+				0x08,
+				0x09,
+				0x0a,
+				0x0b,
+				0x0c,
+				0x0d,
+				0x0e,
+				0x0f,
+				0x10,
+				0x11,
+				0x12,
+				0x13,
+				0x14,
+				0x15,
+				0x16,
+				0x17,
+				0x18,
+				0x19,
+				0x1a,
+				0x1b,
+				0x1c,
+				0x1d,
+				0x1e,
+				0x1f,
+				0x20,
+				0x21,
+				0x22,
+				0x23,
+				0x24,
+				0x25,
+				0x26,
+				0x27,
+				0x28,
+				0x29,
+				0x2a,
+				0x2b,
+				0x2c,
+				0x2d,
+				0x2e,
+				0x2f,
+				0x30,
+			},
+			want: "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f30",
+		},
+		{
+			name:  "empty input",
+			input: primitives.Bytes48{},
+			want:  "0x000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.input.String(); got != tt.want {
+				t.Errorf("Bytes48.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBytes48MarshalText(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   primitives.Bytes48
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "valid input",
+			input: primitives.Bytes48{
+				0x01,
+				0x02,
+				0x03,
+				0x04,
+				0x05,
+				0x06,
+				0x07,
+				0x08,
+				0x09,
+				0x0a,
+				0x0b,
+				0x0c,
+				0x0d,
+				0x0e,
+				0x0f,
+				0x10,
+				0x11,
+				0x12,
+				0x13,
+				0x14,
+				0x15,
+				0x16,
+				0x17,
+				0x18,
+				0x19,
+				0x1a,
+				0x1b,
+				0x1c,
+				0x1d,
+				0x1e,
+				0x1f,
+				0x20,
+				0x21,
+				0x22,
+				0x23,
+				0x24,
+				0x25,
+				0x26,
+				0x27,
+				0x28,
+				0x29,
+				0x2a,
+				0x2b,
+				0x2c,
+				0x2d,
+				0x2e,
+				0x2f,
+				0x30,
+			},
+			want: "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f30",
+		},
+		{
+			name:  "empty input",
+			input: primitives.Bytes48{},
+			want:  "0x000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.input.MarshalText()
+			if (err != nil) != tt.wantErr {
+				t.Errorf(
+					"Bytes48.MarshalText() error = %v, wantErr %v",
+					err,
+					tt.wantErr,
+				)
+				return
+			}
+			if string(got) != tt.want {
+				t.Errorf(
+					"Bytes48.MarshalText() = %s, want %s",
+					string(got),
+					tt.want,
+				)
+			}
+		})
+	}
+}
+
 func TestBytes48UnmarshalText(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -260,9 +586,8 @@ func TestBytes48UnmarshalText(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "valid input",
-			//nolint:lll
-			input: "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829302b2c2d2e2f30",
+			name:  "valid input",
+			input: "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f30",
 			want: primitives.Bytes48{
 				0x01,
 				0x02,
@@ -305,7 +630,7 @@ func TestBytes48UnmarshalText(t *testing.T) {
 				0x27,
 				0x28,
 				0x29,
-				0x30,
+				0x2a,
 				0x2b,
 				0x2c,
 				0x2d,
@@ -315,8 +640,13 @@ func TestBytes48UnmarshalText(t *testing.T) {
 			},
 		},
 		{
+			name:    "invalid input - not hex",
+			input:   "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f30",
+			wantErr: true,
+		},
+		{
 			name:    "invalid input - wrong length",
-			input:   "0x01020304",
+			input:   "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e",
 			wantErr: true,
 		},
 	}
@@ -335,85 +665,6 @@ func TestBytes48UnmarshalText(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Bytes48.UnmarshalText() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-// Test for String() method.
-func TestBytes48String(t *testing.T) {
-	tests := []struct {
-		name string
-		h    primitives.Bytes48
-		want string
-	}{
-		{
-			name: "non-empty bytes",
-			h: primitives.Bytes48{
-				0x01,
-				0x02,
-				0x03,
-				0x04,
-				0x05,
-				0x06,
-				0x07,
-				0x08,
-				0x09,
-				0x0a,
-				0x0b,
-				0x0c,
-				0x0d,
-				0x0e,
-				0x0f,
-				0x10,
-				0x11,
-				0x12,
-				0x13,
-				0x14,
-				0x15,
-				0x16,
-				0x17,
-				0x18,
-				0x19,
-				0x1a,
-				0x1b,
-				0x1c,
-				0x1d,
-				0x1e,
-				0x1f,
-				0x20,
-				0x21,
-				0x22,
-				0x23,
-				0x24,
-				0x25,
-				0x26,
-				0x27,
-				0x28,
-				0x29,
-				0x30,
-				0x2b,
-				0x2c,
-				0x2d,
-				0x2e,
-				0x2f,
-				0x30,
-			},
-			//nolint:lll
-			want: "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829302b2c2d2e2f30",
-		},
-		{
-			name: "empty bytes",
-			h:    primitives.Bytes48{},
-			//nolint:lll
-			want: "0x000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.h.String(); got != tt.want {
-				t.Errorf("Bytes48.String() = %v, want %v", got, tt.want)
 			}
 		})
 	}
