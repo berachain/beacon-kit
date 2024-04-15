1caae2e4f1dff9bc695647d1e85ce36b93fe662a
itsdevbear <itsdevbear@berachain.com>
2024-04-15 18:09:34 -0400

    bet

--- mod/config/params/config.go
+++ mod/config/params/config.go
@@ -39,6 +39,8 @@ type ChainSpec struct {
 	// MaxEffectiveBalance is the maximum effective balance allowed for a
 	// validator.
 	MaxEffectiveBalance uint64 `mapstructure:"max-effective-balance"`
+	// EjectionBalance is the balance at which a validator is ejected.
+	EjectionBalance uint64 `mapstructure:"ejection-balance"`
 	// EffectiveBalanceIncrement is the effective balance increment.
 	EffectiveBalanceIncrement uint64 `mapstructure:"effective-balance-increment"`
 
