f9e36f00934d20092f54218d9cbd66cb69f50031
itsdevbear <itsdevbear@berachain.com>
2024-04-17 22:06:47 -0400

    bet

--- mod/ssz/merkleize.go
+++ mod/ssz/merkleize.go
@@ -26,13 +26,80 @@
 package ssz
 
 import (
+	"fmt"
+
 	"github.com/berachain/beacon-kit/mod/merkle"
+	"github.com/berachain/beacon-kit/mod/merkle/zero"
+	"github.com/berachain/beacon-kit/mod/primitives"
 	"github.com/berachain/beacon-kit/mod/primitives/constants"
 )
 
 // Merkleize hashes a list of chunks and returns the HTR of the list of.
-func Merkleize(chunks [][32]byte, limit uint64) ([32]byte, error) {
-	return merkle.NewRootWithMaxLeaves[[32]byte, [32]byte](chunks, limit)
+// As per the Ethereum 2.0 SSZ Specifcation:
+// https://github.com/ethereum/consensus-specs/blob/dev/ssz/simple-serialize.md#merkleization
+//
+//nolint:lll
+func Merkleize[ChunkT, RootT ~[32]byte](
+	chunks []ChunkT,
+	limit ...uint64,
+) (RootT, error) {
+	var effectiveLimit primitives.U64
+	if len(limit) == 0 {
+		effectiveLimit = primitives.U64(len(chunks))
+	} else {
+		limit := limit[0]
+		if limit < uint64(len(chunks)) {
+			return RootT{}, fmt.Errorf("input exceeds limit")
+		}
+		effectiveLimit = primitives.U64(limit)
+	}
+
+	if effectiveLimit == 0 {
+		return zero.Hashes[0], nil
+	} else if effectiveLimit == 1 {
+		if len(chunks) == 0 {
+			return zero.Hashes[0], nil
+		}
+		return RootT(chunks[0]), nil
+	}
+
+	paddedChunks := make([]ChunkT, effectiveLimit)
+	copy(paddedChunks, chunks)
+	for i := len(chunks); i < len(paddedChunks); i++ {
+		paddedChunks[i] = zero.Hashes[0]
+	}
+
+	tree, err := merkle.NewTreeWithMaxLeaves[ChunkT, RootT](paddedChunks, uint64(effectiveLimit))
+	if err != nil {
+		return RootT{}, err
+	}
+	return tree.Root(), nil
+}
+
+// Pack packs a list of SSZ-marshallable elements into a single byte slice.
+func Pack[S Marshallable](s []S) ([]byte, error) {
+	// Pack each element into a single buffer.
+	buf := make([]byte, 0)
+	for _, el := range s {
+		packed, err := el.MarshalSSZ()
+		if err != nil {
+			return nil, err
+		}
+		buf = append(buf, packed...)
+	}
+
+	// Right pad the buffer to ensure it is a multiple of 32 bytes
+	paddingSize := 32 - (len(buf) % 32)
+	if paddingSize > 0 {
+		padding := make([]byte, paddingSize)
+		buf = append(buf, padding...)
+	}
+	return buf, nil
+}
+
+// MixinLength mixes the length into the root.
+func MixinLength(root [32]byte, length uint64) [32]byte {
+	return merkle.MixinLength(root, length)
 }
 
 // MerkleizeByteSlice hashes a byteslice by chunkifying it and returning the
@@ -47,10 +114,7 @@ func MerkleizeByteSlice(input []byte) ([32]byte, error) {
 	for i := range chunks {
 		copy(chunks[i][:], input[32*i:])
 	}
-	return merkle.NewRootWithMaxLeaves[[32]byte, [32]byte](
-		chunks,
-		numChunks,
-	)
+	return Merkleize[[32]byte, [32]byte](chunks)
 }
 
 // MerkleizeList hashes each element in the list and then returns the HTR of
@@ -78,5 +142,5 @@ func MerkleizeVector[T Hashable[[32]byte]](
 			return [32]byte{}, err
 		}
 	}
-	return merkle.NewRootWithMaxLeaves[[32]byte, [32]byte](roots, length)
+	return Merkleize[[32]byte, [32]byte](roots, length)
 }
