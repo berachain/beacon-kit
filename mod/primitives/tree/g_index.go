d48f2aa7b40b822f54cdf0648424f71377b82662
itsdevbear <itsdevbear@berachain.com>
2024-04-19 18:18:40 -0400

    bet

--- mod/primitives/tree/g_index.go
+++ mod/primitives/tree/g_index.go
@@ -1,21 +1,53 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2024 Berachain Foundation
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 package tree
 
 import "math"
 
 type GeneralizedIndex = int
 
-// Usage note: functions outside this section should manipulate generalized indices using only functions inside this section. This is to make it easier for developers to implement generalized indices with underlying representations other than bigints.
+// Usage note: functions outside this section should manipulate generalized
+// indices using only functions inside this section. This is to make it easier
+// for developers to implement generalized indices with underlying
+// representations other than bigints.
 
-// concatGeneralizedIndices concatenates multiple generalized indices into a single generalized index.
+// concatGeneralizedIndices concatenates multiple generalized indices into a
+// single generalized index.
 func ConcatGeneralizedIndices(indices ...GeneralizedIndex) GeneralizedIndex {
 	o := GeneralizedIndex(1)
 	for _, i := range indices {
-		o = GeneralizedIndex(o*GetPowerOfTwoFloor(i) + (i - GetPowerOfTwoFloor(i)))
+		o = GeneralizedIndex(
+			o*GetPowerOfTwoFloor(i) + (i - GetPowerOfTwoFloor(i)),
+		)
 	}
 	return o
 }
 
-// GetGeneralizedIndexLength returns the length of a path represented by a generalized index.
+// GetGeneralizedIndexLength returns the length of a path represented by a
+// generalized index.
 func GetGeneralizedIndexLength(index GeneralizedIndex) int {
 	return int(math.Log2(float64(index)))
 }
@@ -30,8 +62,12 @@ func GeneralizedIndexSibling(index GeneralizedIndex) GeneralizedIndex {
 	return GeneralizedIndex(index ^ 1)
 }
 
-// GeneralizedIndexChild returns the child index of a given generalized index, specifying if it's the right child.
-func GeneralizedIndexChild(index GeneralizedIndex, rightSide bool) GeneralizedIndex {
+// GeneralizedIndexChild returns the child index of a given generalized index,
+// specifying if it's the right child.
+func GeneralizedIndexChild(
+	index GeneralizedIndex,
+	rightSide bool,
+) GeneralizedIndex {
 	if rightSide {
 		return GeneralizedIndex(index*2 + 1)
 	}
--- mod/primitives/tree/proof.go
+++ mod/primitives/tree/proof.go
@@ -1,3 +1,28 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2024 Berachain Foundation
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 package tree
 
 import (
@@ -7,13 +32,17 @@ import (
 	"github.com/berachain/beacon-kit/mod/primitives"
 )
 
-// GetBranchIndices returns the generalized indices of the sister chunks along the path from the chunk with the
+// GetBranchIndices returns the generalized indices of the sister chunks along
+// the path from the chunk with the
 // given tree index to the root.
 func GetBranchIndices(treeIndex GeneralizedIndex) []GeneralizedIndex {
 	var o []GeneralizedIndex
 	o = append(o, GeneralizedIndexSibling(treeIndex))
 	for o[len(o)-1] > 1 {
-		o = append(o, GeneralizedIndexSibling(generalizedIndexParent(o[len(o)-1])))
+		o = append(
+			o,
+			GeneralizedIndexSibling(generalizedIndexParent(o[len(o)-1])),
+		)
 	}
 	if len(o) > 1 {
 		return o[:len(o)-1]
@@ -21,7 +50,8 @@ func GetBranchIndices(treeIndex GeneralizedIndex) []GeneralizedIndex {
 	return o
 }
 
-// GetPathIndices returns the generalized indices of the chunks along the path from the chunk with the
+// GetPathIndices returns the generalized indices of the chunks along the path
+// from the chunk with the
 // given tree index to the root.
 func GetPathIndices(treeIndex GeneralizedIndex) []GeneralizedIndex {
 	var o []GeneralizedIndex
@@ -35,9 +65,11 @@ func GetPathIndices(treeIndex GeneralizedIndex) []GeneralizedIndex {
 	return o
 }
 
-// GetHelperIndices returns the generalized indices of all "extra" chunks in the tree needed to prove the chunks with the given
-// generalized indices. Note that the decreasing order is chosen deliberately to ensure equivalence to the
-// order of hashes in a regular single-item Merkle proof in the single-item case.
+// GetHelperIndices returns the generalized indices of all "extra" chunks in the
+// tree needed to prove the chunks with the given generalized indices. Note that
+// the decreasing order is chosen deliberately to ensure equivalence to the
+// order of hashes in a regular single-item Merkle proof in the single-item
+// case.
 func GetHelperIndices(indices []GeneralizedIndex) []GeneralizedIndex {
 	allHelperIndices := make(map[GeneralizedIndex]struct{})
 	allPathIndices := make(map[GeneralizedIndex]struct{})
@@ -65,8 +97,13 @@ func GetHelperIndices(indices []GeneralizedIndex) []GeneralizedIndex {
 	return diff
 }
 
-// calculateMerkleRoot calculates the Merkle root from a leaf and a proof based on the generalized index.
-func calculateMerkleRoot(leaf primitives.Bytes32, proof []primitives.Bytes32, index GeneralizedIndex) primitives.Bytes32 {
+// CalculateMerkleRoot calculates the Merkle root from a leaf and a proof based
+// on the generalized index.
+func CalculateMerkleRoot(
+	leaf primitives.Bytes32,
+	proof []primitives.Bytes32,
+	index GeneralizedIndex,
+) primitives.Bytes32 {
 	if len(proof) != GetGeneralizedIndexLength(index) {
 		panic("proof length does not match the expected length from index")
 	}
@@ -80,20 +117,31 @@ func calculateMerkleRoot(leaf primitives.Bytes32, proof []primitives.Bytes32, in
 	return leaf
 }
 
-// verifyMerkleProof verifies a Merkle proof for a single leaf and a given root.
-func verifyMerkleProof(leaf primitives.Bytes32, proof []primitives.Bytes32, index GeneralizedIndex, root primitives.Bytes32) bool {
-	return calculateMerkleRoot(leaf, proof, index) == root
+// VerifyMerkleProof verifies a Merkle proof for a single leaf and a given root.
+func VerifyMerkleProof(
+	leaf primitives.Bytes32,
+	proof []primitives.Bytes32,
+	index GeneralizedIndex,
+	root primitives.Bytes32,
+) bool {
+	return CalculateMerkleRoot(leaf, proof, index) == root
 }
 
-// calculateMultiMerkleRoot calculates the Merkle root for multiple leaves with a given proof and indices.
-func calculateMultiMerkleRoot(leaves []primitives.Bytes32, proof []primitives.Bytes32, indices []GeneralizedIndex) primitives.Bytes32 {
+// CalculateMultiMerkleRoot calculates the Merkle root for multiple leaves with
+// a given proof and indices.
+func CalculateMultiMerkleRoot(
+	leaves []primitives.Bytes32,
+	proof []primitives.Bytes32,
+	indices []GeneralizedIndex,
+) primitives.Bytes32 {
 	if len(leaves) != len(indices) {
-		panic("leaves and indices length mismatch")
+		panic("number of leaves does not match number of indices")
 	}
 	helperIndices := GetHelperIndices(indices)
 	if len(proof) != len(helperIndices) {
 		panic("proof length does not match helper indices length")
 	}
+
 	objects := make(map[GeneralizedIndex]primitives.Bytes32)
 	for i, leaf := range leaves {
 		objects[indices[i]] = leaf
@@ -113,27 +161,28 @@ func calculateMultiMerkleRoot(leaves []primitives.Bytes32, proof []primitives.By
 	pos := 0
 	for pos < len(keys) {
 		k := keys[pos]
-		if _, ok := objects[k]; ok && (objects[k^1] != primitives.Bytes32{}) && objects[k/2] == (primitives.Bytes32{}) {
-			// Assuming left child is k|1^1 and right child is k|1
-			leftIndex := GeneralizedIndex(k | 1 ^ 1)
-			rightIndex := GeneralizedIndex(k | 1)
-			leftChild := objects[leftIndex]
-			rightChild := objects[rightIndex]
-
-			// Hashing the concatenation of left and right child data
+		if _, exists := objects[k]; exists &&
+			objects[GeneralizedIndex(k^1)] != (primitives.Bytes32{}) &&
+			objects[GeneralizedIndex(k/2)] == (primitives.Bytes32{}) {
+			leftChild := objects[GeneralizedIndex((k|1)^1)]
+			rightChild := objects[GeneralizedIndex(k|1)]
 			hashed := sha256.Sum256(append(leftChild[:], rightChild[:]...))
 			parentIndex := GeneralizedIndex(k / 2)
 			objects[parentIndex] = hashed
-
-			// Adding the parent index to keys
 			keys = append(keys, parentIndex)
 		}
 		pos++
 	}
-	return objects[1]
+	return objects[GeneralizedIndex(1)]
 }
 
-// verifyMerkleMultiproof verifies a Merkle multiproof for multiple leaves and a given root.
-func verifyMerkleMultiproof(leaves []primitives.Bytes32, proof []primitives.Bytes32, indices []GeneralizedIndex, root primitives.Bytes32) bool {
-	return calculateMultiMerkleRoot(leaves, proof, indices) == root
+// VerifyMerkleMultiproof verifies a Merkle multiproof for multiple leaves and a
+// given root.
+func VerifyMerkleMultiproof(
+	leaves []primitives.Bytes32,
+	proof []primitives.Bytes32,
+	indices []GeneralizedIndex,
+	root primitives.Bytes32,
+) bool {
+	return CalculateMultiMerkleRoot(leaves, proof, indices) == root
 }
