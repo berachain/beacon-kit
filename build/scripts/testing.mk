fbe8e808d3e138304a53e4a73cce9c0c256d63d0
nidhi-singh02 <trippin@berachain.com>
2024-04-22 22:08:30 +0530

    add ethereumjs as EL
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>

--- build/scripts/testing.mk
+++ build/scripts/testing.mk
@@ -122,6 +122,58 @@ start-erigon: ## start an ephemeral `erigon` node
 	--db.size.limit	3000MB \
 	--datadir .tmp/erigon
 
+start-ethereumjs:
+	docker run \
+	--rm -v $(PWD)/${TESTAPP_DIR}:/${TESTAPP_DIR} \
+	-v $(PWD)/.tmp:/.tmp \
+	-v $(PWD)/beacond/eth-genesis.json:/.tmp/beacond/eth-genesis.json \
+	-p 30303:30303 \
+	-p 8545:8545 \
+	-p 8551:8551 \
+	ethereumjs:nidhi \
+	--gethGenesis /.tmp/beacond/eth-genesis.json \
+	--rpcEngine \
+	--rpc \
+	--rpcEngineAddr 0.0.0.0 \
+	--rpcEnginePort 8551 \
+	--jwtSecret ../../$(JWT_PATH) \
+	--logLevel debug \
+	--rpcDebugVerbose \
+	--rpcCors * 
+
+# --startExecution \
+# --startExecutionFrom 0
+
+
+# --gethGenesis /usr/app/.tmp/beacond/eth-genesis.json \
+#--dataDir .tmp/ethereumjs \
+	ethpandaops/ethereumjs:master-46d09ca \
+# start-ethereumjs:
+# 	rm -rf .tmp/ethereumjs
+# 	docker run \
+# 	--rm -v $(PWD)/${TESTAPP_DIR}:/${TESTAPP_DIR} \
+# 	-v $(PWD)/.tmp:/.tmp \
+# 	-v $(PWD)/beacond/eth-genesis.json:/usr/app/.tmp/beacond/eth-genesis.json \
+# 	-p 30303:30303 \
+# 	-p 8545:8545 \
+# 	-p 8551:8551 \
+# 	ethpandaops/ethereumjs:stable \
+# 	--dataDir .tmp/ethereumjs \
+# 	--jwtSecret ../../$(JWT_PATH) \
+# 	--gethGenesis /usr/app/.tmp/beacond/eth-genesis.json \
+# 	--rpcEngine \
+# 	--port 30303 \
+# 	--ws \
+# 	--rpc \
+# 	--rpcApi eth,net,engine \
+# 	--logLevel debug \
+# 	--maxPeers 50 \
+# 	--init /usr/app/.tmp/beacond/eth-genesis.json \
+# 	--networkId 80087
+
+	# -v $(PWD)/${ETH_GENESIS_PATH}:/.tmp/${ETH_GENESIS_PATH} \
+	# --gethGenesis .tmp/${ETH_GENESIS_PATH} \
+	
 SHORT_FUZZ_TIME=10s
 MEDIUM_FUZZ_TIME=30s
 LONG_FUZZ_TIME=3m
