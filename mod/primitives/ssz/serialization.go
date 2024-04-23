34db94bb53afc008d899832dac839d572fdf3a5d
Chibera bera <chibera@berachain.com>
2024-04-23 14:12:15 -0400

    docs

--- mod/primitives/ssz/serialization.go
+++ mod/primitives/ssz/serialization.go
@@ -149,7 +149,8 @@ func MarshalBitVector(bv []bool) []byte {
 // MarshalBitList converts a slice of boolean values into a byte slice where each bit represents a boolean value, with an additional bit set at the end.
 // Note that from the offset coding, the length (in bytes) of the bitlist is known. An additional 1 bit is added to the end, at index e where e is the length of the bitlist (not the limit), so that the length in bits will also be known.
 func MarshalBitList(bv []bool) []byte {
-	array := make([]byte, (len(bv)/8)+1) // Allocate enough bytes to represent the bit list, plus one for the end bit.
+	// Allocate enough bytes to represent the bit list, plus one for the end bit.
+	array := make([]byte, (len(bv)/8)+1)
 	for i, val := range bv {
 		if val {
 			// Set the bit at the appropriate position if the boolean is true.
