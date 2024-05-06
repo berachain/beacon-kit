106eff31925dba0990f5be288fe3de7c16cd89fe
Chibera bera <chibera@berachain.com>
2024-05-06 12:59:00 -0400

    disable nilaway in spectest

--- mod/primitives/pkg/ssz/v2/spec_test.go
+++ mod/primitives/pkg/ssz/v2/spec_test.go
@@ -22,6 +22,8 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
+//
+//nolint:nilaway // do not check for nil returns in a test file.
 package ssz_test
 
 import (
