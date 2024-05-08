056a8ed49a328f4432e9c957b1b7691b4b3d4106
Chibera bera <chibera@berachain.com>
2024-05-08 17:02:17 -0400

    feat(ssz): Wrapper class to convert struct to same iface as fastssz

--- mod/primitives/pkg/ssz/v2/serializer.go
+++ mod/primitives/pkg/ssz/v2/serializer.go
@@ -70,8 +70,8 @@ type Serializer struct {
 }
 
 type ISerializer interface {
-	Elements(s GenericSSZType) []GenericSSZType
-	MarshalSSZ(s GenericSSZType) ([]byte, error)
+	// Elements(s GenericSSZType) []GenericSSZType
+	// MarshalSSZ(s GenericSSZType) ([]byte, error)
 }
 
 func NewSerializer() Serializer {
@@ -177,23 +177,34 @@ func (s *Serializer) Marshal(
 	return offset, err
 }
 
-func (s *Serializer) MarshalToDefaultBuffer(
+func (s *Serializer) GetSize(
 	val reflect.Value,
 	typ reflect.Type,
-	cb func(reflect.Value, reflect.Type, []byte, uint64) (uint64, error),
-) ([]byte, error) {
+) (int, error) {
 	aLen := 0
-	err := errors.New("MarshalToDefaultBuffer Failure")
+	err := errors.New("GetSize Failure")
 	switch {
 	case val.Kind() == reflect.Ptr && typ.Elem().Kind() == reflect.Struct:
 		aLen, err = CalculateBufferSizeForStruct(val)
 		if err != nil {
-			return nil, err
+			return 0, err
 		}
 	case val.Kind() == reflect.Array || val.Kind() == reflect.Slice:
 		aLen = GetNestedArrayLength(val)
 	default:
 		aLen = val.Len()
+	}
+	return aLen, nil
+}
+
+func (s *Serializer) MarshalToDefaultBuffer(
+	val reflect.Value,
+	typ reflect.Type,
+	cb func(reflect.Value, reflect.Type, []byte, uint64) (uint64, error),
+) ([]byte, error) {
+	aLen, err := s.GetSize(val, typ)
+	if err != nil {
+		return nil, err
 	}
 	buf := make([]byte, aLen)
 	_, err = cb(val, typ, buf, 0)
