acd3d0942d5f58fbeb922a444a2e69b252e1a309
itsdevbear <itsdevbear@berachain.com>
2024-05-09 15:42:00 -0400

    fix(blockchain): increase blocktime for payload build test

--- mod/beacon/blockchain/execution_engine.go
+++ mod/beacon/blockchain/execution_engine.go
@@ -147,7 +147,7 @@ func (s *Service[BlobSidecarsT]) sendPostBlockFCU(
 			slot+1,
 			//#nosec:G701 // won't realistically overflow.
 			// TODO: clock time properly.
-			uint64(time.Now().Unix()),
+			uint64(time.Now().Unix()+1),
 			root,
 			headHash,
 		); err == nil {
