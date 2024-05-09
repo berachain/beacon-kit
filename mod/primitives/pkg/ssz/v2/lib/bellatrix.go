8202d7dfcf2c90704852117760a128db0e9fb8a1
Chibera bera <chibera@berachain.com>
2024-05-09 16:28:01 -0400

    bet

--- mod/primitives/pkg/ssz/v2/lib/bellatrix.go
+++ mod/primitives/pkg/ssz/v2/lib/bellatrix.go
@@ -83,7 +83,6 @@ type ExecutionPayloadHeader struct {
 }
 
 //go:generate go run github.com/ferranbt/fastssz/sszgen -path bellatrix.go -objs BeaconStateBellatrix -include ../../../../pkg/crypto,../../../../pkg/common,../../../../pkg/bytes,../../../constants,../../../../pkg,../../../../pkg/math -output bellatrix.ssz.go
-
 type BeaconStateBellatrix struct {
 	GenesisTime                  uint64                  `json:"genesis_time"`
 	GenesisValidatorsRoot        []byte                  `json:"genesis_validators_root"         ssz-size:"32"`
