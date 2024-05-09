9b47f9dcd91f75ebdb87039c25107f338008c4b1
Cal Bera <calbera@berachain.com>
2024-05-09 19:48:55 -0400

    fix sed command

--- testing/op/deploy.sh
+++ testing/op/deploy.sh
@@ -36,14 +36,25 @@ cd ~/op-stack-deployment/optimism
 cp .envrc.example .envrc # overwrites any existing .envrc variables
 direnv allow
 
+# Update the .envrc file with the L1 block time
+awk -v blockTime="$BLOCK_TIME" '
+$0 ~ /^export L1_RPC_KIND=/ {
+  print $0 "\n"  # Print the line
+  # Print the new setting, add a newline after
+  print "\n# The block time on the L1 chain.\nexport L1_BLOCK_TIME=" blockTime "\n"
+  next  # Skip to the next record to avoid reprinting this line
+}
+1' .envrc > .envrc.new && mv .envrc.new .envrc
+direnv allow
+
 # Update the PRIVATE_KEY, L1_RPC_URL, and L1_RPC_KIND in the .envrc file
 if sed --version 2>&1 | grep -q GNU; then
   sed -i 's/^export PRIVATE_KEY=.*/export PRIVATE_KEY='"$PRIV_KEY"'/' .envrc
-  sed -i 's/^export L1_RPC_URL=.*/export L1_RPC_URL='"$RPC_URL"'/' .envrc
+  sed -i 's|^export L1_RPC_URL=.*|export L1_RPC_URL='"$RPC_URL"'|' .envrc
   sed -i 's/^export L1_RPC_KIND=.*/export L1_RPC_KIND='"$RPC_KIND"'/' .envrc
 else 
   sed -i '' 's/^export PRIVATE_KEY=.*/export PRIVATE_KEY='"$PRIV_KEY"'/' .envrc
-  sed -i '' 's/^export L1_RPC_URL=.*/export L1_RPC_URL='"$RPC_URL"'/' .envrc
+  sed -i '' 's|^export L1_RPC_URL=.*|export L1_RPC_URL='"$RPC_URL"'|' .envrc
   sed -i '' 's/^export L1_RPC_KIND=.*/export L1_RPC_KIND='"$RPC_KIND"'/' .envrc
 fi
 direnv allow
@@ -74,17 +85,6 @@ echo "$wallets" | while IFS= read -r line; do
   fi
 done
 direnv allow 
-
-# Update the .envrc file with the L1 block time
-awk -v blockTime="$BLOCK_TIME" '
-$0 ~ /^export L1_RPC_KIND=/ {
-  print $0 "\n"  # Print the line
-  # Print the new setting, add a newline after
-  print "\n# The block time on the L1 chain.\nexport L1_BLOCK_TIME=" blockTime "\n"
-  next  # Skip to the next record to avoid reprinting this line
-}
-1' .envrc > .envrc.new && mv .envrc.new .envrc
-direnv allow
 
 # Source the updated env variables
 source .envrc
