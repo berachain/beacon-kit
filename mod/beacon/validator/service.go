3853a508a75cf49213979c75b3cf722531cbcbf8
itsdevbear <itsdevbear@berachain.com>
2024-05-14 17:31:33 -0400

    bet

--- mod/beacon/validator/service.go
+++ mod/beacon/validator/service.go
@@ -27,6 +27,7 @@ package validator
 
 import (
 	"context"
+	"time"
 
 	"github.com/berachain/beacon-kit/mod/consensus-types/pkg/types"
 	"github.com/berachain/beacon-kit/mod/errors"
@@ -139,8 +140,23 @@ func (s *Service[BeaconStateT, BlobSidecarsT]) RequestBestBlock(
 	ctx context.Context,
 	requestedSlot math.Slot,
 ) (types.BeaconBlock, BlobSidecarsT, error) {
-	var sidecars BlobSidecarsT
-	s.logger.Info("our turn to propose a block 🙈", "slot", requestedSlot)
+	var (
+		sidecars  BlobSidecarsT
+		startTime = time.Now()
+		g, gCtx   = errgroup.WithContext(ctx)
+	)
+
+	s.logger.Info("requesting beacon block assembly 🙈", "slot", requestedSlot)
+	defer func() {
+		s.logger.Info(
+			"finished beacon block assembly 🛟",
+			"slot",
+			requestedSlot,
+			"duration",
+			time.Since(startTime).String(),
+		)
+	}()
+
 	// The goal here is to acquire a payload whose parent is the previously
 	// finalized block, such that, if this payload is accepted, it will be
 	// the next finalized block in the chain. A byproduct of this design
@@ -186,13 +202,6 @@ func (s *Service[BeaconStateT, BlobSidecarsT]) RequestBestBlock(
 		)
 	}
 
-	// Build the reveal for the current slot.
-	// TODO: We can optimize to pre-compute this in parallel.
-	reveal, err := s.randaoProcessor.BuildReveal(st)
-	if err != nil {
-		return nil, sidecars, errors.Newf("failed to build reveal: %w", err)
-	}
-
 	// Create a new empty block from the current state.
 	blk, err := s.GetEmptyBeaconBlock(
 		st,
@@ -202,10 +211,41 @@ func (s *Service[BeaconStateT, BlobSidecarsT]) RequestBestBlock(
 		return nil, sidecars, err
 	}
 
+	// Get the payload for the block.
+	envelope, err := s.RetrievePayload(
+		ctx, st, blk,
+	)
+	if err != nil {
+		return blk, sidecars, errors.Newf(
+			"failed to get block root at index: %w",
+			err,
+		)
+	} else if envelope == nil {
+		return nil, sidecars, ErrNilPayload
+	}
+
+	// Process Blobs.
+	g.Go(func() error {
+		// Build the sidecars.
+		var sidecarErr error
+		sidecars, sidecarErr = s.BuildSidecars(
+			blk,
+			envelope.GetBlobsBundle(),
+		)
+		return sidecarErr
+	})
+
 	// Assemble a new block with the payload.
 	body := blk.GetBody()
 	if body.IsNil() {
 		return nil, sidecars, ErrNilBlkBody
+	}
+
+	// Build the reveal for the current slot.
+	// TODO: We can optimize to pre-compute this in parallel.
+	reveal, err := s.randaoProcessor.BuildReveal(st)
+	if err != nil {
+		return nil, sidecars, errors.Newf("failed to build reveal: %w", err)
 	}
 
 	// Set the reveal on the block body.
@@ -222,23 +262,15 @@ func (s *Service[BeaconStateT, BlobSidecarsT]) RequestBestBlock(
 	// Set the deposits on the block body.
 	body.SetDeposits(deposits)
 
-	// Get the payload for the block.
-	envelope, err := s.RetrievePayload(
-		ctx, st, blk,
-	)
-	if err != nil {
-		return blk, sidecars, errors.Newf(
-			"failed to get block root at index: %w",
-			err,
-		)
-	} else if envelope == nil {
-		return nil, sidecars, ErrNilPayload
+	// If we get returned a nil blobs bundle, we should return an error.
+	// TODO: allow external block builders to override the payload.
+	blobsBundle := envelope.GetBlobsBundle()
+	if blobsBundle == nil {
+		return nil, sidecars, ErrNilBlobsBundle
 	}
 
-	payload := envelope.GetExecutionPayload()
-	if payload == nil || payload.IsNil() {
-		return nil, sidecars, ErrNilPayload
-	}
+	// Set the KZG commitments on the block body.
+	body.SetBlobKzgCommitments(blobsBundle.GetCommitments())
 
 	// TODO: assemble real eth1data.
 	body.SetEth1Data(&types.Eth1Data{
@@ -247,49 +279,21 @@ func (s *Service[BeaconStateT, BlobSidecarsT]) RequestBestBlock(
 		BlockHash:    common.ExecutionHash{},
 	})
 
+	payload := envelope.GetExecutionPayload()
+	if payload == nil || payload.IsNil() {
+		return nil, sidecars, ErrNilPayload
+	}
+
 	// Set the execution data.
 	if err = body.SetExecutionData(payload); err != nil {
 		return nil, sidecars, err
 	}
-
-	// Parallelize the building of sidecars and block using errgroup.
-	var (
-		blobSidecars BlobSidecarsT
-
-		g, gCtx = errgroup.WithContext(ctx)
-	)
-	// Process Blobs.
-	g.Go(func() error {
-		// If we get returned a nil blobs bundle, we should return an error.
-		// TODO: allow external block builders to override the payload.
-		blobsBundle := envelope.GetBlobsBundle()
-		if blobsBundle == nil {
-			return ErrNilBlobsBundle
-		}
-
-		// Set the KZG commitments on the block body.
-		body.SetBlobKzgCommitments(blobsBundle.GetCommitments())
-
-		// Build the sidecars.
-		var sidecarErr error
-		blobSidecars, sidecarErr = s.BuildSidecars(
-			blk,
-			envelope.GetBlobsBundle(),
-		)
-		if err != nil {
-			return sidecarErr
-		}
-		return nil
-	})
 
 	// Set the block state root.
 	g.Go(func() error {
 		return s.SetBlockStateRoot(gCtx, st, blk)
 	})
 
-	defer s.logger.Info("finished assembling beacon block 🛟",
-		"slot", requestedSlot, "deposits", len(deposits))
-
 	// Set the execution payload on the block body.
-	return blk, blobSidecars, g.Wait()
+	return blk, sidecars, g.Wait()
 }
