3c625a9dcd03142f4adcc0c1a1bd82afb462f27d
ocnc-two <ocnc2@berachain.com>
2024-05-15 14:29:29 -0400

    linting

--- mod/primitives/pkg/hex/hex_test.go
+++ mod/primitives/pkg/hex/hex_test.go
@@ -1,13 +1,39 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2024 Berachain Foundation
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+// nolint:lll // long strings
 package hex_test
 
 import (
 	"math/big"
 	"testing"
 
-	"github.com/berachain/beacon-kit/mod/primitives/pkg/hex" // Replace with actual import path
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/hex"
 )
 
-// ====================== String Invariants Testing ===========================
+// ====================== String Invariants Testing ===========================.
 func TestNewStringStrictInvariants(t *testing.T) {
 	// NewStringStrict constructor should error if the input is invalid
 	tests := []struct {
@@ -157,7 +183,6 @@ func TestFromUint64Invariant(t *testing.T) {
 	}
 }
 
-// TestFromBigIntInvariant tests that FromBigInt always returns a valid String.
 func TestFromBigIntInvariant(t *testing.T) {
 	tests := []struct {
 		name  string
