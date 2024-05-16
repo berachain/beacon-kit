9f1dc343b4438a4b76f8c04c749ea7dda095fe40
Nidhi Singh <trippin@berachain.com>
2024-05-16 19:03:13 +0530

    unit tests for ckzg package (#1050)
    * unit tests for NewVerifier ckzg
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    * test for verifying blob kzg proof
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    * code cleanup and generate
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    * code more modular
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    * removed global variables and created helper functions
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    * nilaway
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    * moved files to testing folder
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    * use absolute path
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    * using basePath and code cleanup
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>
    
    ---------
    
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>

---
+++ mod/da/pkg/kzg/ckzg/ckzg_cgo_test.go
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2024 Berachain Foundation
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+//go:build ckzg
+
+package ckzg_test
+
+import (
+	"encoding/json"
+	prooftypes "github.com/berachain/beacon-kit/mod/da/pkg/kzg/types"
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/eip4844"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/require"
+	"path/filepath"
+	"testing"
+)
+
+func TestVerifyBlobKZGProofCgoEnabled(t *testing.T) {
+	validBlob, validProof, validCommitment := setupTestData(
+		t, "test_data.json")
+	testCases := []struct {
+		name        string
+		blob        *eip4844.Blob
+		proof       eip4844.KZGProof
+		commitment  eip4844.KZGCommitment
+		expectError bool
+	}{
+		{
+			name:        "Valid Proof",
+			blob:        validBlob,
+			proof:       validProof,
+			commitment:  validCommitment,
+			expectError: false,
+		},
+		{
+			name:        "Bad arguments",
+			blob:        &eip4844.Blob{},
+			proof:       eip4844.KZGProof{},
+			commitment:  eip4844.KZGCommitment{},
+			expectError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := globalVerifier.VerifyBlobProof(tc.blob, tc.proof, tc.commitment)
+			if tc.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+// TestVerifyBlobProofBatch tests the VerifyBlobProofBatch function for valid proofs
+func TestVerifyBlobProofBatch(t *testing.T) {
+	// Load the test data
+	fs := afero.NewOsFs()
+	fullPath := filepath.Join(baseDir, "test_data_batch.json")
+	file, err := afero.ReadFile(fs, fullPath)
+	require.NoError(t, err)
+
+	// Unmarshal the JSON data
+	var data struct {
+		Blobs       []string `json:"blobs"`
+		Proofs      []string `json:"proofs"`
+		Commitments []string `json:"commitments"`
+	}
+	err = json.Unmarshal(file, &data)
+	require.NoError(t, err)
+
+	// Convert the data to the types expected by VerifyBlobProofBatch
+	args := &prooftypes.BlobProofArgs{
+		Blobs:       make([]*eip4844.Blob, len(data.Blobs)),
+		Proofs:      make([]eip4844.KZGProof, len(data.Proofs)),
+		Commitments: make([]eip4844.KZGCommitment, len(data.Commitments)),
+	}
+	for i := range data.Blobs {
+		var blob eip4844.Blob
+		err = blob.UnmarshalJSON([]byte(`"` + data.Blobs[i] + `"`))
+		require.NoError(t, err)
+		args.Blobs[i] = &blob
+
+		var proof eip4844.KZGProof
+		err = proof.UnmarshalJSON([]byte(`"` + data.Proofs[i] + `"`))
+		require.NoError(t, err)
+		args.Proofs[i] = proof
+
+		var commitment eip4844.KZGCommitment
+		err = commitment.UnmarshalJSON([]byte(`"` + data.Commitments[i] + `"`))
+		require.NoError(t, err)
+		args.Commitments[i] = commitment
+	}
+
+	err = globalVerifier.VerifyBlobProofBatch(args)
+	require.NoError(t, err)
+}
