16458448d5f50aab2ca4d09cf7bc2c22fb90bb6a
itsdevbear <itsdevbear@berachain.com>
2024-05-27 23:53:50 -0400

    bet

--- mod/node-builder/config.go
+++ mod/node-builder/config.go
@@ -74,7 +74,7 @@ func (nb *NodeBuilder[T]) DefaultCometConfig() *cmtcfg.Config {
 	consensus := cfg.Consensus
 	consensus.TimeoutPropose = 3000 * time.Millisecond
 	consensus.TimeoutVote = 4000 * time.Millisecond
-	consensus.TimeoutCommit = 0000 * time.Millisecond
+	consensus.TimeoutCommit = 1000 * time.Millisecond
 
 	// BeaconKit forces PebbleDB as the database backend.
 	cfg.DBBackend = "pebbledb"
