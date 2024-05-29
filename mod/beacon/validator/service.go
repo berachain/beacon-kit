adcfb0ad63766c42489a22619644708886d93045
itsdevbear <itsdevbear@berachain.com>
2024-05-29 14:03:06 -0400

    bet

--- mod/beacon/validator/service.go
+++ mod/beacon/validator/service.go
@@ -407,9 +407,8 @@ func (s *Service[
 
 	// In order to rebuild a payload for the current slot, we need to know the
 	// previous block root, since we know that this is unmodified state.
-	// We can safely get the latest block header and then rebuild the previous
-	// block
-	// and it's root.
+	// We can safely get the latest block header and then rebuild the
+	// previous block and it's root.
 	latestHeader, err := st.GetLatestBlockHeader()
 	if err != nil {
 		return err
