20bfc78d5746da9f02f904325b41991b0bd0c5c5
Devon Bear <itsdevbear@berachain.com>
2024-06-02 12:51:54 -0400

    feat(deposits): Match the homework (#1265)
    * bet
    
    * bet
    
    * bet

--- mod/state-transition/pkg/core/state_processor_staking.go
+++ mod/state-transition/pkg/core/state_processor_staking.go
@@ -83,10 +83,6 @@ func (sp *StateProcessor[
 		if err := sp.processDeposit(st, dep); err != nil {
 			return err
 		}
-		// TODO: unhood this in better spot later
-		if err := st.SetEth1DepositIndex(dep.GetIndex()); err != nil {
-			return err
-		}
 	}
 	return nil
 }
@@ -111,6 +107,31 @@ func (sp *StateProcessor[
 	// ) {
 	// 	return errors.New("invalid merkle branch")
 	// }
+
+	depositIndex, err := st.GetEth1DepositIndex()
+	if err != nil {
+		return err
+	}
+
+	if err = st.SetEth1DepositIndex(
+		depositIndex + 1,
+	); err != nil {
+		return err
+	}
+
+	return sp.applyDeposit(st, dep)
+}
+
+// processDeposit processes the deposit and ensures it matches the local state.
+func (sp *StateProcessor[
+	BeaconBlockT, BeaconBlockBodyT, BeaconBlockHeaderT,
+	BeaconStateT, BlobSidecarsT, ContextT,
+	DepositT, ExecutionPayloadT, ExecutionPayloadHeaderT,
+	ForkDataT, ValidatorT, WithdrawalT, WithdrawalCredentialsT,
+]) applyDeposit(
+	st BeaconStateT,
+	dep DepositT,
+) error {
 	idx, err := st.ValidatorIndexByPubkey(dep.GetPubkey())
 	// If the validator already exists, we update the balance.
 	if err == nil {
@@ -125,6 +146,7 @@ func (sp *StateProcessor[
 			math.Gwei(sp.cs.MaxEffectiveBalance())))
 		return st.UpdateValidatorAtIndex(idx, val)
 	}
+
 	// If the validator does not exist, we add the validator.
 	// Add the validator to the registry.
 	return sp.createValidator(st, dep)
