42308f2dd5a0778c758cf42d8a776779c6a17f37
itsdevbear <itsdevbear@berachain.com>
2024-06-03 11:27:57 -0400

    bet

--- mod/beacon/validator/service.go
+++ mod/beacon/validator/service.go
@@ -387,7 +387,8 @@ func (s *Service[
 		return
 	}
 
-	// TODO: Verify if the slot number is correct here.
+	// TODO: Verify if the slot number is correct here, I believe in current form
+	// it should be +1'd. Not a big deal until hardforks are in play though.
 	if err = s.localPayloadBuilder.SendForceHeadFCU(ctx, st, slot+1); err != nil {
 		s.logger.Error(
 			"failed to send force head FCU",
