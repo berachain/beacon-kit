f8f8bf3706c6bb34e61f57a372ac713ef062834c
itsdevbear <itsdevbear@berachain.com>
2024-06-04 18:11:50 -0400

    bet

--- build/scripts/release.mk
+++ build/scripts/release.mk
@@ -5,20 +5,15 @@ RELEASE_TARGETS := build-linux-amd64 build-linux-arm64 build-darwin-arm64
 define build_release
 	GOOS=$(1) GOARCH=$(2) CGO_ENABLED=$(3) \
 	cd ${CURRENT_DIR}/$(TESTAPP_CMD_DIR) && \
-	go build -mod=readonly $(BUILD_FLAGS) -o $(OUT_DIR)/beacond-$(1)-$(2)-$(3) ./.
+	go build -mod=readonly $(BUILD_FLAGS) -o $(OUT_DIR)/beacond-$(1)-$(2)-$(git describe --tags --always --drity) ./.
 endef
 
 build-linux-amd64:
-	$(call build_release,linux,amd64,0,$(shell git describe --tags --always --dirty))
-build-linux-amd64-$(VERSION):
-	$(call build_release,linux,amd64,0,$(VERSION))
+	$(call build_release,linux,amd64,0)
 
 build-linux-arm64:
-	$(call build_release,linux,arm64,0,$(shell git describe --tags --always --dirty))
-build-linux-arm64-$(VERSION):
-	$(call build_release,linux,arm64,0,$(VERSION))
+	$(call build_release,linux,arm64,0)
 
 build-darwin-arm64:
-	$(call build_release,darwin,arm64,1,$(shell git describe --tags --always --dirty))
-build-darwin-arm64-$(VERSION):
-	$(call build_release,darwin,arm64,1,$(VERSION))
+	$(call build_release,darwin,arm64,1)
+
