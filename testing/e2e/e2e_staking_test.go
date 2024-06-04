039a855956188020b916b0ee173ab4c02586e019
itsdevbear <itsdevbear@berachain.com>
2024-06-04 15:44:43 -0400

    bet

--- testing/e2e/e2e_staking_test.go
+++ testing/e2e/e2e_staking_test.go
@@ -39,6 +39,91 @@ const (
 	NumDepositsLoad        = 500
 )
 
+// TestDepositContract tests the deposit contract to attempt staking and
+// increasing a validator's consensus power.
+func (s *BeaconKitE2ESuite) TestDepositContract() {
+	// Get the consensus client.
+	client := s.ConsensusClients()["cl-validator-beaconkit-0"]
+	s.Require().NotNil(client)
+
+	// Get the public key.
+	pubkey, err := client.GetPubKey(s.Ctx())
+	s.Require().NoError(err)
+	s.Require().Len(pubkey, 48)
+
+	// Get the consensus power.
+	_, err = client.GetConsensusPower(s.Ctx())
+	s.Require().NoError(err)
+
+	// Bind the deposit contract.
+	dc, err := deposit.NewBeaconDepositContract(
+		common.HexToAddress(DepositContractAddress),
+		s.JSONRPCBalancer(),
+	)
+	s.Require().NoError(err)
+
+	// Generate the credentials.
+	credentials := byteslib.PrependExtendToSize(
+		s.GenesisAccount().Address().Bytes(),
+		32,
+	)
+	credentials[0] = 0x01
+
+	// Generate the signature.
+	signature := [96]byte{}
+	s.Require().Len(signature[:], 96)
+
+	// Get the chain ID.
+	chainID, err := s.JSONRPCBalancer().ChainID(s.Ctx())
+	s.Require().NoError(err)
+
+	// Get the block num
+	blkNum, err := s.JSONRPCBalancer().BlockNumber(s.Ctx())
+	s.Require().NoError(err)
+
+	// Get original evm balance
+	balance, err := s.JSONRPCBalancer().BalanceAt(
+		s.Ctx(),
+		s.GenesisAccount().Address(),
+		big.NewInt(int64(blkNum)),
+	)
+	s.Require().NoError(err)
+
+	// Create a deposit transaction.
+	val, _ := big.NewFloat(32e18).Int(nil)
+	tx, err := dc.Deposit(&bind.TransactOpts{
+		From:   s.GenesisAccount().Address(),
+		Value:  val,
+		Signer: s.GenesisAccount().SignerFunc(chainID),
+	}, pubkey, credentials, 32*suite.OneGwei, signature[:])
+	s.Require().NoError(err)
+
+	// Wait for the transaction to be mined.
+	var receipt *coretypes.Receipt
+	receipt, err = bind.WaitMined(s.Ctx(), s.JSONRPCBalancer(), tx)
+	s.Require().NoError(err)
+	s.Require().Equal(uint64(1), receipt.Status)
+	s.Logger().Info("Deposit transaction mined", "txHash", receipt.TxHash.Hex())
+
+	// Wait for the log to be processed.
+	targetBlkNum := blkNum + 10
+	err = s.WaitForFinalizedBlockNumber(targetBlkNum)
+	s.Require().NoError(err)
+
+	// Check to see if evm balance decreased.
+	postDepositBalance, err := s.JSONRPCBalancer().BalanceAt(
+		s.Ctx(),
+		s.GenesisAccount().Address(),
+		big.NewInt(int64(targetBlkNum)),
+	)
+	s.Require().NoError(err)
+	s.Require().Equal(postDepositBalance.Cmp(balance), -1)
+
+	newPower, err := client.GetConsensusPower(s.Ctx())
+	s.Require().NoError(err)
+	s.Require().Equal(newPower, 32*suite.OneGwei)
+}
+
 func (s *BeaconKitE2ESuite) TestDepositRobustness() {
 	// Get the consensus client.
 	client := s.ConsensusClients()[DefaultClient]
