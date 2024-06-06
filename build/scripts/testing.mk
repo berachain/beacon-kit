476b6c4886504998f6f9e17eab5bd055676bba6a
itsdevbear <itsdevbear@berachain.com>
2024-06-06 18:46:53 -0400

    chainid

--- build/scripts/testing.mk
+++ build/scripts/testing.mk
@@ -161,7 +161,7 @@ start-erigon: ## start an ephemeral `erigon` node
 	--authrpc.addr	0.0.0.0 \
 	--authrpc.jwtsecret $(JWT_PATH) \
 	--authrpc.vhosts "*" \
-	--networkid 80086 \
+	--networkid 80084 \
 	--db.size.limit	3000MB \
 	--datadir .tmp/erigon
 
