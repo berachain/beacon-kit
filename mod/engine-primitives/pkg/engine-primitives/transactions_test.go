3913853279ed4e3344e986cdc3364cab08e61e36
nidhi-singh02 <trippin@berachain.com>
2024-06-10 16:25:08 +0530

    tests for engine primitives package
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>

---
+++ mod/engine-primitives/pkg/engine-primitives/transactions_test.go
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2024, Berachain Foundation. All rights reserved.
+// Use of this software is govered by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
+package engineprimitives_test
+
+import (
+	"testing"
+
+	engineprimitives "github.com/berachain/beacon-kit/mod/engine-primitives/pkg/engine-primitives"
+	"github.com/berachain/beacon-kit/mod/primitives"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransactions(t *testing.T) {
+	txs := engineprimitives.Transactions{
+		[]byte("transaction1"),
+		[]byte("transaction2"),
+		[]byte("transaction3"),
+	}
+
+	root, err := txs.HashTreeRoot()
+	require.NoError(t, err)
+	require.NotNil(t, root)
+
+	require.NotEqual(t, primitives.Root{}, root)
+}
