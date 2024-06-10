f2cc0294aa0368071df1d1c4e32816e83d464e5e
Hoa Nguyen <hoa@notional.ventures>
2024-06-10 22:32:47 +0700

    refactor: add miss defer and refactor name function (#1408)
    refactor: add miss defer
    
    Co-authored-by: Devon Bear <itsdevbear@berachain.com>

--- mod/execution/pkg/deposit/sync.go
+++ mod/execution/pkg/deposit/sync.go
@@ -56,6 +56,7 @@ func (s *Service[
 	WithdrawalCredentialsT, DepositT,
 ]) depositCatchupFetcher(ctx context.Context) {
 	ticker := time.NewTicker(defaultRetryInterval)
+	defer ticker.Stop()
 	for {
 		select {
 		case <-ctx.Done():
