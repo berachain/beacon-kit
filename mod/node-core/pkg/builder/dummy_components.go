9bc7326014f02f12578aa6cdd68e693f1d39e032
ocnc-two <ocnc2@berachain.com>
2024-06-13 14:22:54 -0400

    later problem

--- mod/node-core/pkg/builder/dummy_components.go
+++ mod/node-core/pkg/builder/dummy_components.go
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2024, Berachain Foundation. All rights reserved.
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
 package builder
 
 import (
@@ -12,9 +32,11 @@ import (
 )
 
 // dummy components for the builder. these are needed to support the
-// direct dependencies needed to supply the Module
+// direct dependencies needed to supply the Module.
+//
+//nolint:gochecknoglobals // we eventually shouldn't need to supply empty vars
 var (
-	// MIDDLEWARES
+	// MIDDLEWARES.
 	emptyFinalizeBlockMiddlware = &middleware.FinalizeBlockMiddleware[
 		*consensustypes.BeaconBlock,
 		runtime.BeaconState,
@@ -28,7 +50,7 @@ var (
 		*datypes.BlobSidecars,
 		runtime.Backend,
 	]{}
-	// STORAGE BACKEND
+	// STORAGE BACKEND.
 	emptyStorageBackend = &storage.Backend[
 		*dastore.Store[*consensustypes.BeaconBlockBody],
 		*consensustypes.BeaconBlock,
