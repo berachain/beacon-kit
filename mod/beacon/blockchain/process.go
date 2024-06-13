4cacd053131f22494c14dd6c67c13bdc998d9c2e
itsdevbear <itsdevbear@berachain.com>
2024-06-13 01:43:25 -0400

    bet

--- mod/beacon/blockchain/process.go
+++ mod/beacon/blockchain/process.go
@@ -121,11 +121,9 @@ func (s *Service[
 	// TODO: this is hood as fuck.
 	// We won't send a fcu if the block is bad, should be addressed
 	// via ticker later.
-	defer func() {
-		go func() {
-			s.blockFeed.Send(feed.NewEvent(ctx, events.BeaconBlockFinalized, (blk)))
-			s.sendPostBlockFCU(ctx, st, blk)
-		}()
+	go func() {
+		s.blockFeed.Send(feed.NewEvent(ctx, events.BeaconBlockFinalized, (blk)))
+		s.sendPostBlockFCU(ctx, st, blk)
 	}()
 
 	return valUpdates, nil
