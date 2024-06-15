ae8d1e69bb72f393c42e44f2fade25eebaef5dab
Tuan Tran <tropicaldog17@gmail.com>
2024-06-16 00:07:47 +0700

    bet

--- mod/primitives/pkg/ssz/v2/lib/determine_size_test.go
+++ mod/primitives/pkg/ssz/v2/lib/determine_size_test.go
@@ -16,13 +16,20 @@ func TestGetArrayDimensionality(t *testing.T) {
 		{"1D empty slice", []int32{}, 1},
 		{"1D non-empty array", [3]int32{1, 2, 3}, 1},
 		{"1D non-empty slice", []int32{1, 2, 3}, 1},
+		{"2D empty array", [0][0]int32{}, 2},
+		{"2D empty slice", [][]int32{}, 2},
 		{"2D non-empty array", [2][3]int32{{1, 2, 3}, {4, 5, 6}}, 2},
 		{"2D non-empty slice", [][]int32{{1, 2, 3}, {4, 5, 6}}, 2},
+		{"3D empty array", [0][0][0]int32{}, 3},
+		{"3D empty slice", [][][]int32{}, 3},
 		{"3D non-empty array", [2][2][2]int32{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}, 3},
 		{"3D non-empty slice", [][][]int32{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}, 3},
+		{"1D empty byte array", [0]byte{}, 1},
+		{"1D empty byte slice", []byte{}, 1},
 		{"Byte array", [3]byte{1, 2, 3}, 1},
 		{"Byte slice", []byte{1, 2, 3}, 1},
 		{"2D empty bytes", [][]byte{}, 2},
+		{"2D non-empty bytes", [][]byte{{1, 2, 3}, {4, 5, 6}}, 2},
 	}
 
 	for _, tt := range tests {
