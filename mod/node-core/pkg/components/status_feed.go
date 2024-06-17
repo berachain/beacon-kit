812968095a77d17ceee13669ed2ea8b4fd9bed88
itsdevbear <itsdevbear@berachain.com>
2024-06-17 14:25:54 -0400

    bet

--- mod/node-core/pkg/components/status_feed.go
+++ mod/node-core/pkg/components/status_feed.go
@@ -2,15 +2,15 @@ package components
 
 import (
 	"github.com/berachain/beacon-kit/mod/async/pkg/event"
-	"github.com/berachain/beacon-kit/mod/async/pkg/types"
+	asynctypes "github.com/berachain/beacon-kit/mod/async/pkg/types"
 	"github.com/berachain/beacon-kit/mod/runtime/pkg/service"
 )
 
 // ProvideStatusFeed provides a status feed.
 func ProvideStatusFeed() *event.FeedOf[
-	types.EventID, *types.Event[*service.StatusEvent],
+	asynctypes.EventID, *asynctypes.Event[*service.StatusEvent],
 ] {
 	return &event.FeedOf[
-		types.EventID, *types.Event[*service.StatusEvent],
+		asynctypes.EventID, *asynctypes.Event[*service.StatusEvent],
 	]{}
 }
