80578dac25dae11215e0fc49111a30693ad3014a
Tuan Tran <tropicaldog17@gmail.com>
2024-06-17 21:47:06 +0700

    seperate test by num of dimensional

--- mod/primitives/pkg/ssz/v2/lib/serializer_test.go
+++ mod/primitives/pkg/ssz/v2/lib/serializer_test.go
@@ -70,7 +70,7 @@ func TestMarshalUnmarshalBoolSerializer(t *testing.T) {
 }
 
 func TestGetArrayDimensionality(t *testing.T) {
-	tests := []struct {
+	oneDimensionalTests := []struct {
 		name     string
 		input    any
 		expected int
@@ -79,29 +79,69 @@ func TestGetArrayDimensionality(t *testing.T) {
 		{"1D empty slice", []int32{}, 1},
 		{"1D non-empty array", [3]int32{1, 2, 3}, 1},
 		{"1D non-empty slice", []int32{1, 2, 3}, 1},
-		{"2D empty array", [0][0]int32{}, 2},
-		{"2D empty slice", [][]int32{}, 2},
-		{"2D non-empty array", [2][3]int32{{1, 2, 3}, {4, 5, 6}}, 2},
-		{"2D non-empty slice", [][]int32{{1, 2, 3}, {4, 5, 6}}, 2},
-		{"3D empty array", [0][0][0]int32{}, 3},
-		{"3D empty slice", [][][]int32{}, 3},
-		{"3D non-empty array", [2][2][2]int32{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}, 3},
-		{"3D non-empty slice", [][][]int32{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}, 3},
 		{"1D empty byte array", [0]byte{}, 1},
 		{"1D empty byte slice", []byte{}, 1},
 		{"Byte array", [3]byte{1, 2, 3}, 1},
 		{"Byte slice", []byte{1, 2, 3}, 1},
+	}
+
+	twoDimensionalTests := []struct {
+		name     string
+		input    any
+		expected int
+	}{
+		{"2D empty array", [0][0]int32{}, 2},
+		{"2D empty slice", [][]int32{}, 2},
+		{"2D non-empty array", [2][3]int32{{1, 2, 3}, {4, 5, 6}}, 2},
+		{"2D non-empty slice", [][]int32{{1, 2, 3}, {4, 5, 6}}, 2},
 		{"2D empty bytes", [][]byte{}, 2},
 		{"2D non-empty bytes", [][]byte{{1, 2, 3}, {4, 5, 6}}, 2},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			val := reflect.ValueOf(tt.input)
-			result := sszv2.GetArrayDimensionality(val)
-			if result != tt.expected {
-				t.Errorf("Expected dimensionality %d, but got %d", tt.expected, result)
-			}
-		})
+	threeDimensionalTests := []struct {
+		name     string
+		input    any
+		expected int
+	}{
+		{"3D empty array", [0][0][0]int32{}, 3},
+		{"3D empty slice", [][][]int32{}, 3},
+		{"3D non-empty array", [2][2][2]int32{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}, 3},
+		{"3D non-empty slice", [][][]int32{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}, 3},
 	}
+
+	t.Run("1D", func(t *testing.T) {
+		for _, tt := range oneDimensionalTests {
+			t.Run(tt.name, func(t *testing.T) {
+				val := reflect.ValueOf(tt.input)
+				result := sszv2.GetArrayDimensionality(val)
+				if result != tt.expected {
+					t.Errorf("Expected dimensionality %d, but got %d", tt.expected, result)
+				}
+			})
+		}
+	})
+
+	t.Run("2D", func(t *testing.T) {
+		for _, tt := range twoDimensionalTests {
+			t.Run(tt.name, func(t *testing.T) {
+				val := reflect.ValueOf(tt.input)
+				result := sszv2.GetArrayDimensionality(val)
+				if result != tt.expected {
+					t.Errorf("Expected dimensionality %d, but got %d", tt.expected, result)
+				}
+			})
+		}
+	})
+
+	t.Run("3D", func(t *testing.T) {
+		for _, tt := range threeDimensionalTests {
+			t.Run(tt.name, func(t *testing.T) {
+				val := reflect.ValueOf(tt.input)
+				result := sszv2.GetArrayDimensionality(val)
+				if result != tt.expected {
+					t.Errorf("Expected dimensionality %d, but got %d", tt.expected, result)
+				}
+			})
+		}
+	})
 }
