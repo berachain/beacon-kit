792e00553ae5bf02d7c50329b5d416303e3f71c9
itsdevbear <itsdevbear@berachain.com>
2024-06-22 19:07:25 -0400

    bet

--- mod/da/pkg/da/service.go
+++ mod/da/pkg/da/service.go
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2024, Berachain Foundation. All rights reserved.
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
 package da
 
 import (
@@ -77,7 +97,11 @@ func (s *Service[_, _, BlobSidecarsT, _]) start(ctx context.Context) error {
 		case e := <-ch:
 			if e.Type() == events.BlobSidecarsReceived {
 				if err := s.processBlobSidecars(ctx, 0, e.Data()); err != nil {
-					s.logger.Error("failed to process blob sidecars", "error", err)
+					s.logger.Error(
+						"failed to process blob sidecars",
+						"error",
+						err,
+					)
 				}
 			}
 		}
