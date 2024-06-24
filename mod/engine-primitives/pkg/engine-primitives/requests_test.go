677e39f1aec4d28a95d13c24b782b050342a2653
archbear <arch@berachain.com>
2024-06-24 17:36:01 -0400

    ermst

--- mod/engine-primitives/pkg/engine-primitives/requests_test.go
+++ mod/engine-primitives/pkg/engine-primitives/requests_test.go
@@ -130,7 +130,7 @@ func TestBuildNewPayloadRequest(t *testing.T) {
 
 func TestBuildForkchoiceUpdateRequest(t *testing.T) {
 	state := &engineprimitives.ForkchoiceStateV1{}
-	payloadAttributes := &mocks.PayloadAttributer[any]{}
+	payloadAttributes := &mocks.PayloadAttributer{}
 	forkVersion := uint32(1)
 
 	request := engineprimitives.BuildForkchoiceUpdateRequest(
