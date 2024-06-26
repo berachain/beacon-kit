348e537d5007695189b0556d29fe2263d8c8e994
itsdevbear <itsdevbear@berachain.com>
2024-06-26 10:22:48 -0400

    bet

--- mod/primitives/pkg/ssz/types/vector.go
+++ mod/primitives/pkg/ssz/types/vector.go
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2024, Berachain Foundation. All rights reserved.
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
 package types
 
 import (
@@ -5,7 +25,6 @@ import (
 	"reflect"
 
 	"github.com/berachain/beacon-kit/mod/primitives/pkg/common"
-	"github.com/berachain/beacon-kit/mod/primitives/pkg/math"
 	"github.com/berachain/beacon-kit/mod/primitives/pkg/ssz"
 )
 
@@ -30,7 +49,9 @@ func (l SSZVectorBasic[T]) SizeSSZ() int {
 // HashTreeRoot returns the Merkle root of the SSZVectorBasic.
 func (l SSZVectorBasic[T]) HashTreeRoot() ([32]byte, error) {
 	// Create a merkleizer
-	m := ssz.NewMerkleizer[common.ChainSpec, any, math.U64, [32]byte]()
+	m := ssz.NewMerkleizer[
+		common.ChainSpec, [32]byte, common.Root,
+	]()
 	packedBytes := make([]byte, l.SizeSSZ())
 	for _, v := range l {
 		v, err := v.MarshalSSZ()
--- mod/primitives/pkg/ssz/types/vector_test.go
+++ mod/primitives/pkg/ssz/types/vector_test.go
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2024, Berachain Foundation. All rights reserved.
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
 package types_test
 
 import (
@@ -15,7 +35,7 @@ func TestSSZVectorBasic(t *testing.T) {
 
 	t.Run("SizeSSZ for byte slice vector", func(t *testing.T) {
 		vector := types.SSZVectorBasic[types.SSZUInt8]{1, 2, 3, 4, 5, 6, 7, 8}
-		require.Equal(t, 48, vector.SizeSSZ())
+		require.Equal(t, 8, vector.SizeSSZ())
 	})
 
 	t.Run("SizeSSZ for uint64 vector", func(t *testing.T) {
