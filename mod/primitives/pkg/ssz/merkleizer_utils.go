e485b6b8eeb21b02ab9368e9a55438fc0f5fbeea
Cal Bera <calbera@berachain.com>
2024-06-25 21:42:06 -0400

    nit

--- mod/primitives/pkg/ssz/merkleizer_utils.go
+++ mod/primitives/pkg/ssz/merkleizer_utils.go
@@ -88,7 +88,6 @@ func (m *merkleizer[SpecT, RootT, T]) pack(values []T) ([]RootT, error) {
 func (m *merkleizer[SpecT, RootT, T]) partitionBytes(input []byte) (
 	[]RootT, uint64, error,
 ) {
-	
 	numChunks := max(len(input)+31/constants.RootLength, 1)
 	chunks := m.intermediateBuffer.Get(numChunks)
 	for i := range chunks {
