9b531626f599c67a64638cb94f9c2e686dd93bc1
itsdevbear <itsdevbear@berachain.com>
2024-07-01 01:22:43 -0400

    bet

--- mod/primitives/pkg/ssz/list.go
+++ mod/primitives/pkg/ssz/list.go
@@ -58,6 +58,7 @@ func ListFromElements[T types.MinimalSSZType](
 
 // ByteList from Bytes creates a new List from bytes.
 func ByteListFromBytes(bytes []byte, limit uint64) *List[Byte] {
+	//#nosec:G103 // its fine, but we should find  abetter solution.
 	elements := *(*[]Byte)(unsafe.Pointer(&bytes))
 	return &List[Byte]{
 		elements: elements,
