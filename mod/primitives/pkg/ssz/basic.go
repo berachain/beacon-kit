6dab29b3bea865f1296ece4cbd4ae1ea18977ccc
itsdevbear <itsdevbear@berachain.com>
2024-07-01 01:39:15 -0400

    bet

--- mod/primitives/pkg/ssz/basic.go
+++ mod/primitives/pkg/ssz/basic.go
@@ -25,6 +25,7 @@ import (
 	"encoding/binary"
 	"fmt"
 
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/ssz/constants"
 	"github.com/berachain/beacon-kit/mod/primitives/pkg/ssz/types"
 )
 
@@ -56,8 +57,8 @@ type (
 /* -------------------------------------------------------------------------- */
 
 // SizeSSZ returns the size of the bool in bytes.
-func (b Bool) SizeSSZ() int {
-	return 1
+func (Bool) SizeSSZ() int {
+	return constants.BoolSize
 }
 
 // MarshalSSZ marshals the bool into SSZ format.
@@ -70,9 +71,10 @@ func (b Bool) MarshalSSZ() ([]byte, error) {
 
 // NewFromSSZ creates a new Bool from SSZ format.
 func (Bool) NewFromSSZ(buf []byte) (Bool, error) {
-	if len(buf) != 1 {
+	if len(buf) != constants.BoolSize {
 		return false, fmt.Errorf(
-			"invalid buffer length: expected 1, got %d",
+			"invalid buffer length: expected %d, got %d",
+			constants.BoolSize,
 			len(buf),
 		)
 	}
@@ -81,11 +83,11 @@ func (Bool) NewFromSSZ(buf []byte) (Bool, error) {
 
 // HashTreeRoot returns the hash tree root of the bool.
 func (b Bool) HashTreeRoot() ([32]byte, error) {
-	buf := make([]byte, 32)
+	buf := make([]byte, constants.BytesPerChunk)
 	if b {
 		buf[0] = 1
 	}
-	return [32]byte(buf), nil
+	return [constants.BytesPerChunk]byte(buf), nil
 }
 
 // IsFixed returns true if the bool is fixed size.
@@ -108,8 +110,8 @@ func (Bool) ChunkCount() uint64 {
 /* -------------------------------------------------------------------------- */
 
 // SizeSSZ returns the size of the uint8 in bytes.
-func (u U8) SizeSSZ() int {
-	return 1
+func (U8) SizeSSZ() int {
+	return constants.U8Size
 }
 
 // MarshalSSZ marshals the uint8 into SSZ format.
@@ -119,9 +121,10 @@ func (u U8) MarshalSSZ() ([]byte, error) {
 
 // NewFromSSZ creates a new U8 from SSZ format.
 func (U8) NewFromSSZ(buf []byte) (U8, error) {
-	if len(buf) != 1 {
+	if len(buf) != constants.U8Size {
 		return 0, fmt.Errorf(
-			"invalid buffer length: expected 1, got %d",
+			"invalid buffer length: expected %d, got %d",
+			constants.U8Size,
 			len(buf),
 		)
 	}
@@ -132,7 +135,7 @@ func (U8) NewFromSSZ(buf []byte) (U8, error) {
 
 // HashTreeRoot returns the hash tree root of the uint8.
 func (u U8) HashTreeRoot() ([32]byte, error) {
-	buf := make([]byte, 32)
+	buf := make([]byte, constants.BytesPerChunk)
 	buf[0] = byte(u)
 	return [32]byte(buf), nil
 }
@@ -157,22 +160,23 @@ func (U8) ChunkCount() uint64 {
 /* -------------------------------------------------------------------------- */
 
 // SizeSSZ returns the size of the uint16 in bytes.
-func (u U16) SizeSSZ() int {
-	return 2
+func (U16) SizeSSZ() int {
+	return constants.U16Size
 }
 
 // MarshalSSZ marshals the uint16 into SSZ format.
 func (u U16) MarshalSSZ() ([]byte, error) {
-	buf := make([]byte, 2)
+	buf := make([]byte, constants.U16Size)
 	binary.LittleEndian.PutUint16(buf, uint16(u))
 	return buf, nil
 }
 
 // NewFromSSZ creates a new U16 from SSZ format.
 func (U16) NewFromSSZ(buf []byte) (U16, error) {
-	if len(buf) != 2 {
+	if len(buf) != constants.U16Size {
 		return 0, fmt.Errorf(
-			"invalid buffer length: expected 2, got %d",
+			"invalid buffer length: expected %d, got %d",
+			constants.U16Size,
 			len(buf),
 		)
 	}
@@ -181,8 +185,8 @@ func (U16) NewFromSSZ(buf []byte) (U16, error) {
 
 // HashTreeRoot returns the hash tree root of the uint16.
 func (u U16) HashTreeRoot() ([32]byte, error) {
-	buf := make([]byte, 32)
-	binary.LittleEndian.PutUint16(buf[:2], uint16(u))
+	buf := make([]byte, constants.BytesPerChunk)
+	binary.LittleEndian.PutUint16(buf[:constants.U16Size], uint16(u))
 	return [32]byte(buf), nil
 }
 
@@ -206,22 +210,23 @@ func (U16) ChunkCount() uint64 {
 /* -------------------------------------------------------------------------- */
 
 // SizeSSZ returns the size of the uint32 in bytes.
-func (u U32) SizeSSZ() int {
-	return 4
+func (U32) SizeSSZ() int {
+	return constants.U32Size
 }
 
 // MarshalSSZ marshals the uint32 into SSZ format.
 func (u U32) MarshalSSZ() ([]byte, error) {
-	buf := make([]byte, 4)
+	buf := make([]byte, constants.U32Size)
 	binary.LittleEndian.PutUint32(buf, uint32(u))
 	return buf, nil
 }
 
 // NewFromSSZ creates a new U32 from SSZ format.
 func (U32) NewFromSSZ(buf []byte) (U32, error) {
-	if len(buf) != 4 {
+	if len(buf) != constants.U32Size {
 		return 0, fmt.Errorf(
-			"invalid buffer length: expected 4, got %d",
+			"invalid buffer length: expected %d, got %d",
+			constants.U32Size,
 			len(buf),
 		)
 	}
@@ -230,8 +235,8 @@ func (U32) NewFromSSZ(buf []byte) (U32, error) {
 
 // HashTreeRoot returns the hash tree root of the uint32.
 func (u U32) HashTreeRoot() ([32]byte, error) {
-	buf := make([]byte, 32)
-	binary.LittleEndian.PutUint32(buf[:4], uint32(u))
+	buf := make([]byte, constants.BytesPerChunk)
+	binary.LittleEndian.PutUint32(buf[:constants.U32Size], uint32(u))
 	return [32]byte(buf), nil
 }
 
@@ -255,22 +260,23 @@ func (U32) ChunkCount() uint64 {
 /* -------------------------------------------------------------------------- */
 
 // SizeSSZ returns the size of the uint64 in bytes.
-func (u U64) SizeSSZ() int {
-	return 8
+func (U64) SizeSSZ() int {
+	return constants.U64Size
 }
 
 // MarshalSSZ marshals the uint64 into SSZ format.
 func (u U64) MarshalSSZ() ([]byte, error) {
-	buf := make([]byte, 8)
+	buf := make([]byte, constants.U64Size)
 	binary.LittleEndian.PutUint64(buf, uint64(u))
 	return buf, nil
 }
 
 // NewFromSSZ creates a new U64 from SSZ format.
 func (U64) NewFromSSZ(buf []byte) (U64, error) {
-	if len(buf) != 8 {
+	if len(buf) != constants.U64Size {
 		return 0, fmt.Errorf(
-			"invalid buffer length: expected 8, got %d",
+			"invalid buffer length: expected %d, got %d",
+			constants.U64Size,
 			len(buf),
 		)
 	}
@@ -279,8 +285,8 @@ func (U64) NewFromSSZ(buf []byte) (U64, error) {
 
 // HashTreeRoot returns the hash tree root of the uint64.
 func (u U64) HashTreeRoot() ([32]byte, error) {
-	buf := make([]byte, 32)
-	binary.LittleEndian.PutUint64(buf[:8], uint64(u))
+	buf := make([]byte, constants.BytesPerChunk)
+	binary.LittleEndian.PutUint64(buf[:constants.U64Size], uint64(u))
 	return [32]byte(buf), nil
 }
 
@@ -300,12 +306,66 @@ func (U64) ChunkCount() uint64 {
 }
 
 /* -------------------------------------------------------------------------- */
+<<<<<<< Updated upstream
+=======
+/*                                    U256                                    */
+/* -------------------------------------------------------------------------- */
+
+func NewU256FromUint64(v uint64) *U256 {
+	return (*U256)(uint256.NewInt(0).SetUint64(v))
+}
+
+// SizeSSZ returns the size of the U256 in bytes.
+func (U256) SizeSSZ() int {
+	return constants.U256Size
+}
+
+// MarshalSSZ marshals the U256 into SSZ format.
+func (u *U256) MarshalSSZ() ([]byte, error) {
+	return (*uint256.Int)(u).MarshalSSZ()
+}
+
+// NewFromSSZ creates a new U256 from SSZ format.
+func (U256) NewFromSSZ(buf []byte) (*U256, error) {
+	if len(buf) != constants.U256Size {
+		return nil, fmt.Errorf(
+			"invalid buffer length: expected %d, got %d",
+			constants.U256Size,
+			len(buf),
+		)
+	}
+	u := new(uint256.Int)
+	return (*U256)(u), u.UnmarshalSSZ(buf)
+}
+
+// HashTreeRoot returns the hash tree root of the U256.
+func (u *U256) HashTreeRoot() ([32]byte, error) {
+	return (*uint256.Int)(u).HashTreeRoot()
+}
+
+// IsFixed returns true if the U256 is fixed size.
+func (*U256) IsFixed() bool {
+	return true
+}
+
+// Type returns the type of the U256.
+func (*U256) Type() types.Type {
+	return types.Basic
+}
+
+// ChunkCount returns the number of chunks required to store the U256.
+func (*U256) ChunkCount() uint64 {
+	return 1
+}
+
+/* -------------------------------------------------------------------------- */
+>>>>>>> Stashed changes
 /*                                    Byte                                    */
 /* -------------------------------------------------------------------------- */
 
 // SizeSSZ returns the size of the byte slice in bytes.
-func (b Byte) SizeSSZ() int {
-	return 1
+func (Byte) SizeSSZ() int {
+	return constants.ByteSize
 }
 
 // MarshalSSZ marshals the byte into SSZ format.
@@ -315,9 +375,10 @@ func (b Byte) MarshalSSZ() ([]byte, error) {
 
 // NewFromSSZ creates a new Byte from SSZ format.
 func (Byte) NewFromSSZ(buf []byte) (Byte, error) {
-	if len(buf) != 1 {
+	if len(buf) != constants.ByteSize {
 		return 0, fmt.Errorf(
-			"invalid buffer length: expected 1, got %d",
+			"invalid buffer length: expected %d, got %d",
+			constants.ByteSize,
 			len(buf),
 		)
 	}
@@ -326,7 +387,7 @@ func (Byte) NewFromSSZ(buf []byte) (Byte, error) {
 
 // HashTreeRoot returns the hash tree root of the byte.
 func (b Byte) HashTreeRoot() ([32]byte, error) {
-	buf := make([]byte, 32)
+	buf := make([]byte, constants.BytesPerChunk)
 	buf[0] = byte(b)
 	return [32]byte(buf), nil
 }
