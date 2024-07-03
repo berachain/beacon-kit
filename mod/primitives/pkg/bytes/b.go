dc2385ec58bd43d8847dfbcaee9dad99d771cabe
itsdevbear <itsdevbear@berachain.com>
2024-07-03 15:39:42 -0400

    bet

--- mod/primitives/pkg/bytes/b.go
+++ mod/primitives/pkg/bytes/b.go
@@ -18,7 +18,7 @@
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
 // TITLE.
 //
-//nolint:dupl // it's okay to have similar code for different types
+
 package bytes
 
 import (
