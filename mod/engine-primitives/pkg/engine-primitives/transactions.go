94448d3ef6895561bf7687cfa2aec8795ccba2d7
Cal Bera <calbera@berachain.com>
2024-07-04 00:04:51 -0400

    test(ssz): consistency tests of transactions hash tree root (#1678)
    Signed-off-by: Cal Bera <calbera@berachain.com>
    Co-authored-by: itsdevbear <itsdevbear@berachain.com>

--- mod/engine-primitives/pkg/engine-primitives/transactions.go
+++ mod/engine-primitives/pkg/engine-primitives/transactions.go
@@ -32,7 +32,7 @@ import (
 // Transactions is a typealias for [][]byte, which is how transactions are
 // received in the execution payload.
 //
-// TODO: make it take a generic SpecT type.
+// TODO: Remove and deprecate this type once migrated to ProperTransactions.
 type Transactions [][]byte
 
 // HashTreeRoot returns the hash tree root of the Transactions list.
@@ -43,40 +43,6 @@ func (txs Transactions) HashTreeRoot() (common.Root, error) {
 		merkleizer.New[[32]byte, common.Root](),
 	)
 }
-
-type BartioTransactions = ssz.List[ssz.Vector[ssz.Byte]]
-
-// BartioTransactionsFromBytes creates a Transactions object from a byte slice.
-func BartioTransactionsFromBytes(data [][]byte) *BartioTransactions {
-	return ssz.ListFromElements(
-		// TODO: Move this value to chain spec.
-		constants.MaxTxsPerPayload,
-		//#nosec:G103 // todo fix later.
-		*(*[]ssz.Vector[ssz.Byte])(unsafe.Pointer(&data))...)
-}
-
-type ProperTransactions = ssz.List[*ssz.List[ssz.Byte]]
-
-// ProperTransactionsFromBytes creates a Transactions object from a byte slice.
-func ProperTransactionsFromBytes(data [][]byte) *ProperTransactions {
-	txs := make([]*ssz.List[ssz.Byte], len(data))
-	for i, tx := range data {
-		//nolint:mnd // unhood later.
-		txs[i] = ssz.ByteListFromBytes(tx, 1073741824)
-	}
-
-	y := ssz.ListFromElements(constants.MaxTxsPerPayload, txs...)
-
-	return ssz.ListFromElements(
-		constants.MaxTxsPerPayload,
-		y.Elements()...,
-	)
-}
-
-// TxsMerkleizer is a ssz merkleizer used for transactions.
-//
-// TODO: make the ChainSpec a generic on this type.
-type TxsMerkleizer merkleizer.Merkleizer[[32]byte, common.Root]
 
 // HashTreeRootWith returns the hash tree root of the Transactions list
 // using the given merkleizer.
@@ -96,4 +62,27 @@ func (txs Transactions) HashTreeRootWith(
 	}
 
 	return merkleizer.MerkleizeListComposite(roots, constants.MaxTxsPerPayload)
+}
+
+// TODO: Remove and deprecate this type once migrated to ProperTransactions.
+type BartioTransactions = ssz.List[ssz.Vector[ssz.Byte]]
+
+// BartioTransactionsFromBytes creates a Transactions object from a byte slice.
+func BartioTransactionsFromBytes(data [][]byte) *BartioTransactions {
+	return ssz.ListFromElements(
+		constants.MaxTxsPerPayload,
+		//#nosec:G103 // todo fix later.
+		*(*[]ssz.Vector[ssz.Byte])(unsafe.Pointer(&data))...)
+}
+
+type ProperTransactions = ssz.List[*ssz.List[ssz.Byte]]
+
+// ProperTransactionsFromBytes creates a Transactions object from a byte slice.
+func ProperTransactionsFromBytes(data [][]byte) *ProperTransactions {
+	txs := make([]*ssz.List[ssz.Byte], len(data))
+	for i, tx := range data {
+		txs[i] = ssz.ByteListFromBytes(tx, constants.MaxBytesPerTx)
+	}
+
+	return ssz.ListFromElements(constants.MaxTxsPerPayload, txs...)
 }
--- mod/primitives/pkg/merkle/hasher.go
+++ mod/primitives/pkg/merkle/hasher.go
@@ -21,7 +21,6 @@
 package merkle
 
 import (
-	"runtime"
 	"unsafe"
 
 	"github.com/berachain/beacon-kit/mod/errors"
@@ -29,7 +28,6 @@ import (
 	"github.com/berachain/beacon-kit/mod/primitives/pkg/math"
 	"github.com/berachain/beacon-kit/mod/primitives/pkg/merkle/zero"
 	"github.com/prysmaticlabs/gohashtree"
-	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -132,77 +130,91 @@ func (rh *RootHasher[RootT]) NewRootWithDepth(
 func BuildParentTreeRoots[RootT ~[32]byte](
 	outputList, inputList []RootT,
 ) error {
-	return BuildParentTreeRootsWithNRoutines(
+	return gohashtree.Hash(
 		//#nosec:G103 // on purpose.
 		*(*[][32]byte)(unsafe.Pointer(&outputList)),
 		//#nosec:G103 // on purpose.
 		*(*[][32]byte)(unsafe.Pointer(&inputList)),
-		runtime.GOMAXPROCS(0)-1,
+		// runtime.GOMAXPROCS(0)-1,
 	)
 }
 
-// BuildParentTreeRootsWithNRoutines optimizes hashing of a list of roots
-// using CPU-specific vector instructions and parallel processing. This
-// method adapts to the host machine's hardware for potential performance
-// gains over sequential hashing.
-//
-// TODO: We do not use generics here due to the gohashtree library not
-// supporting generics.
-func BuildParentTreeRootsWithNRoutines(
-	outputList, inputList [][32]byte, n int,
+// TODO: Reparallelize this function.
+func BuildParentTreeRootsWithNRoutines[RootT ~[32]byte](
+	outputList, inputList []RootT, _ int,
 ) error {
-	// Validate input list length.
-	inputLength := len(inputList)
-	if inputLength%2 != 0 {
-		return ErrOddLengthTreeRoots
-	}
-
-	// Build output variables
-	outputLength := inputLength / two
-
-	// If the input list is small, hash it using the default method since
-	// the overhead of parallelizing the hashing process is not worth it.
-	if inputLength < MinParallelizationSize {
-		//#nosec:G103 // used of unsafe calls should be audited.
-		return gohashtree.Hash(outputList, inputList)
-	}
-
-	// Otherwise parallelize the hashing process for large inputs.
-	// Take the max(n, 1) to prevent division by 0.
-	groupSize := inputLength / (two * max(n, 1))
-	twiceGroupSize := two * groupSize
-	eg := new(errgroup.Group)
-
-	// if n is 0 the parallelization is disabled and the whole inputList is
-	// hashed in the main goroutine at the end of this function.
-	for j := range n + 1 {
-		eg.Go(func() error {
-			// inputList:  [-------------------2*groupSize-------------------]
-			//              ^                  ^                    ^        ^
-			//              |                  |                    |        |
-			// j*2*groupSize   (j+1)*2*groupSize    (j+2)*2*groupSize   End
-			//
-			// outputList: [---------groupSize---------]
-			//              ^                         ^
-			//              |                         |
-			//             j*groupSize         (j+1)*groupSize
-			//
-			// Each goroutine processes a segment of inputList that is twice as
-			// large as the segment it fills in outputList. This is because the
-			// hash
-			// operation reduces the
-			// size of the input by half.
-			// Define the segment of the inputList each goroutine will process.
-			segmentStart := j * twiceGroupSize
-			segmentEnd := min((j+1)*twiceGroupSize, inputLength)
-
-			return gohashtree.Hash(
-				outputList[j*groupSize:min((j+1)*groupSize, outputLength)],
-				inputList[segmentStart:segmentEnd],
-			)
-		})
-	}
-
-	// Wait for all goroutines to complete.
-	return eg.Wait()
+	return gohashtree.Hash(
+		//#nosec:G103 // on purpose.
+		*(*[][32]byte)(unsafe.Pointer(&outputList)),
+		//#nosec:G103 // on purpose.
+		*(*[][32]byte)(unsafe.Pointer(&inputList)),
+	)
 }
+
+// TODO: Revive this function.
+//
+// // BuildParentTreeRootsWithNRoutines optimizes hashing of a list of roots
+// // using CPU-specific vector instructions and parallel processing. This
+// // method adapts to the host machine's hardware for potential performance
+// // gains over sequential hashing.
+// //
+// // TODO: We do not use generics here due to the gohashtree library not
+// // supporting generics.
+// func BuildParentTreeRootsWithNRoutines(
+// 	outputList, inputList [][32]byte, n int,
+// ) error {
+// 	// Validate input list length.
+// 	inputLength := len(inputList)
+// 	if inputLength%2 != 0 {
+// 		return ErrOddLengthTreeRoots
+// 	}
+
+// 	// Build output variables
+// 	outputLength := inputLength / two
+
+// 	// If the input list is small, hash it using the default method since
+// 	// the overhead of parallelizing the hashing process is not worth it.
+// 	if inputLength < MinParallelizationSize {
+// 		//#nosec:G103 // used of unsafe calls should be audited.
+// 		return gohashtree.Hash(outputList, inputList)
+// 	}
+
+// 	// Otherwise parallelize the hashing process for large inputs.
+// 	// Take the max(n, 1) to prevent division by 0.
+// 	groupSize := inputLength / (two * max(n, 1))
+// 	twiceGroupSize := two * groupSize
+// 	eg := new(errgroup.Group)
+
+// 	// if n is 0 the parallelization is disabled and the whole inputList is
+// 	// hashed in the main goroutine at the end of this function.
+// 	for j := range n + 1 {
+// 		eg.Go(func() error {
+// 			// inputList:  [-------------------2*groupSize-------------------] 			//
+//            ^                  ^                    ^        ^ 			//
+//    |                  |                    |        |
+// 			// j*2*groupSize   (j+1)*2*groupSize    (j+2)*2*groupSize   End
+// 			//
+// 			// outputList: [---------groupSize---------]
+// 			//              ^                         ^
+// 			//              |                         |
+// 			//             j*groupSize         (j+1)*groupSize
+// 			//
+// 			// Each goroutine processes a segment of inputList that is twice as 			//
+// large as the segment it fills in outputList. This is because the
+// 			// hash
+// 			// operation reduces the
+// 			// size of the input by half.
+// 			// Define the segment of the inputList each goroutine will process.
+// 			segmentStart := j * twiceGroupSize
+// 			segmentEnd := min((j+1)*twiceGroupSize, inputLength)
+
+// 			return gohashtree.Hash(
+// 				outputList[j*groupSize:min((j+1)*groupSize, outputLength)],
+// 				inputList[segmentStart:segmentEnd],
+// 			)
+// 		})
+// 	}
+
+// 	// Wait for all goroutines to complete.
+// 	return eg.Wait()
+// }
