2b03b6131fab68d9f2877d7d52a9d695a1f1664f
itsdevbear <itsdevbear@berachain.com>
2024-07-04 01:33:02 -0400

    chore(primitives): cleanup deadcode

--- mod/primitives/pkg/bytes/utils.go
+++ mod/primitives/pkg/bytes/utils.go
@@ -85,44 +85,6 @@ func FromHex(input string) ([]byte, error) {
 	return h, nil
 }
 
-// SafeCopy creates a copy of the provided byte slice. If the input slice is
-// non-nil and has a length of 32 bytes, it assumes the slice represents a hash
-// and copies it into a fixed-size array before returning a slice of that array.
-// For other non-nil slices, it returns a dynamically allocated copy. If the
-// input slice is nil, it returns nil.
-func SafeCopy(src []byte) []byte {
-	if src == nil {
-		return nil
-	}
-
-	//nolint:mnd // 32 bytes.
-	if len(src) == 32 {
-		var copied [32]byte
-		copy(copied[:], src)
-		return copied[:]
-	}
-
-	copied := make([]byte, len(src))
-	copy(copied, src)
-	return copied
-}
-
-// SafeCopy2D creates a copy of a two-dimensional byte slice. It iterates over
-// the outer slice, copying each inner slice using SafeCopy. If the input is
-// non-nil, it returns a copy of the
-// two-dimensional slice. If the input is nil, it returns nil.
-func SafeCopy2D(src [][]byte) [][]byte {
-	if src == nil {
-		return nil
-	}
-
-	copied := make([][]byte, len(src))
-	for i, s := range src {
-		copied[i] = SafeCopy(s)
-	}
-	return copied
-}
-
 // CopyAndReverseEndianess will copy the input byte slice and return the
 // flipped version of it.
 func CopyAndReverseEndianess(input []byte) []byte {
@@ -141,16 +103,6 @@ func ExtendToSize(slice []byte, length int) []byte {
 		return slice
 	}
 	return append(slice, make([]byte, length-len(slice))...)
-}
-
-// PrependExtendToSize extends a byte slice to a specified length by
-// prepending zero bytes. It returns the original slice if it's
-// already larger.
-func PrependExtendToSize(slice []byte, length int) []byte {
-	if len(slice) >= length {
-		return slice
-	}
-	return append(make([]byte, length-len(slice)), slice...)
 }
 
 // UnmarshalFixedJSON decodes the input as a string with 0x prefix. The length
