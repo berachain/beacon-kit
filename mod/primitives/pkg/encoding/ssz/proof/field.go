e4fbaa9d75d60be8030b2ed48f91760fc53279e2
itsdevbear <itsdevbear@berachain.com>
2024-07-07 13:55:07 -0400

    bet

--- mod/primitives/pkg/encoding/ssz/proof/field.go
+++ mod/primitives/pkg/encoding/ssz/proof/field.go
@@ -3,10 +3,5 @@ package proof
 // Field represents a field in a container.
 type Field struct {
 	SSZType
-	name string
-}
-
-// GetName returns the name of the field.
-func (f Field) GetName() string {
-	return f.name
+	Name string
 }
