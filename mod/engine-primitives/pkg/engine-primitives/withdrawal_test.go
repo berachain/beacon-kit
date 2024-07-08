f82f3ab4f28a9b411339ebe67471366f26414c1d
Tuan Tran <tropicaldog17@gmail.com>
2024-07-08 16:24:19 +0700

    bet

--- mod/engine-primitives/pkg/engine-primitives/withdrawal_test.go
+++ mod/engine-primitives/pkg/engine-primitives/withdrawal_test.go
@@ -21,6 +21,9 @@
 package engineprimitives_test
 
 import (
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/constants"
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/encoding/ssz/types"
+	"github.com/ethereum/go-ethereum/common"
 	"testing"
 
 	engineprimitives "github.com/berachain/beacon-kit/mod/engine-primitives/pkg/engine-primitives"
@@ -31,12 +34,7 @@ import (
 )
 
 func TestWithdrawal(t *testing.T) {
-	withdrawal := engineprimitives.Withdrawal{
-		Index:     math.U64(1),
-		Validator: math.ValidatorIndex(1),
-		Address:   gethprimitives.ExecutionAddress{1, 2, 3, 4, 5},
-		Amount:    math.Gwei(1000),
-	}
+	withdrawal := (&engineprimitives.Withdrawal{}).New(math.U64(1), math.ValidatorIndex(1), gethprimitives.ExecutionAddress{1, 2, 3, 4, 5}, math.Gwei(1000))
 
 	require.Equal(t, math.U64(1), withdrawal.GetIndex())
 	require.Equal(t, math.ValidatorIndex(1), withdrawal.GetValidatorIndex())
@@ -128,4 +126,32 @@ func TestWithdrawal_HashTreeRoot(t *testing.T) {
 		containerRoot,
 		"Hash tree roots should be equal",
 	)
+}
+
+func TestWithdrawalMethods(t *testing.T) {
+	withdrawal := &engineprimitives.Withdrawal{
+		Index:     math.U64(1),
+		Validator: math.ValidatorIndex(2),
+		Address:   [20]byte{1, 2, 3},
+		Amount:    math.Gwei(100),
+	}
+
+	t.Run("IsFixed", func(t *testing.T) {
+		require.True(t, withdrawal.IsFixed())
+	})
+
+	t.Run("Type", func(t *testing.T) {
+		require.Equal(t, types.Composite, withdrawal.Type())
+	})
+
+	t.Run("ItemLength", func(t *testing.T) {
+		require.Equal(t, uint64(constants.RootLength), withdrawal.ItemLength())
+	})
+
+	t.Run("Getters", func(t *testing.T) {
+		require.Equal(t, math.U64(1), withdrawal.GetIndex())
+		require.Equal(t, math.ValidatorIndex(2), withdrawal.GetValidatorIndex())
+		require.Equal(t, common.Address([20]byte{0x01, 0x02, 0x03}), withdrawal.GetAddress())
+		require.Equal(t, math.U64(100), withdrawal.GetAmount())
+	})
 }
