48e01633db82841d5868f861098743bdd97e30b3
stellrust <gohunter@foxmail.com>
2024-07-15 19:36:31 +0800

    chore: fix some comments for struct field
    Signed-off-by: stellrust <gohunter@foxmail.com>

--- mod/payload/pkg/attributes/types.go
+++ mod/payload/pkg/attributes/types.go
@@ -28,7 +28,7 @@ import (
 
 // BeaconState is an interface for accessing the beacon state.
 type BeaconState[WithdrawalT any] interface {
-	// ExpectedWithrawals returns the expected withdrawals.
+	// ExpectedWithdrawals returns the expected withdrawals.
 	ExpectedWithdrawals() ([]WithdrawalT, error)
 	// GetRandaoMixAtIndex returns the randao mix at the given index.
 	GetRandaoMixAtIndex(index uint64) (common.Root, error)
