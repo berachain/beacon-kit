a551c45f6955729c41bff69883ac8e4997255037
Khanh Hoa <hoa@notional.ventures>
2024-07-16 08:25:50 +0700

    bet

--- mod/consensus-types/pkg/types/block_denebplus_test.go
+++ mod/consensus-types/pkg/types/block_denebplus_test.go
@@ -72,8 +72,8 @@ func generateBeaconBlockDenebPlus() *types.BeaconBlockDenebPlus {
 					},
 				},
 				Graffiti: [32]byte{
-					1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-					20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+					1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+					18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
 				},
 				Deposits: []*types.Deposit{},
 			},
@@ -83,7 +83,16 @@ func generateBeaconBlockDenebPlus() *types.BeaconBlockDenebPlus {
 				Transactions: make([][]byte, 0),
 				Withdrawals:  make([]*engineprimitives.Withdrawal, 0),
 			},
-			Attestations:       []*types.AttestationData{},
+			Attestations: []*types.AttestationData{
+				{
+					Slot:  12345,
+					Index: 1,
+					BeaconBlockRoot: [32]byte{
+						1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+						18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+					},
+				},
+			},
 			SlashingInfo:       []*types.SlashingInfo{},
 			BlobKzgCommitments: []eip4844.KZGCommitment{},
 		},
