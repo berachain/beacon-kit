99bb01bfe31aac126de750e7ae4cf4fc0e0dcfa1
Cal Bera <calbera@berachain.com>
2024-07-18 07:52:20 -0400

    cleaner

--- contracts/src/eip4788/BeaconProver.sol
+++ contracts/src/eip4788/BeaconProver.sol
@@ -5,13 +5,19 @@ import { SSZ } from "./SSZ.sol";
 import { Verifier } from "./Verifier.sol";
 import { ValidatorVerifier } from "./ValidatorVerifier.sol";
 
+/// @author Berachain Team
+/// @author [madlabman](https://github.com/madlabman/eip-4788-proof)
 contract BeaconProver is Verifier {
     error InvalidProposer();
 
-    ValidatorVerifier public immutable validatorVerifier;
+    uint64 internal constant VALIDATOR_REGISTRY_LIMIT = 1 << 40;
 
-    constructor(uint256 valGIndex) {
-        validatorVerifier = new ValidatorVerifier(valGIndex);
+    /// @dev Generalized index of the first validator struct root in the
+    /// registry.
+    uint256 public immutable valGIndex;
+
+    constructor(uint256 _valGIndex) {
+        valGIndex = _valGIndex;
     }
 
     // naive, unoptimized implementation
@@ -30,13 +36,34 @@ contract BeaconProver is Verifier {
         }
 
         // Then check that the validator is a validator of the beacon chain during this time.
-        validatorVerifier.proveValidator(validatorProof, validator, validatorIndex, timestamp);
+        proveValidator(validatorProof, validator, validatorIndex, timestamp);
 
         // Finally verify that the block header is the valid block header for this slot & time.
         bytes32 expectedBeaconRoot = getParentBlockRoot(timestamp);
         bytes32 givenBeaconRoot = SSZ.beaconHeaderHashTreeRoot(blockHeader);
         if (expectedBeaconRoot != givenBeaconRoot) {
             revert RootNotFound();
+        }
+    }
+
+    function proveValidator(
+        bytes32[] calldata validatorProof,
+        SSZ.Validator calldata validator,
+        uint64 validatorIndex,
+        uint64 ts
+    )
+        internal
+    {
+        if (validatorIndex >= VALIDATOR_REGISTRY_LIMIT) {
+            revert IndexOutOfRange();
+        }
+
+        uint256 gI = valGIndex + validatorIndex;
+        bytes32 validatorRoot = SSZ.validatorHashTreeRoot(validator);
+        bytes32 blockRoot = getParentBlockRoot(ts);
+
+        if (!SSZ.verifyProof(validatorProof, blockRoot, validatorRoot, gI)) {
+            revert InvalidProof();
         }
     }
 }
