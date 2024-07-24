ab27ac2723cb1cb3508471ca8582df6063be2437
itsdevbear <itsdevbear@berachain.com>
2024-07-24 13:09:04 -0400

    bet

---
+++ mod/consensus-types/pkg/types/v2/beacon_state.go
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2024, Berachain Foundation. All rights reserved.
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
+package types
+
+import (
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/common"
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/math"
+	"github.com/karalabe/ssz"
+)
+
+// Cached static size computed on package init.
+var staticSizeCacheBeaconState = 32 + 8 + (*Fork)(nil).SizeSSZ() + (*BeaconBlockHeader)(nil).SizeSSZ() + 4 + 4 + (*Eth1Data)(nil).SizeSSZ() + 8 + 4 + 4 + 4 + 4 + 8 + 8 + 4 + 8
+
+type BeaconState struct {
+	// Versioning
+	//
+	//nolint:lll
+	GenesisValidatorsRoot common.Root
+	Slot                  math.Slot
+	Fork                  *Fork
+
+	// History
+	LatestBlockHeader *BeaconBlockHeader
+	BlockRoots        []common.Root `ssz-max:"8192"`
+	StateRoots        []common.Root `ssz-max:"8192"`
+
+	// Eth1
+	Eth1Data                     *Eth1Data
+	Eth1DepositIndex             math.U64
+	LatestExecutionPayloadHeader *ExecutionPayloadHeader
+
+	// Registry
+	Validators []*Validator `ssz-max:"1099511627776"`
+	Balances   []uint64     `ssz-max:"1099511627776"`
+
+	// Randomness
+	RandaoMixes []common.Bytes32 `ssz-max:"65536"`
+
+	// Withdrawals
+	NextWithdrawalIndex          math.U64
+	NextWithdrawalValidatorIndex math.ValidatorIndex
+
+	// Slashing
+	Slashings     []math.U64 `ssz-max:"1099511627776"`
+	TotalSlashing math.Gwei
+}
+
+// SizeSSZ returns either the static size of the object if fixed == true, or
+// the total size otherwise.
+func (obj *BeaconState) SizeSSZ(fixed bool) uint32 {
+	var size = uint32(staticSizeCacheBeaconState)
+	if fixed {
+		return size
+	}
+	size += ssz.SizeSliceOfStaticBytes(obj.BlockRoots)
+	size += ssz.SizeSliceOfStaticBytes(obj.StateRoots)
+	size += ssz.SizeDynamicObject(obj.LatestExecutionPayloadHeader)
+	size += ssz.SizeSliceOfStaticObjects(obj.Validators)
+	size += ssz.SizeSliceOfUint64s(obj.Balances)
+	size += ssz.SizeSliceOfStaticBytes(obj.RandaoMixes)
+	size += ssz.SizeSliceOfUint64s(obj.Slashings)
+
+	return size
+}
+
+// DefineSSZ defines how an object is encoded/decoded.
+func (obj *BeaconState) DefineSSZ(codec *ssz.Codec) {
+	// Define the static data (fields and dynamic offsets)
+	ssz.DefineStaticBytes(codec, &obj.GenesisValidatorsRoot)                    // Field  ( 0) -        GenesisValidatorsRoot - 32 bytes
+	ssz.DefineUint64(codec, &obj.Slot)                                          // Field  ( 1) -                         Slot -  8 bytes
+	ssz.DefineStaticObject(codec, &obj.Fork)                                    // Field  ( 2) -                         Fork -  ? bytes (Fork)
+	ssz.DefineStaticObject(codec, &obj.LatestBlockHeader)                       // Field  ( 3) -            LatestBlockHeader -  ? bytes (BeaconBlockHeader)
+	ssz.DefineSliceOfStaticBytesOffset(codec, &obj.BlockRoots, 8192)            // Offset ( 4) -                   BlockRoots -  4 bytes
+	ssz.DefineSliceOfStaticBytesOffset(codec, &obj.StateRoots, 8192)            // Offset ( 5) -                   StateRoots -  4 bytes
+	ssz.DefineStaticObject(codec, &obj.Eth1Data)                                // Field  ( 6) -                     Eth1Data -  ? bytes (Eth1Data)
+	ssz.DefineUint64(codec, &obj.Eth1DepositIndex)                              // Field  ( 7) -             Eth1DepositIndex -  8 bytes
+	ssz.DefineDynamicObjectOffset(codec, &obj.LatestExecutionPayloadHeader)     // Offset ( 8) - LatestExecutionPayloadHeader -  4 bytes
+	ssz.DefineSliceOfStaticObjectsOffset(codec, &obj.Validators, 1099511627776) // Offset ( 9) -                   Validators -  4 bytes
+	ssz.DefineSliceOfUint64sOffset(codec, &obj.Balances, 1099511627776)         // Offset (10) -                     Balances -  4 bytes
+	ssz.DefineSliceOfStaticBytesOffset(codec, &obj.RandaoMixes, 65536)          // Offset (11) -                  RandaoMixes -  4 bytes
+	ssz.DefineUint64(codec, &obj.NextWithdrawalIndex)                           // Field  (12) -          NextWithdrawalIndex -  8 bytes
+	ssz.DefineUint64(codec, &obj.NextWithdrawalValidatorIndex)                  // Field  (13) - NextWithdrawalValidatorIndex -  8 bytes
+	ssz.DefineSliceOfUint64sOffset(codec, &obj.Slashings, 1099511627776)        // Offset (14) -                    Slashings -  4 bytes
+	ssz.DefineUint64(codec, &obj.TotalSlashing)                                 // Field  (15) -                TotalSlashing -  8 bytes
+
+	// Define the dynamic data (fields)
+	ssz.DefineSliceOfStaticBytesContent(codec, &obj.BlockRoots, 8192)            // Field  ( 4) -                   BlockRoots - ? bytes
+	ssz.DefineSliceOfStaticBytesContent(codec, &obj.StateRoots, 8192)            // Field  ( 5) -                   StateRoots - ? bytes
+	ssz.DefineDynamicObjectContent(codec, &obj.LatestExecutionPayloadHeader)     // Field  ( 8) - LatestExecutionPayloadHeader - ? bytes
+	ssz.DefineSliceOfStaticObjectsContent(codec, &obj.Validators, 1099511627776) // Field  ( 9) -                   Validators - ? bytes
+	ssz.DefineSliceOfUint64sContent(codec, &obj.Balances, 1099511627776)         // Field  (10) -                     Balances - ? bytes
+	ssz.DefineSliceOfStaticBytesContent(codec, &obj.RandaoMixes, 65536)          // Field  (11) -                  RandaoMixes - ? bytes
+	ssz.DefineSliceOfUint64sContent(codec, &obj.Slashings, 1099511627776)        // Field  (14) -                    Slashings - ? bytes
+}
---
+++ mod/consensus-types/pkg/types/v2/block.go
@@ -0,0 +1,107 @@
+package types
+
+import (
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/common"
+	"github.com/berachain/beacon-kit/mod/primitives/pkg/math"
+	"github.com/karalabe/ssz"
+)
+
+// BeaconBlockHeaderBase represents the base of a beacon block header.
+type BeaconBlock struct {
+	// Slot represents the position of the block in the chain.
+	Slot math.Slot
+	// ProposerIndex is the index of the validator who proposed the block.
+	ProposerIndex math.ValidatorIndex
+	// ParentBlockRoot is the hash of the parent block
+	ParentBlockRoot common.Root
+	// StateRoot is the hash of the state at the block.
+	StateRoot common.Root
+	// Body is the body of the BeaconBlockDeneb, containing the block's
+	// operations.
+	Body *BeaconBlockBody
+}
+
+// DefineSSZ defines the SSZ encoding for the BeaconBlock object.
+func (b *BeaconBlock) DefineSSZ(codec *ssz.Codec) {
+	ssz.DefineUint64(codec, &b.Slot)
+	ssz.DefineUint64(codec, &b.ProposerIndex)
+	ssz.DefineStaticBytes(codec, &b.ParentBlockRoot)
+	ssz.DefineStaticBytes(codec, &b.StateRoot)
+	ssz.DefineDynamicObjectContent(codec, &b.Body)
+}
+
+// SizeSSZ returns the size of the BeaconBlock object in SSZ encoding.
+func (b *BeaconBlock) SizeSSZ(isFixed bool) uint32 {
+	return 4 + // Slot
+		8 + // ProposerIndex
+		32 + // ParentBlockRoot
+		32 + // StateRoot
+		b.Body.SizeSSZ(isFixed) // Body
+}
+
+// Version returns the version of the BeaconBlock.
+func (b *BeaconBlock) Version() uint32 {
+	return b.Body.version
+}
+
+// IsNil checks if the BeaconBlock instance is nil.
+func (b *BeaconBlock) IsNil() bool {
+	return b == nil
+}
+
+// SetStateRoot sets the state root of the BeaconBlock.
+func (b *BeaconBlock) SetStateRoot(root common.Root) {
+	b.StateRoot = root
+}
+
+// GetBody retrieves the body of the BeaconBlock.
+func (b *BeaconBlock) GetBody() *BeaconBlockBody {
+	return b.Body
+}
+
+// GetSlot retrieves the slot of the BeaconBlock.
+func (b *BeaconBlock) GetSlot() math.Slot {
+	return b.Slot
+}
+
+// GetParentBlockRoot
+func (b *BeaconBlock) GetParentBlockRoot() common.Root {
+	return b.ParentBlockRoot
+}
+
+// GetStateRoot
+func (b *BeaconBlock) GetStateRoot() common.Root {
+	return b.StateRoot
+}
+
+// MarshalSSZ serializes the BeaconBlock into a writer.
+func (b *BeaconBlock) MarshalSSZ() ([]byte, error) {
+	buf := make([]byte, b.SizeSSZ(false))
+	return b.MarshalSSZTo(buf)
+}
+
+// MarshalSSZTo serializes the BeaconBlock into a writer.
+func (b *BeaconBlock) MarshalSSZTo(buf []byte) ([]byte, error) {
+	return buf, ssz.EncodeToBytes(buf, b)
+}
+
+// UnmarshalSSZ deserializes the BeaconBlock from a byte slice.
+func (b *BeaconBlock) UnmarshalSSZ(buf []byte) error {
+	return ssz.DecodeFromBytes(buf, b)
+}
+
+// HashTreeRoot returns the hash tree root of the BeaconBlock.
+func (b *BeaconBlock) HashTreeRoot() ([32]byte, error) {
+	return ssz.HashSequential(b), nil
+}
+
+// GetHeader builds a BeaconBlockHeader from the BeaconBlock.
+func (b *BeaconBlock) GetHeader() *BeaconBlockHeader {
+	return &BeaconBlockHeader{
+		Slot:            b.Slot,
+		ProposerIndex:   b.ProposerIndex,
+		ParentBlockRoot: b.ParentBlockRoot,
+		StateRoot:       b.StateRoot,
+		BodyRoot:        common.Root(b.GetBody().HashTreeRoot()),
+	}
+}
