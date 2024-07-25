561b0a09f5036f82a83746a0a4a9953d3bb706cd
archbear <arch@berachain.com>
2024-07-25 01:02:06 -0400

    yes

--- mod/node/pkg/app/app.go
+++ mod/node/pkg/app/app.go
@@ -5,8 +5,21 @@ import (
 	"github.com/berachain/beacon-kit/mod/runtime/pkg/service"
 )
 
-type App struct {
+type App[
+	StorageBackendT any,
+	StateProcessorT any,
+] struct {
 	logger log.Logger[any]
-	service.Registry
-}
 
+	// Services contained within the service registry
+	// are the services used to push the chain and
+	// related components forward.
+	services service.Registry
+	// The backend is the central data access layer for
+	// the application.
+	backend StorageBackendT
+	// The state processor is the component that is
+	// responsible for transitioning the state of the
+	// chain.
+	stateProcessor StateProcessorT
+}
