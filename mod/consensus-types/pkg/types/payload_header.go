666fd1bfc1db07e2a98747589449ce2f5ac4c599
itsdevbear <itsdevbear@berachain.com>
2024-07-25 23:09:49 -0400

    bet

--- mod/consensus-types/pkg/types/payload_header.go
+++ mod/consensus-types/pkg/types/payload_header.go
@@ -13,7 +13,7 @@
 // LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
 //
 // TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
-// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// AN "AS IS" BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
 // EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
 // TITLE.
@@ -36,27 +36,27 @@ import (
 //nolint:lll
 type ExecutionPayloadHeader struct {
 	version          uint32
-	ParentHash       gethprimitives.ExecutionHash    `json:"parentHash"       ssz-size:"32"  gencodec:"required"`
-	FeeRecipient     gethprimitives.ExecutionAddress `json:"feeRecipient"     ssz-size:"20"  gencodec:"required"`
-	StateRoot        common.Bytes32                  `json:"stateRoot"        ssz-size:"32"  gencodec:"required"`
-	ReceiptsRoot     common.Bytes32                  `json:"receiptsRoot"     ssz-size:"32"  gencodec:"required"`
-	LogsBloom        bytes.B256                      `json:"logsBloom"        ssz-size:"256" gencodec:"required"`
-	Random           common.Bytes32                  `json:"prevRandao"       ssz-size:"32"  gencodec:"required"`
-	Number           math.U64                        `json:"blockNumber"                     gencodec:"required"`
-	GasLimit         math.U64                        `json:"gasLimit"                        gencodec:"required"`
-	GasUsed          math.U64                        `json:"gasUsed"                         gencodec:"required"`
-	Timestamp        math.U64                        `json:"timestamp"                       gencodec:"required"`
-	ExtraData        []byte                          `json:"extraData"                       gencodec:"required" ssz-max:"32"`
-	BaseFeePerGas    math.Wei                        `json:"baseFeePerGas"    ssz-size:"32"  gencodec:"required"`
-	BlockHash        gethprimitives.ExecutionHash    `json:"blockHash"        ssz-size:"32"  gencodec:"required"`
-	TransactionsRoot common.Root                     `json:"transactionsRoot" ssz-size:"32"  gencodec:"required"`
-	WithdrawalsRoot  common.Root                     `json:"withdrawalsRoot"  ssz-size:"32"`
+	ParentHash       gethprimitives.ExecutionHash    `json:"parentHash"        gencodec:"required"`
+	FeeRecipient     gethprimitives.ExecutionAddress `json:"feeRecipient"      gencodec:"required"`
+	StateRoot        common.Bytes32                  `json:"stateRoot"         gencodec:"required"`
+	ReceiptsRoot     common.Bytes32                  `json:"receiptsRoot"      gencodec:"required"`
+	LogsBloom        bytes.B256                      `json:"logsBloom"         gencodec:"required"`
+	Random           common.Bytes32                  `json:"prevRandao"        gencodec:"required"`
+	Number           math.U64                        `json:"blockNumber"       gencodec:"required"`
+	GasLimit         math.U64                        `json:"gasLimit"          gencodec:"required"`
+	GasUsed          math.U64                        `json:"gasUsed"           gencodec:"required"`
+	Timestamp        math.U64                        `json:"timestamp"         gencodec:"required"`
+	ExtraData        []byte                          `json:"extraData"         gencodec:"required"`
+	BaseFeePerGas    math.Wei                        `json:"baseFeePerGas"     gencodec:"required"`
+	BlockHash        gethprimitives.ExecutionHash    `json:"blockHash"         gencodec:"required"`
+	TransactionsRoot common.Root                     `json:"transactionsRoot"  gencodec:"required"`
+	WithdrawalsRoot  common.Root                     `json:"withdrawalsRoot"`
 	BlobGasUsed      math.U64                        `json:"blobGasUsed"`
 	ExcessBlobGas    math.U64                        `json:"excessBlobGas"`
 }
 
 // Empty returns an empty ExecutionPayload for the given fork version.
-func (e *ExecutionPayloadHeader) Empty(
+func (h *ExecutionPayloadHeader) Empty(
 	forkVersion uint32,
 ) *ExecutionPayloadHeader {
 	return &ExecutionPayloadHeader{
@@ -65,25 +65,17 @@ func (e *ExecutionPayloadHeader) Empty(
 }
 
 // NewFromSSZ returns a new ExecutionPayloadHeader from the given SSZ bytes.
-func (e *ExecutionPayloadHeader) NewFromSSZ(
+func (h *ExecutionPayloadHeader) NewFromSSZ(
 	bz []byte, forkVersion uint32,
 ) (*ExecutionPayloadHeader, error) {
-	e = e.Empty(forkVersion)
-	if err := e.UnmarshalSSZ(bz); err != nil {
-		return nil, err
-	}
-	return e, nil
+	return h, h.Empty(forkVersion).UnmarshalSSZ(bz)
 }
 
 // NewFromJSON returns a new ExecutionPayloadHeader from the given JSON bytes.
-func (e *ExecutionPayloadHeader) NewFromJSON(
+func (h *ExecutionPayloadHeader) NewFromJSON(
 	bz []byte, forkVersion uint32,
 ) (*ExecutionPayloadHeader, error) {
-	e = e.Empty(forkVersion)
-	if err := e.UnmarshalJSON(bz); err != nil {
-		return nil, err
-	}
-	return e, nil
+	return h, h.Empty(forkVersion).UnmarshalJSON(bz)
 }
 
 type executionPayloadHeaderMarshaling struct {
@@ -96,64 +88,123 @@ type executionPayloadHeaderMarshaling struct {
 
 // SizeSSZ returns either the static size of the object if fixed == true, or
 // the total size otherwise.
-func (e *ExecutionPayloadHeader) SizeSSZ(fixed bool) uint32 {
+func (h *ExecutionPayloadHeader) SizeSSZ(fixed bool) uint32 {
 	var size = uint32(32 + 20 + 32 + 32 + 256 + 32 + 8 + 8 + 8 + 8 + 4 + 32 + 32 + 32 + 32 + 8 + 8)
 	if fixed {
 		return size
 	}
-	size += ssz.SizeDynamicBytes(e.ExtraData)
+	size += ssz.SizeDynamicBytes(h.ExtraData)
 
 	return size
 }
 
 // DefineSSZ defines how an object is encoded/decoded.
-func (e *ExecutionPayloadHeader) DefineSSZ(codec *ssz.Codec) {
+func (h *ExecutionPayloadHeader) DefineSSZ(codec *ssz.Codec) {
 	// Define the static data (fields and dynamic offsets)
-	ssz.DefineStaticBytes(codec, &e.ParentHash)           // Field  ( 0) -       ParentHash -  32 bytes
-	ssz.DefineStaticBytes(codec, &e.FeeRecipient)         // Field  ( 1) -     FeeRecipient -  20 bytes
-	ssz.DefineStaticBytes(codec, &e.StateRoot)            // Field  ( 2) -        StateRoot -  32 bytes
-	ssz.DefineStaticBytes(codec, &e.ReceiptsRoot)         // Field  ( 3) -     ReceiptsRoot -  32 bytes
-	ssz.DefineStaticBytes(codec, &e.LogsBloom)            // Field  ( 4) -        LogsBloom - 256 bytes
-	ssz.DefineStaticBytes(codec, &e.Random)               // Field  ( 5) -       PrevRandao -  32 bytes
-	ssz.DefineUint64(codec, &e.Number)                    // Field  ( 6) -      BlockNumber -   8 bytes
-	ssz.DefineUint64(codec, &e.GasLimit)                  // Field  ( 7) -         GasLimit -   8 bytes
-	ssz.DefineUint64(codec, &e.GasUsed)                   // Field  ( 8) -          GasUsed -   8 bytes
-	ssz.DefineUint64(codec, &e.Timestamp)                 // Field  ( 9) -        Timestamp -   8 bytes
-	ssz.DefineDynamicBytesOffset(codec, &e.ExtraData, 32) // Offset (10) -        ExtraData -   4 bytes
-	ssz.DefineStaticBytes(codec, &e.BaseFeePerGas)        // Field  (11) -    BaseFeePerGas -  32 bytes
-	ssz.DefineStaticBytes(codec, &e.BlockHash)            // Field  (12) -        BlockHash -  32 bytes
-	ssz.DefineStaticBytes(codec, &e.TransactionsRoot)     // Field  (13) - TransactionsRoot -  32 bytes
-	ssz.DefineStaticBytes(codec, &e.WithdrawalsRoot)      // Field  (14) -   WithdrawalRoot -  32 bytes
-	ssz.DefineUint64(codec, &e.BlobGasUsed)               // Field  (15) -      BlobGasUsed -   8 bytes
-	ssz.DefineUint64(codec, &e.ExcessBlobGas)             // Field  (16) -    ExcessBlobGas -   8 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.ParentHash,
+	) // Field  ( 0) -       ParentHash -  32 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.FeeRecipient,
+	) // Field  ( 1) -     FeeRecipient -  20 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.StateRoot,
+	) // Field  ( 2) -        StateRoot -  32 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.ReceiptsRoot,
+	) // Field  ( 3) -     ReceiptsRoot -  32 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.LogsBloom,
+	) // Field  ( 4) -        LogsBloom - 256 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.Random,
+	) // Field  ( 5) -       PrevRandao -  32 bytes
+	ssz.DefineUint64(
+		codec,
+		&h.Number,
+	) // Field  ( 6) -      BlockNumber -   8 bytes
+	ssz.DefineUint64(
+		codec,
+		&h.GasLimit,
+	) // Field  ( 7) -         GasLimit -   8 bytes
+	ssz.DefineUint64(
+		codec,
+		&h.GasUsed,
+	) // Field  ( 8) -          GasUsed -   8 bytes
+	ssz.DefineUint64(
+		codec,
+		&h.Timestamp,
+	) // Field  ( 9) -        Timestamp -   8 bytes
+	ssz.DefineDynamicBytesOffset(
+		codec,
+		&h.ExtraData,
+		32,
+	) // Offset (10) -        ExtraData -   4 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.BaseFeePerGas,
+	) // Field  (11) -    BaseFeePerGas -  32 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.BlockHash,
+	) // Field  (12) -        BlockHash -  32 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.TransactionsRoot,
+	) // Field  (13) - TransactionsRoot -  32 bytes
+	ssz.DefineStaticBytes(
+		codec,
+		&h.WithdrawalsRoot,
+	) // Field  (14) -   WithdrawalRoot -  32 bytes
+	ssz.DefineUint64(
+		codec,
+		&h.BlobGasUsed,
+	) // Field  (15) -      BlobGasUsed -   8 bytes
+	ssz.DefineUint64(
+		codec,
+		&h.ExcessBlobGas,
+	) // Field  (16) -    ExcessBlobGas -   8 bytes
 
 	// Define the dynamic data (fields)
-	ssz.DefineDynamicBytesContent(codec, &e.ExtraData, 32) // Field  (10) -        ExtraData - ? bytes
+	ssz.DefineDynamicBytesContent(
+		codec,
+		&h.ExtraData,
+		32,
+	) // Field  (10) -        ExtraData - ? bytes
 }
 
-// MarshalSSZ serializes the ExecutionPayloadHeader object into a slice of bytes.
-func (e *ExecutionPayloadHeader) MarshalSSZ() ([]byte, error) {
-	buf := make([]byte, e.SizeSSZ(false))
-	return buf, ssz.EncodeToBytes(buf, e)
+// MarshalSSZ serializes the ExecutionPayloadHeader object into a slice of
+// bytes.
+func (h *ExecutionPayloadHeader) MarshalSSZ() ([]byte, error) {
+	buf := make([]byte, h.SizeSSZ(false))
+	return buf, ssz.EncodeToBytes(buf, h)
 }
 
-// UnmarshalSSZ unmarshals the ExecutionPayloadHeaderDeneb object from a source array
-func (e *ExecutionPayloadHeader) UnmarshalSSZ(bz []byte) error {
-	return ssz.DecodeFromBytes(bz, e)
+// UnmarshalSSZ unmarshals the ExecutionPayloadHeaderDeneb object from a source
+// array
+func (h *ExecutionPayloadHeader) UnmarshalSSZ(bz []byte) error {
+	return ssz.DecodeFromBytes(bz, h)
 }
 
 // HashTreeRootSSZ returns the hash tree root of the ExecutionPayloadHeader.
-func (e *ExecutionPayloadHeader) HashTreeRoot() ([32]byte, error) {
-	return ssz.HashConcurrent(e), nil
+func (h *ExecutionPayloadHeader) HashTreeRoot() ([32]byte, error) {
+	return ssz.HashConcurrent(h), nil
 }
 
 /* -------------------------------------------------------------------------- */
 /*                                   FastSSZ                                  */
 /* -------------------------------------------------------------------------- */
 
-// MarshalSSZTo ssz marshals the ExecutionPayloadHeaderDeneb object to a target array
-func (e *ExecutionPayloadHeader) MarshalSSZTo(dst []byte) ([]byte, error) {
-	bz, err := e.MarshalSSZ()
+// MarshalSSZTo ssz marshals the ExecutionPayloadHeaderDeneb object to a target
+// array
+func (h *ExecutionPayloadHeader) MarshalSSZTo(dst []byte) ([]byte, error) {
+	bz, err := h.MarshalSSZ()
 	if err != nil {
 		return nil, err
 	}
@@ -161,79 +212,86 @@ func (e *ExecutionPayloadHeader) MarshalSSZTo(dst []byte) ([]byte, error) {
 	return dst, nil
 }
 
-// HashTreeRootWith ssz hashes the ExecutionPayloadHeaderDeneb object with a hasher
-func (e *ExecutionPayloadHeader) HashTreeRootWith(hh fastssz.HashWalker) error {
+// HashTreeRootWith ssz hashes the ExecutionPayloadHeaderDeneb object with a
+// hasher
+//
+//nolint:mnd // from fastssz.
+func (h *ExecutionPayloadHeader) HashTreeRootWith(hh fastssz.HashWalker) error {
 	indx := hh.Index()
 
 	// Field (0) 'ParentHash'
-	hh.PutBytes(e.ParentHash[:])
+	hh.PutBytes(h.ParentHash[:])
 
 	// Field (1) 'FeeRecipient'
-	hh.PutBytes(e.FeeRecipient[:])
+	hh.PutBytes(h.FeeRecipient[:])
 
 	// Field (2) 'StateRoot'
-	hh.PutBytes(e.StateRoot[:])
+	hh.PutBytes(h.StateRoot[:])
 
 	// Field (3) 'ReceiptsRoot'
-	hh.PutBytes(e.ReceiptsRoot[:])
+	hh.PutBytes(h.ReceiptsRoot[:])
 
 	// Field (4) 'LogsBloom'
-	if size := len(e.LogsBloom); size != 256 {
-		return fastssz.ErrBytesLengthFn("ExecutionPayloadHeaderDeneb.LogsBloom", size, 256)
+	if size := len(h.LogsBloom); size != 256 {
+		return fastssz.ErrBytesLengthFn(
+			"ExecutionPayloadHeaderDeneb.LogsBloom",
+			size,
+			256,
+		)
 	}
-	hh.PutBytes(e.LogsBloom[:])
+	hh.PutBytes(h.LogsBloom[:])
 
 	// Field (5) 'Random'
-	hh.PutBytes(e.Random[:])
+	hh.PutBytes(h.Random[:])
 
 	// Field (6) 'Number'
-	hh.PutUint64(uint64(e.Number))
+	hh.PutUint64(uint64(h.Number))
 
 	// Field (7) 'GasLimit'
-	hh.PutUint64(uint64(e.GasLimit))
+	hh.PutUint64(uint64(h.GasLimit))
 
 	// Field (8) 'GasUsed'
-	hh.PutUint64(uint64(e.GasUsed))
+	hh.PutUint64(uint64(h.GasUsed))
 
 	// Field (9) 'Timestamp'
-	hh.PutUint64(uint64(e.Timestamp))
+	hh.PutUint64(uint64(h.Timestamp))
 
 	// Field (10) 'ExtraData'
 	{
 		elemIndx := hh.Index()
-		byteLen := uint64(len(e.ExtraData))
+		byteLen := uint64(len(h.ExtraData))
 		if byteLen > 32 {
 			return fastssz.ErrIncorrectListSize
 		}
-		hh.Append(e.ExtraData)
+		hh.Append(h.ExtraData)
 		hh.MerkleizeWithMixin(elemIndx, byteLen, (32+31)/32)
 	}
 
 	// Field (11) 'BaseFeePerGas'
-	hh.PutBytes(e.BaseFeePerGas[:])
+	hh.PutBytes(h.BaseFeePerGas[:])
 
 	// Field (12) 'BlockHash'
-	hh.PutBytes(e.BlockHash[:])
+	hh.PutBytes(h.BlockHash[:])
 
 	// Field (13) 'TransactionsRoot'
-	hh.PutBytes(e.TransactionsRoot[:])
+	hh.PutBytes(h.TransactionsRoot[:])
 
 	// Field (14) 'WithdrawalsRoot'
-	hh.PutBytes(e.WithdrawalsRoot[:])
+	hh.PutBytes(h.WithdrawalsRoot[:])
 
 	// Field (15) 'BlobGasUsed'
-	hh.PutUint64(uint64(e.BlobGasUsed))
+	hh.PutUint64(uint64(h.BlobGasUsed))
 
 	// Field (16) 'ExcessBlobGas'
-	hh.PutUint64(uint64(e.ExcessBlobGas))
+	hh.PutUint64(uint64(h.ExcessBlobGas))
 
 	hh.Merkleize(indx)
 	return nil
 }
 
 // GetTree ssz hashes the ExecutionPayloadHeaderDeneb object
-func (e *ExecutionPayloadHeader) GetTree() (*fastssz.Node, error) {
-	return fastssz.ProofTree(e)
+func (h *ExecutionPayloadHeader) GetTree() (*fastssz.Node, error) {
+	return fastssz.ProofTree(h)
 }
 
 /* -------------------------------------------------------------------------- */
@@ -241,59 +299,59 @@ func (e *ExecutionPayloadHeader) GetTree() (*fastssz.Node, error) {
 /* -------------------------------------------------------------------------- */
 
 // Version returns the version of the ExecutionPayloadHeader.
-func (d *ExecutionPayloadHeader) Version() uint32 {
+func (h *ExecutionPayloadHeader) Version() uint32 {
 	return version.Deneb
 }
 
 // IsNil checks if the ExecutionPayloadHeader is nil.
-func (d *ExecutionPayloadHeader) IsNil() bool {
-	return d == nil
+func (h *ExecutionPayloadHeader) IsNil() bool {
+	return h == nil
 }
 
 // IsBlinded checks if the ExecutionPayloadHeader is blinded.
-func (d *ExecutionPayloadHeader) IsBlinded() bool {
+func (h *ExecutionPayloadHeader) IsBlinded() bool {
 	return false
 }
 
 // GetParentHash returns the parent hash of the ExecutionPayloadHeader.
 func (
-	d *ExecutionPayloadHeader,
+	h *ExecutionPayloadHeader,
 ) GetParentHash() gethprimitives.ExecutionHash {
-	return d.ParentHash
+	return h.ParentHash
 }
 
 // GetFeeRecipient returns the fee recipient address of the
 // ExecutionPayloadHeader.
 //
 //nolint:lll // long variable names.
-func (d *ExecutionPayloadHeader) GetFeeRecipient() gethprimitives.ExecutionAddress {
-	return d.FeeRecipient
+func (h *ExecutionPayloadHeader) GetFeeRecipient() gethprimitives.ExecutionAddress {
+	return h.FeeRecipient
 }
 
 // GetStateRoot returns the state root of the ExecutionPayloadHeader.
-func (d *ExecutionPayloadHeader) GetStateRoot() common.Bytes32 {
-	return d.StateRoot
+func (h *ExecutionPayloadHeader) GetStateRoot() common.Bytes32 {
+	return h.StateRoot
 }
 
 // GetReceiptsRoot returns the receipts root of the ExecutionPayloadHeader.
-func (d *ExecutionPayloadHeader) GetReceiptsRoot() common.Bytes32 {
-	return d.ReceiptsRoot
+func (h *ExecutionPayloadHeader) GetReceiptsRoot() common.Bytes32 {
+	return h.ReceiptsRoot
 }
 
 // GetLogsBloom returns the logs bloom of the ExecutionPayloadHeader.
-func (d *ExecutionPayloadHeader) GetLogsBloom() []byte {
-	return d.LogsBloom[:]
+func (h *ExecutionPayloadHeader) GetLogsBloom() []byte {
+	return h.LogsBloom[:]
 }
 
 // GetPrevRandao returns the previous Randao value of the
 // ExecutionPayloadHeader.
-func (d *ExecutionPayloadHeader) GetPrevRandao() common.Bytes32 {
-	return d.Random
+func (h *ExecutionPayloadHeader) GetPrevRandao() common.Bytes32 {
+	return h.Random
 }
 
 // GetNumber returns the block number of the ExecutionPayloadHeader.
-func (d *ExecutionPayloadHeader) GetNumber() math.U64 {
-	return d.Number
+func (h *ExecutionPayloadHeader) GetNumber() math.U64 {
+	return h.Number
 }
 
 // GetGasLimit returns the gas limit of the ExecutionPayloadHeader.
