0e2dcde94e5b7c6832b02e2aeacfe48cb4cbf32b
Cal Bera <calbera@berachain.com>
2024-07-29 12:27:08 -0400

    bet

--- contracts/src/eip4788/interfaces/IBeaconVerifier.sol
+++ contracts/src/eip4788/interfaces/IBeaconVerifier.sol
@@ -4,19 +4,23 @@ pragma solidity ^0.8.21;
 import { SSZ } from "../SSZ.sol";
 
 interface IBeaconVerifier {
-    /// @dev Emitted when the validator pubkey length is not 48.
-    error InvalidValidatorPubkeyLength();
-
     /// @notice Emitted when the zero validator pubkey Generalized Index is
     /// changed.
     event ZeroValidatorPubkeyGIndexChanged(
         uint256 newZeroValidatorPubkeyGIndex
     );
+    /// @notice Emitted when the execution number Generalized Index is changed.
+    event ExecutionNumberGIndexChanged(uint256 newExecutionNumberGIndex);
 
     /// @dev Generalized Index of the pubkey of the first validator (validator
     /// index of 0) in the registry of the beacon state in the beacon block.
     /// @dev In the Deneb beacon chain fork, this should be 3254554418216960.
     function zeroValidatorPubkeyGIndex() external view returns (uint256);
+
+    /// @dev Generalized Index of the execution number in the beacon state in
+    /// the beacon block.
+    /// @dev In the Deneb beacon chain fork, this should be 5894.
+    function executionNumberGIndex() external view returns (uint256);
 
     /// @notice Get the parent beacon block root from a block's timestamp.
     /// @param timestamp `uint64` timestamp of the block.
@@ -34,11 +38,24 @@ interface IBeaconVerifier {
     /// @param validatorPubkeyProof `bytes32[]` proof of the validator pubkey.
     /// @param validatorPubkey `ValidatorPubkey` to verify.
     /// @param proposerIndex `uint64` validator index of the proposer.
-    function proveBeaconBlockProposer(
+    function verifyBeaconBlockProposer(
         uint64 timestamp,
         bytes32[] calldata validatorPubkeyProof,
         bytes calldata validatorPubkey,
         uint64 proposerIndex
+    )
+        external
+        view;
+
+    /// @notice Verifies the execution number in the beacon block at the given
+    /// timestamp. Reverts if proof invalid.
+    /// @param timestamp `uint64` timestamp of the block.
+    /// @param executionNumberProof `bytes32[]` proof of the execution number.
+    /// @param blockNumber `uint64` execution number of the block.
+    function verifyExecutionNumber(
+        uint64 timestamp,
+        bytes32[] calldata executionNumberProof,
+        uint64 blockNumber
     )
         external
         view;
