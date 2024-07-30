abfecdaeb9e958d35a3f7e803b468f3246b49c43
itsdevbear <itsdevbear@berachain.com>
2024-07-29 21:55:39 -0400

    bet

--- mod/state-transition/pkg/core/state_processor_randao.go
+++ mod/state-transition/pkg/core/state_processor_randao.go
@@ -90,13 +90,9 @@ func (sp *StateProcessor[
 		return err
 	}
 
-	mix, err := sp.buildRandaoMix(prevMix, body.GetRandaoReveal())
-	if err != nil {
-		return err
-	}
-
 	return st.UpdateRandaoMixAtIndex(
-		uint64(epoch)%sp.cs.EpochsPerHistoricalVector(), mix,
+		uint64(epoch)%sp.cs.EpochsPerHistoricalVector(),
+		sp.buildRandaoMix(prevMix, body.GetRandaoReveal()),
 	)
 }
 
@@ -133,14 +129,12 @@ func (sp *StateProcessor[
 ]) buildRandaoMix(
 	mix common.Bytes32,
 	reveal crypto.BLSSignature,
-) (common.Bytes32, error) {
+) common.Bytes32 {
 	newMix := make([]byte, constants.RootLength)
 	revealHash := sha256.Hash(reveal[:])
 	// Apparently this library giga fast? Good project? lmeow.
-	if numXor := xor.Bytes(
+	_ = xor.Bytes(
 		newMix, mix[:], revealHash[:],
-	); numXor != constants.RootLength {
-		return common.Bytes32{}, ErrXorInvalid
-	}
-	return common.Bytes32(newMix), nil
+	)
+	return common.Bytes32(newMix)
 }
