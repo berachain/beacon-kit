9a8fa1a4cf183e8402e3c9e6125ae3d94e0fdc0b
MyronFanQiu <qiufan_glamour@hotmail.com>
2024-07-31 18:08:37 +0800

    add tests for MarshaSSZTo function

--- mod/consensus-types/pkg/types/fork_test.go
+++ mod/consensus-types/pkg/types/fork_test.go
@@ -45,6 +45,13 @@ func TestFork_Serialization(t *testing.T) {
 	err = unmarshalled.UnmarshalSSZ(data)
 	require.NoError(t, err)
 	require.Equal(t, original, &unmarshalled)
+
+	var buf []byte
+	buf, err = original.MarshalSSZTo(buf)
+	require.NoError(t, err)
+
+	// The two byte slices should be equal
+	require.Equal(t, data, buf)
 }
 
 func TestFork_SizeSSZ(t *testing.T) {
