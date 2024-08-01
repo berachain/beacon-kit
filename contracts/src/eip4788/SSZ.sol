d69eba78cb52e147e44d9f7bb693e87eaef0df31
Cal Bera <calbera@berachain.com>
2024-08-01 15:07:58 -0400

    chore(eip4788): Optimize and test BeaconVerifier contract (#1838)

--- contracts/src/eip4788/SSZ.sol
+++ contracts/src/eip4788/SSZ.sol
@@ -32,14 +32,6 @@ library SSZ {
         }
     }
 
-    function uint64HashTreeRoot(uint64 v)
-        internal
-        pure
-        returns (bytes32 root)
-    {
-        return toLittleEndian(uint256(v));
-    }
-
     function addressHashTreeRoot(address v)
         internal
         pure
@@ -48,38 +40,11 @@ library SSZ {
         return bytes32(bytes20(v));
     }
 
-    // forgefmt: disable-next-item
-    function toLittleEndian(uint256 v) internal pure returns (bytes32) {
-        v =
-            ((v &
-                0xFF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00) >>
-                8) |
-            ((v &
-                0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF) <<
-                8);
-        v =
-            ((v &
-                0xFFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000) >>
-                16) |
-            ((v &
-                0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF) <<
-                16);
-        v =
-            ((v &
-                0xFFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000) >>
-                32) |
-            ((v &
-                0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF) <<
-                32);
-        v =
-            ((v &
-                0xFFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF0000000000000000) >>
-                64) |
-            ((v &
-                0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF) <<
-                64);
-        v = (v >> 128) | (v << 128);
-        return bytes32(v);
+    function uint64HashTreeRoot(uint64 v) internal pure returns (bytes32) {
+        v = ((v & 0xFF00FF00FF00FF00) >> 8) | ((v & 0x00FF00FF00FF00FF) << 8);
+        v = ((v & 0xFFFF0000FFFF0000) >> 16) | ((v & 0x0000FFFF0000FFFF) << 16);
+        v = (v >> 32) | (v << 32);
+        return bytes32(uint256(v) << 192);
     }
 
     /// @notice Modified version of `verify` from `MerkleProofLib` to support
