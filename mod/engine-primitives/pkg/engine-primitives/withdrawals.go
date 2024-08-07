582eb32e5ba72e14dc8be8cee08d639c06bf9b8b
itsdevbear <itsdevbear@berachain.com>
2024-08-07 17:18:57 -0400

    bet

--- mod/engine-primitives/pkg/engine-primitives/withdrawals.go
+++ mod/engine-primitives/pkg/engine-primitives/withdrawals.go
@@ -80,7 +80,6 @@ func (w Withdrawals) Len() int { return len(w) }
 // withdrawals that were either
 // constructed by decoding or via public API in this package.
 func (w Withdrawals) EncodeIndex(i int, _w *bytes.Buffer) {
-	// // #nosec:G703 // its okay.
+	// #nosec:G703 // its okay.
 	_ = w[i].EncodeRLP(_w)
-	// rlp.Encode(_w, w[i])
 }
