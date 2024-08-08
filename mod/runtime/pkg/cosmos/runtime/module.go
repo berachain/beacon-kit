3c9589b97104049c566187f251ebc49c6467e612
itsdevbear <itsdevbear@berachain.com>
2024-08-08 13:42:25 -0400

    remove autocli query

--- mod/runtime/pkg/cosmos/runtime/module.go
+++ mod/runtime/pkg/cosmos/runtime/module.go
@@ -26,7 +26,6 @@ import (
 
 	runtimev1alpha1 "cosmossdk.io/api/cosmos/app/runtime/v1alpha1"
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
-	reflectionv1 "cosmossdk.io/api/cosmos/reflection/v1"
 	"cosmossdk.io/core/appmodule"
 	"cosmossdk.io/core/store"
 	"cosmossdk.io/depinject"
@@ -47,30 +46,7 @@ func (m appModule) IsOnePerModuleType() {}
 func (m appModule) IsAppModule()        {}
 
 func (m appModule) AutoCLIOptions() *autocliv1.ModuleOptions {
-	return &autocliv1.ModuleOptions{
-		Query: &autocliv1.ServiceCommandDescriptor{
-			SubCommands: map[string]*autocliv1.ServiceCommandDescriptor{
-				"autocli": {
-					Service: autocliv1.Query_ServiceDesc.ServiceName,
-					RpcCommandOptions: []*autocliv1.RpcCommandOptions{
-						{
-							RpcMethod: "AppOptions",
-							Short:     "Query the custom autocli options",
-						},
-					},
-				},
-				"reflection": {
-					Service: reflectionv1.ReflectionService_ServiceDesc.ServiceName,
-					RpcCommandOptions: []*autocliv1.RpcCommandOptions{
-						{
-							RpcMethod: "FileDescriptors",
-							Short:     "Query the app's protobuf file descriptors",
-						},
-					},
-				},
-			},
-		},
-	}
+	return &autocliv1.ModuleOptions{}
 }
 
 var (
