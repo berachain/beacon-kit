7e4d3aa15df333c3fbde6826dcc6019e0ebbddc9
ocnc-two <ocnc2@berachain.com>
2024-08-08 15:40:21 -0400

    fix color

--- mod/log/pkg/phuslu/formatter.go
+++ mod/log/pkg/phuslu/formatter.go
@@ -111,14 +111,11 @@ func (f *Formatter) printWithColor(
 	b.Bytes = append(b.Bytes, args.Message...)
 	for _, kv := range args.KeyValues {
 		b.Bytes = append(b.Bytes, ' ')
-		// apply the key color if configured
-		if kColor, ok := f.keyColors[kv.Key]; ok {
-			b.Bytes = append(b.Bytes, kColor.Raw()...)
-		}
-		// apply the key+value color if configured (kv color takes precedence
-		// over key color)
+		// apply the key+value color if configured, otherwise apply key color
 		if kvColor, ok := f.keyValColors[kv.Key+kv.Value]; ok {
 			b.Bytes = append(b.Bytes, kvColor.Raw()...)
+		} else if kColor, ok := f.keyColors[kv.Key]; ok {
+			b.Bytes = append(b.Bytes, kColor.Raw()...)
 		}
 		b.Bytes = append(b.Bytes, kv.Key...)
 		b.Bytes = append(b.Bytes, '=')
