9cc1ccf6caef6b0099603532d389daa65d0f9a9c
itsdevbear <itsdevbear@berachain.com>
2024-08-09 15:38:18 -0400

    bet

--- mod/node-core/pkg/components/depinject.go
+++ mod/node-core/pkg/components/depinject.go
@@ -26,13 +26,10 @@ func ProvideEnvironment(
 	logger log.Logger,
 	app *runtime.AppBuilder,
 ) (store.KVStoreService, appmodule.Environment) {
-	var (
-		kvService store.KVStoreService = failingStoreService{}
-	)
 
 	// skips modules that have no store
 	storeKey := ProvideKVStoreKey(app)
-	kvService = kvStoreService{key: storeKey}
+	kvService := kvStoreService{key: storeKey}
 
 	return kvService, appmodule.Environment{
 		Logger: logger.With(
@@ -54,28 +51,6 @@ type kvStoreService struct {
 
 func (k kvStoreService) OpenKVStore(ctx context.Context) store.KVStore {
 	return newKVStore(sdk.UnwrapSDKContext(ctx).KVStore(k.key))
-}
-
-type failingStoreService struct{}
-
-func (failingStoreService) OpenKVStore(ctx context.Context) store.KVStore {
-	panic(
-		"kv store service not available for this module: verify runtime `skip_store_keys` app config if not expected",
-	)
-}
-
-func (failingStoreService) OpenMemoryStore(ctx context.Context) store.KVStore {
-	panic(
-		"memory kv store service not available for this module: verify runtime `skip_store_keys` app config if not expected",
-	)
-}
-
-func (failingStoreService) OpenTransientStore(
-	ctx context.Context,
-) store.KVStore {
-	panic(
-		"transient kv store service not available for this module: verify runtime `skip_store_keys` app config if not expected",
-	)
 }
 
 // CoreKVStore is a wrapper of Core/Store kvstore interface
