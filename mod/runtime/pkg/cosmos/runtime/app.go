17fe7be80ab753e0106be7437d8ac1f15ccdd3eb
itsdevbear <itsdevbear@berachain.com>
2024-08-09 12:14:37 -0400

    bet

--- mod/runtime/pkg/cosmos/runtime/app.go
+++ mod/runtime/pkg/cosmos/runtime/app.go
@@ -164,14 +164,6 @@ func (a *App) EndBlocker(ctx sdk.Context) (sdk.EndBlock, error) {
 	return a.ModuleManager.EndBlock(ctx)
 }
 
-// Precommiter application updates every commit.
-func (a *App) Precommiter(ctx sdk.Context) {
-	err := a.ModuleManager.Precommit(ctx)
-	if err != nil {
-		panic(err)
-	}
-}
-
 // InitChainer initializes the chain.
 func (a *App) InitChainer(
 	ctx sdk.Context,
