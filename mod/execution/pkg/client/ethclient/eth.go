6b736081084748f430b06de590504ba1c881f6d3
itsdevbear <itsdevbear@berachain.com>
2024-08-12 16:08:00 -0400

    bet

--- mod/execution/pkg/client/ethclient/eth.go
+++ mod/execution/pkg/client/ethclient/eth.go
@@ -108,38 +108,3 @@ func toBlockNumArg(number *big.Int) string {
 	// It's negative and large, which is invalid.
 	return fmt.Sprintf("<invalid %d>", number)
 }
-
-func toCallArg(msg ethereum.CallMsg) interface{} {
-	arg := map[string]interface{}{
-		"from": msg.From,
-		"to":   msg.To,
-	}
-	if len(msg.Data) > 0 {
-		arg["input"] = hexutil.Bytes(msg.Data)
-	}
-	if msg.Value != nil {
-		arg["value"] = (*hexutil.Big)(msg.Value)
-	}
-	if msg.Gas != 0 {
-		arg["gas"] = hexutil.Uint64(msg.Gas)
-	}
-	if msg.GasPrice != nil {
-		arg["gasPrice"] = (*hexutil.Big)(msg.GasPrice)
-	}
-	if msg.GasFeeCap != nil {
-		arg["maxFeePerGas"] = (*hexutil.Big)(msg.GasFeeCap)
-	}
-	if msg.GasTipCap != nil {
-		arg["maxPriorityFeePerGas"] = (*hexutil.Big)(msg.GasTipCap)
-	}
-	if msg.AccessList != nil {
-		arg["accessList"] = msg.AccessList
-	}
-	if msg.BlobGasFeeCap != nil {
-		arg["maxFeePerBlobGas"] = (*hexutil.Big)(msg.BlobGasFeeCap)
-	}
-	if msg.BlobHashes != nil {
-		arg["blobVersionedHashes"] = msg.BlobHashes
-	}
-	return arg
-}
