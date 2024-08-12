ea9206d2513ea4736c13721617b51a22283300a5
Cal Bera <calbera@berachain.com>
2024-08-12 16:16:57 -0400

    revive placeholders

--- mod/node-api/handlers/node/routes.go
+++ mod/node-api/handlers/node/routes.go
@@ -55,7 +55,7 @@ func (h *Handler[ContextT]) RegisterRoutes(
 		{
 			Method:  http.MethodGet,
 			Path:    "/eth/v1/node/version",
-			Handler: h.NotImplemented,
+			Handler: h.Version,
 		},
 		{
 			Method:  http.MethodGet,
