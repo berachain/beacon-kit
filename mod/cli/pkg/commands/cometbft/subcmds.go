d4ea939184140e47012cde5b1f0c368560ab5cf3
ocnc-two <ocnc2@berachain.com>
2024-08-19 15:15:54 -0400

    ermst

--- mod/cli/pkg/commands/cometbft/subcmds.go
+++ mod/cli/pkg/commands/cometbft/subcmds.go
@@ -1,17 +1,61 @@
 package cometbft
 
 import (
-	"github.com/berachain/beacon-kit/mod/cli/pkg/context"
+	"context"
+
 	"github.com/cometbft/cometbft/p2p"
 	pvm "github.com/cometbft/cometbft/privval"
 	cmtversion "github.com/cometbft/cometbft/version"
 	"github.com/spf13/cobra"
 	"sigs.k8s.io/yaml"
 
+	cmtjson "github.com/cometbft/cometbft/libs/json"
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/grpc/cmtservice"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/server"
 )
+
+// StatusCommand returns the command to return the status of the network.
+func StatusCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "status",
+		Short: "Query remote node for status",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			status, err := cmtservice.GetNodeStatus(
+				context.Background(), clientCtx,
+			)
+			if err != nil {
+				return err
+			}
+
+			output, err := cmtjson.Marshal(status)
+			if err != nil {
+				return err
+			}
+
+			outputFormat, _ := cmd.Flags().GetString(flags.FlagOutput)
+			if outputFormat == flags.OutputFormatJSON {
+				clientCtx = clientCtx.WithOutputFormat(flags.OutputFormatJSON)
+			}
+
+			return clientCtx.PrintRaw(output)
+		},
+	}
+
+	cmd.Flags().StringP(flags.FlagNode, "n",
+		"tcp://localhost:26657", "Node to connect to")
+	cmd.Flags().StringP(flags.FlagOutput, "o", "json",
+		"Output format (text|json)")
+
+	return cmd
+}
 
 // ShowNodeIDCmd - ported from CometBFT, dump node ID to stdout
 func ShowNodeIDCmd() *cobra.Command {
@@ -19,7 +63,7 @@ func ShowNodeIDCmd() *cobra.Command {
 		Use:   "show-node-id",
 		Short: "Show this node's ID",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			serverCtx := context.GetServerContextFromCmd(cmd)
+			serverCtx := server.GetServerContextFromCmd(cmd)
 			cfg := serverCtx.Config
 
 			nodeKey, err := p2p.LoadNodeKey(cfg.NodeKeyFile())
@@ -39,11 +83,13 @@ func ShowValidatorCmd() *cobra.Command {
 		Use:   "show-validator",
 		Short: "Show this node's CometBFT validator info",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			serverCtx := context.GetServerContextFromCmd(cmd)
+			serverCtx := server.GetServerContextFromCmd(cmd)
 			cfg := serverCtx.Config
 
-			privValidator := pvm.LoadFilePV(cfg.PrivValidatorKeyFile(),
-				cfg.PrivValidatorStateFile())
+			privValidator := pvm.LoadFilePV(
+				cfg.PrivValidatorKeyFile(),
+				cfg.PrivValidatorStateFile(),
+			)
 			pk, err := privValidator.GetPubKey()
 			if err != nil {
 				return err
@@ -74,13 +120,15 @@ func ShowAddressCmd() *cobra.Command {
 		Use:   "show-address",
 		Short: "Shows this node's CometBFT validator consensus address",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			serverCtx := context.GetServerContextFromCmd(cmd)
+			serverCtx := server.GetServerContextFromCmd(cmd)
 			cfg := serverCtx.Config
 
-			privValidator := pvm.LoadFilePV(cfg.PrivValidatorKeyFile(),
-				cfg.PrivValidatorStateFile())
+			privValidator := pvm.LoadFilePV(
+				cfg.PrivValidatorKeyFile(),
+				cfg.PrivValidatorStateFile(),
+			)
 
-			valConsAddr := (sdk.ConsAddress)(privValidator.GetAddress())
+			valConsAddr := privValidator.GetAddress()
 
 			cmd.Println(valConsAddr.String())
 			return nil
