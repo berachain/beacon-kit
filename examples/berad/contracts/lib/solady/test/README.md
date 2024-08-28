878a063a00286400beb89a48e9496b5c179795c6
Cal Bera <calbera@berachain.com>
2024-08-28 13:27:01 -0400

    bet

--- examples/berad/contracts/lib/solady/test/README.md
+++
@@ -1,5 +0,0 @@
-## WARNING! 
-
-All test files are strictly intended for testing purposes only.
-
-Do NOT copy anything here into production code unless you really know what you are doing.
--- examples/berad/contracts/lib/solady/test/utils/mocks/MockReceiver.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-import {Receiver} from "../../../src/accounts/Receiver.sol";
-
-/// @dev WARNING! This mock is strictly intended for testing purposes only.
-/// Do NOT copy anything here into production code unless you really know what you are doing.
-contract MockReceiver is Receiver {}
