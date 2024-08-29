c0ac57dfdef99728ed9ff9c0e70dd9ee4bde7334
nidhi-singh02 <trippin@berachain.com>
2024-08-29 19:32:31 +0530

    add test for fork and others
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>

--- testing/e2e/e2e_api_test.go
+++ testing/e2e/e2e_api_test.go
@@ -21,6 +21,7 @@
 package e2e_test
 
 import (
+	"fmt"
 	beaconapi "github.com/attestantio/go-eth2-client/api"
 	"github.com/berachain/beacon-kit/mod/node-api/handlers/utils"
 	"github.com/berachain/beacon-kit/testing/e2e/config"
@@ -47,4 +48,57 @@ func (s *BeaconKitE2ESuite) TestBeaconAPIStartup() {
 	s.Require().NoError(err)
 	s.Require().NotEmpty(stateRootResp)
 	s.Require().False(stateRootResp.Data.IsZero())
+	fmt.Println("stateRootResp", stateRootResp.Data.String())
+
+	// Ensure the state fork is not nil.
+	//Error: Received unexpected error: failed to unmarshal data : previous version missing
+
+	stateForkResp, err := client.Fork(s.Ctx(), &beaconapi.ForkOpts{
+		State: stateRootResp.Data.String(),
+	})
+	fmt.Println("err", err)
+	s.Require().NoError(err)
+	s.Require().NotNil(stateForkResp)
+
+	// Ensure the state validators are not nil.
+	//stateValidatorsResp, err := client.Validators(s.Ctx(), &beaconapi.ValidatorsOpts{
+	//	State:   utils.StateIDHead,
+	//	Indices: make([]phase0.ValidatorIndex, 0),
+	//PubKeys:         make([]phase0.BLSPubKey, 0),
+	//ValidatorStates: make([]apiv1.ValidatorState, 0),
+	//})
+	//s.Require().NoError(err)
+	//s.Require().NotNil(stateValidatorsResp)
+	////s.Require().NotEmpty(stateValidatorsResp.Data)
+
+	// Ensure the state validator are not nil.
+	//stateValidatorResp, err := client.Validator(s.Ctx(), &beaconapi.ValidatorsOpts{
+	//	State: utils.StateIDHead,
+	//})
+	//s.Require().NoError(err)
+	//s.Require().NotNil(stateValidatorResp)
+	//s.Require().NotNil(stateValidatorResp.Data)
+
+	// {"level":"debug","service":"client",
+	// "impl":"http","id":"36162dc1","address":"http://0.0.0.0:52501",
+	//"endpoint":"/eth/v1/beacon/states/0/validator_balances",
+	//"status_code":400,"status_code":400,
+	//"response":{"code":400,"message":"invalid request"},"time":"2024-08-29T15:58:40+05:30","message":"POST failed"}
+	// Ensure the state validator balances are not nil.
+	//stateValidatorBalanceResp, err := client.ValidatorBalances(s.Ctx(), &beaconapi.ValidatorBalancesOpts{
+	//	State: "0",
+	//	//Indices: []phase0.ValidatorIndex{},
+	//	//PubKeys: nil,
+	//})
+	//s.Require().NoError(err)
+	//s.Require().NotNil(stateValidatorBalanceResp)
+
+	// json: cannot unmarshal string into Go value of type http.beaconStateRandaoJSON
+	// Ensure beacon randao is not nil.
+	//stateRandaoResp, err := client.BeaconStateRandao(s.Ctx(), &beaconapi.BeaconStateRandaoOpts{
+	//	State: utils.StateIDHead,
+	//})
+	//s.Require().NoError(err)
+	//s.Require().NotNil(stateRandaoResp)
+
 }
