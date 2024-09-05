5217fd21b18853cf7bb967bdc1304e28b98ff55b
Cal Bera <calbera@berachain.com>
2024-09-05 12:15:12 -0400

    block proposer test

--- beacond/cmd/main.go
+++ beacond/cmd/main.go
@@ -29,6 +29,7 @@ import (
 	nodebuilder "github.com/berachain/beacon-kit/mod/node-core/pkg/builder"
 	nodecomponents "github.com/berachain/beacon-kit/mod/node-core/pkg/components"
 	nodetypes "github.com/berachain/beacon-kit/mod/node-core/pkg/types"
+	"github.com/berachain/beacon-kit/beacond/cmd/types"
 	"go.uber.org/automaxprocs/maxprocs"
 )
 
@@ -44,7 +45,7 @@ func run() error {
 	// Build the node using the node-core.
 	nb := nodebuilder.New(
 		// Set the Runtime Components to the Default.
-		nodebuilder.WithComponents[Node, *Logger, *LoggerConfig](
+		nodebuilder.WithComponents[Node, *types.Logger, *types.LoggerConfig](
 			DefaultComponents(),
 		),
 	)
@@ -52,15 +53,19 @@ func run() error {
 	// Build the root command using the builder
 	cb := clibuilder.New(
 		// Set the Name to the Default.
-		clibuilder.WithName[Node, *ExecutionPayload, *Logger](
+		clibuilder.WithName[Node, *types.ExecutionPayload, *types.Logger](
 			"beacond",
 		),
 		// Set the Description to the Default.
-		clibuilder.WithDescription[Node, *ExecutionPayload, *Logger](
+		clibuilder.WithDescription[
+			Node, *types.ExecutionPayload, *types.Logger,
+		](
 			"A basic beacon node, usable most standard networks.",
 		),
 		// Set the Runtime Components to the Default.
-		clibuilder.WithComponents[Node, *ExecutionPayload, *Logger](
+		clibuilder.WithComponents[
+			Node, *types.ExecutionPayload, *types.Logger,
+		](
 			append(
 				clicomponents.DefaultClientComponents(),
 				// TODO: remove these, and eventually pull cfg and chainspec
@@ -70,7 +75,7 @@ func run() error {
 		),
 		// Set the NodeBuilderFunc to the NodeBuilder Build.
 		clibuilder.WithNodeBuilderFunc[
-			Node, *ExecutionPayload, *Logger,
+			Node, *types.ExecutionPayload, *types.Logger,
 		](nb.Build),
 	)
 
