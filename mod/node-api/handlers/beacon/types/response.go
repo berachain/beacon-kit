23c994d65ab90c9d11ca954019ed79ba286834e0
nidhi-singh02 <trippin@berachain.com>
2024-09-17 12:41:47 +0530

    define type outside generic method
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>

--- mod/node-api/handlers/beacon/types/response.go
+++ mod/node-api/handlers/beacon/types/response.go
@@ -71,25 +71,25 @@ type ValidatorData[ValidatorT Validator] struct {
 	Validator ValidatorT `json:"validator"`
 }
 
+type ValidatorJSON struct {
+	PublicKey                  string `json:"pubkey"`
+	WithdrawalCredentials      string `json:"withdrawal_credentials"`
+	EffectiveBalance           string `json:"effective_balance"`
+	Slashed                    bool   `json:"slashed"`
+	ActivationEligibilityEpoch string `json:"activation_eligibility_epoch"`
+	ActivationEpoch            string `json:"activation_epoch"`
+	ExitEpoch                  string `json:"exit_epoch"`
+	WithdrawableEpoch          string `json:"withdrawable_epoch"`
+}
+
+type ResponseJSON struct {
+	Index     string        `json:"index"`
+	Balance   string        `json:"balance"`
+	Status    string        `json:"status"`
+	Validator ValidatorJSON `json:"validator"`
+}
+
 func (vd ValidatorData[ValidatorT]) MarshalJSON() ([]byte, error) {
-	type ValidatorJSON struct {
-		PublicKey                  string `json:"pubkey"`
-		WithdrawalCredentials      string `json:"withdrawal_credentials"`
-		EffectiveBalance           string `json:"effective_balance"`
-		Slashed                    bool   `json:"slashed"`
-		ActivationEligibilityEpoch string `json:"activation_eligibility_epoch"`
-		ActivationEpoch            string `json:"activation_epoch"`
-		ExitEpoch                  string `json:"exit_epoch"`
-		WithdrawableEpoch          string `json:"withdrawable_epoch"`
-	}
-
-	type ResponseJSON struct {
-		Index     string        `json:"index"`
-		Balance   string        `json:"balance"`
-		Status    string        `json:"status"`
-		Validator ValidatorJSON `json:"validator"`
-	}
-
 	withdrawalCredentials := vd.Validator.GetWithdrawalCredentials()
 
 	return json.Marshal(ResponseJSON{
