4d415aed3eedbc4862eb864e7904474c024531a2
Cal Bera <calbera@berachain.com>
2024-11-20 19:09:56 +0100

    69420

--- mod/state-transition/pkg/core/state/statedb.go
+++ mod/state-transition/pkg/core/state/statedb.go
@@ -29,8 +29,9 @@ import (
 
 const (
 	// EVMMintingSlot is the slot at which we force a single withdrawal to
-	// mint EVMMintingAmount EVM tokens to EVMMintingAddress.
-	EVMMintingSlot uint64 = 308000
+	// mint EVMMintingAmount EVM tokens to EVMMintingAddress. No other
+	// withdrawals are inserted at this slot.
+	EVMMintingSlot uint64 = 69420
 
 	// EVMMintingAddress is the address at which we mint EVM tokens to.
 	EVMMintingAddress = "0x8a73D1380345942F1cb32541F1b19C40D8e6C94B"
