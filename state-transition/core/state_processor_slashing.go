1cd77bff6e0dcb778b66e676a30bdcbde72e2d49
aBear <abear@berachain.com>
2024-12-07 22:55:27 -0500

    simplified slashing reset

--- state-transition/core/state_processor_slashing.go
+++ state-transition/core/state_processor_slashing.go
@@ -21,6 +21,7 @@
 package core
 
 import (
+	"github.com/berachain/beacon-kit/config/spec"
 	"github.com/berachain/beacon-kit/primitives/math"
 )
 
@@ -33,108 +34,28 @@ func (sp *StateProcessor[
 ]) processSlashingsReset(
 	st BeaconStateT,
 ) error {
-	// Get the current epoch.
+	// processSlashingsReset does not really do anything right now.
+	// However we cannot simply drop it because appHash accounts
+	// for the list of operations carried out over the state
+	// even if the operations does not affect the final state
+	// (currently no slashing on beaconKit)
+
 	slot, err := st.GetSlot()
 	if err != nil {
 		return err
+	}
+
+	switch {
+	case sp.cs.DepositEth1ChainID() == spec.BartioChainID:
+		// go head doing the processing
+	case sp.cs.DepositEth1ChainID() == spec.BoonetEth1ChainID &&
+		slot < math.U64(spec.BoonetFork3Height):
+		// go head doing the processing
+	default:
+		// no real need to perform slashing reset
+		return nil
 	}
 
 	index := (sp.cs.SlotToEpoch(slot).Unwrap() + 1) % sp.cs.EpochsPerSlashingsVector()
 	return st.UpdateSlashingAtIndex(index, 0)
-}
-
-// processProposerSlashing as defined in the Ethereum 2.0 specification.
-// https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#proposer-slashings
-//
-//nolint:lll,unused // will be used later
-func (sp *StateProcessor[
-	_, _, _, BeaconStateT, _, _, _, _, _, _, _, _, _, _, _, _, _,
-]) processProposerSlashing(
-	_ BeaconStateT,
-	// ps ProposerSlashing,
-) error {
-	return nil
-}
-
-// processSlashings as defined in the Ethereum 2.0 specification.
-// https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#slashings
-//
-// processSlashings processes the slashings and ensures they match the local
-// state.
-//
-//nolint:lll,unused // will be used later
-func (sp *StateProcessor[
-	_, _, _, BeaconStateT, _, _, _, _, _, _, _, _, _, _, _, _, _,
-]) processSlashings(
-	st BeaconStateT,
-) error {
-	totalBalance, err := st.GetTotalActiveBalances(sp.cs.SlotsPerEpoch())
-	if err != nil {
-		return err
-	}
-
-	totalSlashings, err := st.GetTotalSlashing()
-	if err != nil {
-		return err
-	}
-
-	adjustedTotalSlashingBalance := min(
-		totalSlashings.Unwrap()*sp.cs.ProportionalSlashingMultiplier(),
-		totalBalance.Unwrap(),
-	)
-
-	vals, err := st.GetValidators()
-	if err != nil {
-		return err
-	}
-
-	// Get the current slot.
-	slot, err := st.GetSlot()
-	if err != nil {
-		return err
-	}
-
-	//nolint:mnd // this is in the spec
-	slashableEpoch := (sp.cs.SlotToEpoch(slot).Unwrap() + sp.cs.EpochsPerSlashingsVector()) / 2
-
-	// Iterate through the validators and slash if needed.
-	for _, val := range vals {
-		if val.IsSlashed() &&
-			(slashableEpoch == val.GetWithdrawableEpoch().Unwrap()) {
-			if err = sp.processSlash(
-				st, val,
-				adjustedTotalSlashingBalance,
-				totalBalance.Unwrap(),
-			); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-// processSlash handles the logic for slashing a validator.
-//
-//nolint:unused // will be used later
-func (sp *StateProcessor[
-	_, _, _, BeaconStateT, _, _, _, _, _, _, _, _, ValidatorT, _, _, _, _,
-]) processSlash(
-	st BeaconStateT,
-	val ValidatorT,
-	adjustedTotalSlashingBalance uint64,
-	totalBalance uint64,
-) error {
-	// Calculate the penalty.
-	increment := sp.cs.EffectiveBalanceIncrement()
-	balDivIncrement := val.GetEffectiveBalance().Unwrap() / increment
-	penaltyNumerator := balDivIncrement * adjustedTotalSlashingBalance
-	penalty := penaltyNumerator / totalBalance * increment
-
-	// Get the val index and decrease the balance of the validator.
-	idx, err := st.ValidatorIndexByPubkey(val.GetPubkey())
-	if err != nil {
-		return err
-	}
-
-	return st.DecreaseBalance(idx, math.Gwei(penalty))
 }
