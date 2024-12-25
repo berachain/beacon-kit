b634144967f5586b93bcb207710151ef8dcabd94
Cal Bera <calbera@berachain.com>
2024-12-25 15:10:20 -0800

    remove withdrawals special casing

--- state-transition/core/state/statedb.go
+++ state-transition/core/state/statedb.go
@@ -24,7 +24,6 @@ import (
 	"context"
 
 	"github.com/berachain/beacon-kit/chain-spec/chain"
-	"github.com/berachain/beacon-kit/config/spec"
 	ctypes "github.com/berachain/beacon-kit/consensus-types/types"
 	engineprimitives "github.com/berachain/beacon-kit/engine-primitives/engine-primitives"
 	"github.com/berachain/beacon-kit/errors"
@@ -122,41 +121,18 @@ func (s *StateDB) ExpectedWithdrawals() (engineprimitives.Withdrawals, error) {
 		validator         *ctypes.Validator
 		balance           math.Gwei
 		withdrawalAddress common.ExecutionAddress
-		withdrawals       = make([]*engineprimitives.Withdrawal, 0)
+		maxWithdrawals    = s.cs.MaxWithdrawalsPerPayload()
+		withdrawals       = make([]*engineprimitives.Withdrawal, 0, maxWithdrawals)
 		withdrawal        *engineprimitives.Withdrawal
 	)
+
+	// The first withdrawal is fixed to be the EVM inflation withdrawal.
+	withdrawals = append(withdrawals, s.EVMInflationWithdrawal())
 
 	slot, err := s.GetSlot()
 	if err != nil {
 		return nil, err
 	}
-
-	// Handle special cases wherever it's necessary
-	switch {
-	case s.cs.DepositEth1ChainID() == spec.BartioChainID:
-		// nothing special to do
-
-	case s.cs.DepositEth1ChainID() == spec.BoonetEth1ChainID &&
-		slot == math.U64(spec.BoonetFork1Height):
-		// Slot used to emergency mint EVM tokens on Boonet.
-		withdrawals = append(withdrawals, withdrawal.New(
-			0, // NOT USED
-			0, // NOT USED
-			common.NewExecutionAddressFromHex(EVMMintingAddress),
-			math.Gwei(EVMMintingAmount),
-		))
-		return withdrawals, nil
-
-	case s.cs.DepositEth1ChainID() == spec.BoonetEth1ChainID &&
-		slot < math.U64(spec.BoonetFork2Height):
-		// Boonet inherited the Bartio behaviour pre BoonetFork2Height
-		// nothing specific to do
-
-	default:
-		// The first withdrawal is fixed to be the EVM inflation withdrawal.
-		withdrawals = append(withdrawals, s.EVMInflationWithdrawal())
-	}
-
 	epoch := math.Epoch(slot.Unwrap() / s.cs.SlotsPerEpoch())
 
 	withdrawalIndex, err := s.GetNextWithdrawalIndex()
@@ -196,8 +172,7 @@ func (s *StateDB) ExpectedWithdrawals() (engineprimitives.Withdrawals, error) {
 		// validator.
 		//nolint:gocritic,nestif // ok.
 		if validator.IsFullyWithdrawable(balance, epoch) {
-			withdrawalAddress, err = validator.
-				GetWithdrawalCredentials().ToExecutionAddress()
+			withdrawalAddress, err = validator.GetWithdrawalCredentials().ToExecutionAddress()
 			if err != nil {
 				return nil, err
 			}
@@ -233,36 +208,15 @@ func (s *StateDB) ExpectedWithdrawals() (engineprimitives.Withdrawals, error) {
 
 			// Increment the withdrawal index to process the next withdrawal.
 			withdrawalIndex++
-		} else if s.cs.DepositEth1ChainID() == spec.BartioChainID {
-			// Backward compatibility with Bartio
-			// TODO: Drop this when we drop other Bartio special cases.
-
-			withdrawalAddress, err = validator.
-				GetWithdrawalCredentials().ToExecutionAddress()
-			if err != nil {
-				return nil, err
-			}
-
-			withdrawal = withdrawal.New(
-				math.U64(withdrawalIndex),
-				validatorIndex,
-				withdrawalAddress,
-				0,
-			)
-
-			withdrawals = append(withdrawals, withdrawal)
-			withdrawalIndex++
 		}
 
 		// Cap the number of withdrawals to the maximum allowed per payload.
-		if uint64(len(withdrawals)) == s.cs.MaxWithdrawalsPerPayload() {
+		if uint64(len(withdrawals)) == maxWithdrawals {
 			break
 		}
 
 		// Increment the validator index to process the next validator.
-		validatorIndex = (validatorIndex + 1) % math.ValidatorIndex(
-			totalValidators,
-		)
+		validatorIndex = (validatorIndex + 1) % math.ValidatorIndex(totalValidators)
 	}
 
 	return withdrawals, nil
@@ -270,8 +224,8 @@ func (s *StateDB) ExpectedWithdrawals() (engineprimitives.Withdrawals, error) {
 
 // EVMInflationWithdrawal returns the withdrawal used for EVM balance inflation.
 //
-// NOTE: The withdrawal index and validator index are both set to 0 as they are
-// not used during processing.
+// NOTE: The withdrawal index and validator index are both set to max(uint64) as
+// they are not used during processing.
 func (s *StateDB) EVMInflationWithdrawal() *engineprimitives.Withdrawal {
 	var withdrawal *engineprimitives.Withdrawal
 	return withdrawal.New(
