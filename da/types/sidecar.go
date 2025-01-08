9e6629fdd0f5be2c3a2b492bdf083a92ab9a4a46
Cal Bera <calbera@berachain.com>
2025-01-08 22:21:14 +0100

    undo regression

--- da/types/sidecar.go
+++ da/types/sidecar.go
@@ -77,7 +77,7 @@ func (b *BlobSidecar) HasValidInclusionProof() bool {
 		b.KzgCommitment.HashTreeRoot(),
 		b.InclusionProof,
 		ctypes.KZGInclusionProofDepth,
-		ctypes.KZGOffsetDeneb,
+		ctypes.KZGOffsetDeneb+b.Index,
 		header.BodyRoot,
 	)
 }
