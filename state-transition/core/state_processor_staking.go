8a9259384340701618210d88506b2156748c37d6
crStiv <cryptostiv7@gmail.com>
2025-01-26 23:19:26 +0100

    Update state_processor_staking.go
    Signed-off-by: crStiv <cryptostiv7@gmail.com>

--- state-transition/core/state_processor_staking.go
+++ state-transition/core/state_processor_staking.go
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 //
-// Copyright (C) 2024, Berachain Foundation. All rights reserved.
+// Copyright (C) 2025, Berachain Foundation. All rights reserved.
 // Use of this software is governed by the Business Source License included
 // in the LICENSE file of this repository and at www.mariadb.com/bsl11.
 //
@@ -13,7 +13,7 @@
 // LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
 //
 // TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
-// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// AN "AS IS" BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
 // EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
 // TITLE.
@@ -21,29 +21,26 @@
 package core
 
 import (
-	"github.com/berachain/beacon-kit/config/spec"
+	"context"
+	"fmt"
+
 	ctypes "github.com/berachain/beacon-kit/consensus-types/types"
 	"github.com/berachain/beacon-kit/errors"
 	"github.com/berachain/beacon-kit/primitives/common"
+	"github.com/berachain/beacon-kit/primitives/constants"
 	"github.com/berachain/beacon-kit/primitives/math"
 	"github.com/berachain/beacon-kit/primitives/version"
 	"github.com/berachain/beacon-kit/state-transition/core/state"
 )
 
-// processOperations processes the operations and ensures they match the
-// local state.
-func (sp *StateProcessor[
-	BeaconBlockT, _, BeaconStateT, _, _, _, _,
-]) processOperations(
-	st BeaconStateT,
-	blk BeaconBlockT,
+// processOperations processes the operations and ensures they match the local state.
+func (sp *StateProcessor[_]) processOperations(
+	ctx context.Context, st *state.StateDB, blk *ctypes.BeaconBlock,
 ) error {
-	// Verify that outstanding deposits are processed
-	// up to the maximum number of deposits
-
-	// Unlike Eth 2.0 specs we don't check that
-	// len(body.deposits) ==  min(MAX_DEPOSITS,
-	// state.eth1_data.deposit_count - state.eth1_deposit_index)
+	// Verify that outstanding deposits are processed up to the maximum number of deposits.
+	//
+	// Unlike Eth 2.0 specs we don't check the following:
+	// `len(body.deposits) ==  min(MAX_DEPOSITS, state.eth1_data.deposit_count - state.eth1_deposit_index)`.
 	// Instead we directly compare block deposits with store ones.
 	deposits := blk.GetBody().GetDeposits()
 	if uint64(len(deposits)) > sp.cs.MaxDepositsPerBlock() {
@@ -52,100 +49,53 @@ func (sp *StateProcessor[
 			sp.cs.MaxDepositsPerBlock(), len(deposits),
 		)
 	}
-	if err := sp.validateNonGenesisDeposits(st, deposits); err != nil {
+
+	if err := sp.validateNonGenesisDeposits(
+		ctx, st, deposits, blk.GetBody().GetEth1Data().DepositRoot,
+	); err != nil {
 		return err
 	}
+
 	for _, dep := range deposits {
 		if err := sp.processDeposit(st, dep); err != nil {
 			return err
 		}
 	}
-	return nil
+
+	return st.SetEth1Data(blk.GetBody().Eth1Data)
 }
 
 // processDeposit processes the deposit and ensures it matches the local state.
-func (sp *StateProcessor[
-	_, _, BeaconStateT, _, _, _, _,
-]) processDeposit(
-	st BeaconStateT,
-	dep *ctypes.Deposit,
-) error {
-	slot, err := st.GetSlot()
+func (sp *StateProcessor[_]) processDeposit(st *state.StateDB, dep *ctypes.Deposit) error {
+	eth1DepositIndex, err := st.GetEth1DepositIndex()
 	if err != nil {
 		return err
 	}
 
-	depositIndex := dep.GetIndex().Unwrap()
-	switch {
-	case sp.cs.DepositEth1ChainID() == spec.BartioChainID:
-		// Bartio has a bug which makes DepositEth1ChainID point to
-		// next deposit index, not latest processed deposit index.
-		// We keep it for backward compatibility.
-		depositIndex++
-	case sp.cs.DepositEth1ChainID() == spec.BoonetEth1ChainID &&
-		slot != 0 && slot < math.U64(spec.BoonetFork2Height):
-		// Boonet pre fork2 has a bug which makes DepositEth1ChainID point to
-		// next deposit index, not latest processed deposit index.
-		// We keep it for backward compatibility.
-		depositIndex++
-	default:
-		// Nothing to do. We correctly set the deposit index to the last
-		// processed deposit index.
-	}
-
-	if err = st.SetEth1DepositIndex(depositIndex); err != nil {
+	if err = st.SetEth1DepositIndex(eth1DepositIndex + 1); err != nil {
 		return err
 	}
 
 	sp.logger.Info(
 		"Processed deposit to set Eth 1 deposit index",
-		"deposit_index", depositIndex,
+		"previous", eth1DepositIndex, "new", eth1DepositIndex+1,
 	)
-
-	return sp.applyDeposit(st, dep)
+	if err = sp.applyDeposit(st, dep); err != nil {
+		return fmt.Errorf("failed to apply deposit: %w", err)
+	}
+	return nil
 }
 
 // applyDeposit processes the deposit and ensures it matches the local state.
-func (sp *StateProcessor[
-	_, _, BeaconStateT, _, _, _, _,
-]) applyDeposit(
-	st BeaconStateT,
-	dep *ctypes.Deposit,
-) error {
+func (sp *StateProcessor[_]) applyDeposit(st *state.StateDB, dep *ctypes.Deposit) error {
 	idx, err := st.ValidatorIndexByPubkey(dep.GetPubkey())
 	if err != nil {
-		// If the validator does not exist, we add the validator.
-		// TODO: improve error handling by distinguishing
-		// ErrNotFound from other kind of errors
-		return sp.createValidator(st, dep)
-	}
-
-	// The validator already exist and we need to update its balance.
-	// EffectiveBalance must be updated in processEffectiveBalanceUpdates
-	// However before BoonetFork2Height we mistakenly update EffectiveBalance
-	// every slot. We must preserve backward compatibility so we special case
-	// Boonet to allow proper bootstrapping.
-	slot, err := st.GetSlot()
-	if err != nil {
-		return err
-	}
-	if sp.cs.DepositEth1ChainID() == spec.BoonetEth1ChainID &&
-		slot < math.U64(spec.BoonetFork2Height) {
-		var val *ctypes.Validator
-		val, err = st.ValidatorByIndex(idx)
-		if err != nil {
-			return err
+		if errors.Is(err, errors.ErrNotFound) {
+			sp.logger.Info("Validator does not exist so creating",
+				"pubkey", dep.GetPubkey(), "index", dep.GetIndex(), "deposit_amount", dep.GetAmount())
+			return sp.createValidator(st, dep)
 		}
-
-		updatedBalance := ctypes.ComputeEffectiveBalance(
-			val.GetEffectiveBalance()+dep.GetAmount(),
-			math.Gwei(sp.cs.EffectiveBalanceIncrement()),
-			math.Gwei(sp.cs.MaxEffectiveBalance(false)),
-		)
-		val.SetEffectiveBalance(updatedBalance)
-		if err = st.UpdateValidatorAtIndex(idx, val); err != nil {
-			return err
-		}
+		return fmt.Errorf("failed to get validator index by pubkey: %w", err)
 	}
 
 	// if validator exist, just update its balance
@@ -162,12 +112,7 @@ func (sp *StateProcessor[
 }
 
 // createValidator creates a validator if the deposit is valid.
-func (sp *StateProcessor[
-	_, _, BeaconStateT, _, _, _, _,
-]) createValidator(
-	st BeaconStateT,
-	dep *ctypes.Deposit,
-) error {
+func (sp *StateProcessor[_]) createValidator(st *state.StateDB, dep *ctypes.Deposit) error {
 	// Get the current slot.
 	slot, err := st.GetSlot()
 	if err != nil {
@@ -184,68 +129,53 @@ func (sp *StateProcessor[
 		}
 	}
 
-	// Get the current epoch.
-	epoch := sp.cs.SlotToEpoch(slot)
-
-	// Verify that the deposit has the ETH1 withdrawal credentials.
+	// Check that the deposit has the ETH1 withdrawal credentials.
 	if !dep.HasEth1WithdrawalCredentials() {
-		// Ignore deposits with non-ETH1 withdrawal credentials.
-		sp.logger.Info(
-			"ignoring deposit with non-ETH1 withdrawal credentials",
+		sp.logger.Warn(
+			"adding validator with non-ETH1 withdrawal credentials -- NOT withdrawable",
+			"pubkey", dep.GetPubkey().String(),
 			"deposit_index", dep.GetIndex(),
+			"amount_gwei", dep.GetAmount().Unwrap(),
 		)
-		return nil
+		sp.metrics.incrementValidatorNotWithdrawable()
 	}
 
 	// Verify that the message was signed correctly.
 	err = dep.VerifySignature(
 		ctypes.NewForkData(
-			version.FromUint32[common.Version](
-				sp.cs.ActiveForkVersionForEpoch(epoch),
-			), genesisValidatorsRoot,
+			// Deposits must be signed with GENESIS_FORK_VERSION.
+			version.FromUint32[common.Version](constants.GenesisVersion),
+			genesisValidatorsRoot,
 		),
 		sp.cs.DomainTypeDeposit(),
 		sp.signer.VerifySignature,
 	)
 	if err != nil {
 		// Ignore deposits that fail the signature check.
-		sp.logger.Info(
+		sp.logger.Warn(
 			"failed deposit signature verification",
+			"pubkey", dep.GetPubkey().String(),
 			"deposit_index", dep.GetIndex(),
+			"amount_gwei", dep.GetAmount().Unwrap(),
 			"error", err,
 		)
+		sp.metrics.incrementDepositStakeLost()
 		return nil
 	}
 
 	// Add the validator to the registry.
-	return sp.addValidatorToRegistry(st, dep, slot)
+	return sp.addValidatorToRegistry(st, dep)
 }
 
 // addValidatorToRegistry adds a validator to the registry.
-func (sp *StateProcessor[
-	_, _, BeaconStateT, _, _, _, _,
-]) addValidatorToRegistry(
-	st BeaconStateT,
-	dep *ctypes.Deposit,
-	slot math.Slot,
-) error {
-	var val *ctypes.Validator
-	val = val.New(
+func (sp *StateProcessor[_]) addValidatorToRegistry(st *state.StateDB, dep *ctypes.Deposit) error {
+	val := ctypes.NewValidatorFromDeposit(
 		dep.GetPubkey(),
 		dep.GetWithdrawalCredentials(),
 		dep.GetAmount(),
 		math.Gwei(sp.cs.EffectiveBalanceIncrement()),
-		math.Gwei(sp.cs.MaxEffectiveBalance(
-			state.IsPostFork3(sp.cs.DepositEth1ChainID(), slot),
-		)),
+		math.Gwei(sp.cs.MaxEffectiveBalance()),
 	)
-
-	// TODO: This is a bug that lives on bArtio. Delete this eventually.
-	if sp.cs.DepositEth1ChainID() == spec.BartioChainID {
-		// Note in AddValidatorBartio we implicitly increase
-		// the balance from state st. This is unlike AddValidator.
-		return st.AddValidatorBartio(val)
-	}
 
 	if err := st.AddValidator(val); err != nil {
 		return err
@@ -260,8 +190,7 @@ func (sp *StateProcessor[
 	sp.logger.Info(
 		"Processed deposit to create new validator",
 		"deposit_amount", float64(dep.GetAmount().Unwrap())/math.GweiPerWei,
-		"validator_index", idx,
-		"withdrawal_epoch", val.GetWithdrawableEpoch(),
+		"validator_index", idx, "withdrawal_epoch", val.GetWithdrawableEpoch(),
 	)
 	return nil
 }
