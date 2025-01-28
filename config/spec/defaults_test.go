033dab211c4745834555d0d5596e841ded073496
aBear <abear@berachain.com>
2025-01-27 19:44:11 -0500

    appease linter

--- config/spec/defaults_test.go
+++ config/spec/defaults_test.go
@@ -1,19 +1,21 @@
-package spec
+package spec_test
 
 import (
 	"testing"
 
+	"github.com/berachain/beacon-kit/config/spec"
 	"github.com/berachain/beacon-kit/primitives/bytes"
 	"github.com/stretchr/testify/require"
 )
 
 func TestDomainTypeConversion(t *testing.T) {
-	require.Equal(t, bytes.B4([]byte{0x00, 0x00, 0x00, 0x00}), bytes.FromUint32(defaultDomainTypeProposer))
-	require.Equal(t, bytes.B4([]byte{0x01, 0x00, 0x00, 0x00}), bytes.FromUint32(defaultDomainTypeAttester))
-	require.Equal(t, bytes.B4([]byte{0x02, 0x00, 0x00, 0x00}), bytes.FromUint32(defaultDomainTypeRandao))
-	require.Equal(t, bytes.B4([]byte{0x03, 0x00, 0x00, 0x00}), bytes.FromUint32(defaultDomainTypeDeposit))
-	require.Equal(t, bytes.B4([]byte{0x04, 0x00, 0x00, 0x00}), bytes.FromUint32(defaultDomainTypeVoluntaryExit))
-	require.Equal(t, bytes.B4([]byte{0x05, 0x00, 0x00, 0x00}), bytes.FromUint32(defaultDomainTypeSelectionProof))
-	require.Equal(t, bytes.B4([]byte{0x06, 0x00, 0x00, 0x00}), bytes.FromUint32(defaultDomainTypeAggregateAndProof))
-	require.Equal(t, bytes.B4([]byte{0x00, 0x00, 0x00, 0x01}), bytes.FromUint32(defaultDomainTypeApplicationMask))
+	cs := spec.MainnetChainSpecData()
+	require.Equal(t, bytes.B4([]byte{0x00, 0x00, 0x00, 0x00}), cs.DomainTypeProposer)
+	require.Equal(t, bytes.B4([]byte{0x01, 0x00, 0x00, 0x00}), cs.DomainTypeAttester)
+	require.Equal(t, bytes.B4([]byte{0x02, 0x00, 0x00, 0x00}), cs.DomainTypeRandao)
+	require.Equal(t, bytes.B4([]byte{0x03, 0x00, 0x00, 0x00}), cs.DomainTypeDeposit)
+	require.Equal(t, bytes.B4([]byte{0x04, 0x00, 0x00, 0x00}), cs.DomainTypeVoluntaryExit)
+	require.Equal(t, bytes.B4([]byte{0x05, 0x00, 0x00, 0x00}), cs.DomainTypeSelectionProof)
+	require.Equal(t, bytes.B4([]byte{0x06, 0x00, 0x00, 0x00}), cs.DomainTypeAggregateAndProof)
+	require.Equal(t, bytes.B4([]byte{0x00, 0x00, 0x00, 0x01}), cs.DomainTypeApplicationMask)
 }
