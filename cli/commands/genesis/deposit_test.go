a5044f3a9a868961e1d639e334e0dc35248e7a3c
Rez <rez@berachain.com>
2025-01-30 20:53:29 -0500

    Added working test case

--- cli/commands/genesis/deposit_test.go
+++ cli/commands/genesis/deposit_test.go
@@ -1,26 +1,42 @@
-package genesis
+package genesis_test
 
 import (
 	"testing"
 
+	"github.com/berachain/beacon-kit/cli/commands/genesis"
 	"github.com/berachain/beacon-kit/config/spec"
 	"github.com/berachain/beacon-kit/node-core/components/signer"
 	"github.com/berachain/beacon-kit/primitives/common"
 	"github.com/berachain/beacon-kit/primitives/math"
+	"github.com/berachain/beacon-kit/testing/utils"
 	cmtcfg "github.com/cometbft/cometbft/config"
+	"github.com/cometbft/cometbft/crypto/bls12381"
+	"github.com/cometbft/cometbft/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGenesisDeposit(t *testing.T) {
+	homeDir := utils.MakeTempHomeDir(t)
+	t.Log("Home folder:", homeDir)
+	defer utils.DeleteTempHomeDir(t, homeDir)
+
 	chainSpec, err := spec.DevnetChainSpec()
 	require.NoError(t, err)
 
 	cometConfig := cmtcfg.DefaultConfig()
+
+	cometConfig.SetRoot(homeDir)
+
+	// Forces Comet to Create it
+	cometConfig.NodeKey = "nodekey.json"
+
 	depositAmount := math.Gwei(32000000000)
 	withdrawalAdress := common.NewExecutionAddressFromHex("0x981114102592310C347E61368342DDA67017bf84")
 	outputDocument := ""
 
-	blsSigner := signer.NewBLSSigner(privValKeyFile, privValStateFile)
-	err = AddGenesisDeposit(chainSpec, cometConfig, blsSigner, depositAmount, withdrawalAdress, outputDocument)
+	blsSigner := signer.BLSSigner{PrivValidator: types.NewMockPVWithKeyType(bls12381.KeyType)}
+
+	err = genesis.AddGenesisDeposit(chainSpec, cometConfig, blsSigner, depositAmount, withdrawalAdress, outputDocument)
+
 	require.NoError(t, err)
 }
