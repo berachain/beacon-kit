149c05f57cd1046c6124cbfe8bdb69d6138566ab
aBear <abear@berachain.com>
2025-02-01 15:24:10 -0500

    added UTs

---
+++ payload/builder/payload_test.go
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2025, Berachain Foundation. All rights reserved.
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
+package builder_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/berachain/beacon-kit/config/spec"
+	ctypes "github.com/berachain/beacon-kit/consensus-types/types"
+	engineprimitives "github.com/berachain/beacon-kit/engine-primitives/engine-primitives"
+	"github.com/berachain/beacon-kit/errors"
+	"github.com/berachain/beacon-kit/log/noop"
+	"github.com/berachain/beacon-kit/payload/builder"
+	"github.com/berachain/beacon-kit/payload/cache"
+	"github.com/berachain/beacon-kit/primitives/common"
+	"github.com/berachain/beacon-kit/primitives/eip4844"
+	"github.com/berachain/beacon-kit/primitives/math"
+	statedb "github.com/berachain/beacon-kit/state-transition/core/state"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRetrievePayloadSunnyPath(t *testing.T) {
+	t.Parallel()
+
+	chainSpec, err := spec.MainnetChainSpec()
+	require.NoError(t, err)
+
+	// Create payload builder
+	var (
+		logger = noop.NewLogger[any]()
+		cfg    = &builder.Config{Enabled: true}
+		ee     = &stubExecutionEngine{}
+		cache  = cache.NewPayloadIDCache[[32]byte, math.Slot]()
+		af     = &stubAttributesFactory{}
+	)
+	pb := builder.New(
+		cfg,
+		chainSpec,
+		logger,
+		ee,
+		cache,
+		af,
+	)
+
+	// create inputs and set expectations
+	var (
+		ctx             = context.TODO()
+		slot            = math.Slot(2025)
+		parentBlockRoot = common.Root{0xff, 0xaa}
+		dummyPayloadID  = engineprimitives.PayloadID{0xab}
+
+		res = &ctypes.ExecutionPayloadEnvelope[*engineprimitives.BlobsBundleV1[
+			eip4844.KZGCommitment,
+			eip4844.KZGProof,
+			eip4844.Blob,
+		]]{
+			ExecutionPayload: &ctypes.ExecutionPayload{
+				Withdrawals: engineprimitives.Withdrawals{},
+			},
+			BlobsBundle: &engineprimitives.BlobsBundleV1[
+				eip4844.KZGCommitment,
+				eip4844.KZGProof,
+				eip4844.Blob,
+			]{},
+		}
+	)
+
+	cache.Set(slot, parentBlockRoot, dummyPayloadID)
+	ee.payloadEnvToReturn = res
+
+	payload, err := pb.RetrievePayload(ctx, slot, parentBlockRoot)
+	require.NoError(t, err)
+	require.Equal(t, res, payload)
+}
+
+// HELPERS section
+
+var errStubNotImplemented = errors.New("stub not implemented")
+
+type stubExecutionEngine struct {
+	payloadEnvToReturn ctypes.BuiltExecutionPayloadEnv
+	errToReturn        error
+}
+
+func (ee *stubExecutionEngine) GetPayload(
+	context.Context, *ctypes.GetPayloadRequest,
+) (ctypes.BuiltExecutionPayloadEnv, error) {
+	return ee.payloadEnvToReturn, ee.errToReturn
+}
+
+func (ee *stubExecutionEngine) NotifyForkchoiceUpdate(
+	context.Context, *ctypes.ForkchoiceUpdateRequest,
+) (*engineprimitives.PayloadID, *common.ExecutionHash, error) {
+	return nil, nil, errStubNotImplemented
+}
+
+type stubAttributesFactory struct{}
+
+func (ee *stubAttributesFactory) BuildPayloadAttributes(
+	*statedb.StateDB, math.U64,
+	uint64, [32]byte,
+) (*engineprimitives.PayloadAttributes, error) {
+	return nil, errStubNotImplemented
+}
