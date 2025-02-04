c768d7f7c154d3031ec57c49da0f1c8258616be1
Giuseppe Cocomazzi <giuseppe@asymmetric.re>
2025-02-04 17:02:30 +0100

    Added nil-pointer seeder and related robust nil checks.

--- testing/quick/karalabe_ssz_test.go
+++ testing/quick/karalabe_ssz_test.go
@@ -24,6 +24,7 @@ import (
 	"encoding/json"
 	"math/rand"
 	"reflect"
+	"slices"
 	"testing"
 	"testing/quick"
 
@@ -39,97 +40,117 @@ import (
 
 var concurrencyThreshold uint64 = 65536
 
-func roll(n int, r *rand.Rand) int {
-	k := r.Intn(n)
-	if k%2 == 0 {
-		return 0
-	}
-	return k
-}
-
-func rbytes(n int, r *rand.Rand) []byte {
-	rbs := make([]byte, n)
-	k := r.Intn(n)
-	if k%2 == 0 {
-		r.Read(rbs)
-	}
-	return rbs
-}
-
 type BbbDeneb struct {
 	types.BeaconBlockBody
 }
 
-func (b *BbbDeneb) Generate(r *rand.Rand, _ int) reflect.Value {
-	sizer := &ssz.Sizer{}
+type BodyTypes interface {
+	types.Eth1Data | types.Deposit | engineprimitives.Withdrawal |
+	[]byte | types.ExecutionPayload | eip4844.KZGCommitment
+}
 
-	b = &BbbDeneb{}
-	b.RandaoReveal = crypto.BLSSignature(rbytes(96, r))
-	b.Eth1Data = &types.Eth1Data{
-		DepositRoot:  common.Root(rbytes(32, r)),
-		DepositCount: math.U64(r.Uint64()),
-		BlockHash:    common.ExecutionHash(rbytes(32, r)),
+func orNil[T interface{ *E | []*E | []E }, E BodyTypes](
+	what T, r *rand.Rand,
+) T {
+	if r.Intn(2) == 0 {
+		return what
 	}
-	b.Graffiti = [32]byte(rbytes(32, r))
+	return nil
+}
 
-	k := roll(16, r)
+func (b *BbbDeneb) Generate(r *rand.Rand, _ int) reflect.Value {
+	roll := func(n int) int {
+		k := r.Intn(n)
+		if k%2 == 0 {
+			return 0
+		}
+		return k
+	}
+	rbytes := func(n int) []byte {
+		rbs := make([]byte, n)
+		r.Read(rbs)
+		return rbs
+	}
+
+	sizer := &ssz.Sizer{}
+	b = &BbbDeneb{}
+	b.RandaoReveal = crypto.BLSSignature(rbytes(96))
+	b.Eth1Data = orNil[*types.Eth1Data, types.Eth1Data](&types.Eth1Data{
+		DepositRoot:  common.Root(rbytes(32)),
+		DepositCount: math.U64(r.Uint64()),
+		BlockHash:    common.ExecutionHash(rbytes(32)),
+	}, r)
+	b.Graffiti = [32]byte(rbytes(32))
+
+	k := roll(16)
 	var depositsLen = uint32(k)
 	if k != 0 {
-		depositsLen = uint32(concurrencyThreshold)/(&types.Deposit{}).SizeSSZ(sizer) + 1
+		depositsLen = uint32(concurrencyThreshold)/(
+			&types.Deposit{}).SizeSSZ(sizer) + 1
 	}
 	b.Deposits = make([]*types.Deposit, depositsLen)
 	for i := range depositsLen {
 		b.Deposits[i] = &types.Deposit{
-			Pubkey:      crypto.BLSPubkey(rbytes(48, r)),
-			Credentials: types.WithdrawalCredentials(rbytes(32, r)),
+			Pubkey:      crypto.BLSPubkey(rbytes(48)),
+			Credentials: types.WithdrawalCredentials(rbytes(32)),
 			Amount:      math.Gwei(r.Uint64()),
-			Signature:   crypto.BLSSignature(rbytes(96, r)),
+			Signature:   crypto.BLSSignature(rbytes(96)),
 			Index:       r.Uint64(),
 		}
 	}
+	b.Deposits = orNil[[]*types.Deposit, types.Deposit](b.Deposits, r)
 
-	k = roll(10, r) // MaxTxsPerPayload 1048576 too big
+	k = roll(10) // MaxTxsPerPayload 1048576 too big
 	txs := make([][]byte, k)
 	for i := range k {
-		txs[i] = rbytes(1024, r) // MaxBytesPerTx 1073741824 too big
+		txs[i] = rbytes(1024) // MaxBytesPerTx 1073741824 too big
 	}
+	txs = orNil[[][]byte, []byte](txs, r)
 
-	k = roll(16, r)
+	k = roll(16)
 	withdrawals := make([]*engineprimitives.Withdrawal, k)
 	for i := range k {
 		withdrawals[i] = &engineprimitives.Withdrawal{
 			Index:     math.U64(r.Uint64()),
 			Validator: math.U64(r.Uint64()),
-			Address:   common.ExecutionAddress(rbytes(20, r)),
+			Address:   common.ExecutionAddress(rbytes(20)),
 			Amount:    math.U64(r.Uint64()),
 		}
 	}
+	withdrawals = orNil[
+		[]*engineprimitives.Withdrawal, engineprimitives.Withdrawal,
+	](withdrawals, r)
 
-	b.ExecutionPayload = &types.ExecutionPayload{
-		ParentHash:    common.ExecutionHash(rbytes(32, r)),
-		FeeRecipient:  common.ExecutionAddress(rbytes(20, r)),
-		StateRoot:     bytes.B32(rbytes(32, r)),
-		ReceiptsRoot:  bytes.B32(rbytes(32, r)),
-		LogsBloom:     bytes.B256(rbytes(256, r)),
-		Random:        common.Bytes32(rbytes(32, r)),
+	b.ExecutionPayload = orNil[
+		*types.ExecutionPayload, types.ExecutionPayload,
+	](&types.ExecutionPayload{
+		ParentHash:    common.ExecutionHash(rbytes(32)),
+		FeeRecipient:  common.ExecutionAddress(rbytes(20)),
+		StateRoot:     bytes.B32(rbytes(32)),
+		ReceiptsRoot:  bytes.B32(rbytes(32)),
+		LogsBloom:     bytes.B256(rbytes(256)),
+		Random:        common.Bytes32(rbytes(32)),
 		Number:        math.U64(r.Uint64()),
 		GasLimit:      math.U64(r.Uint64()),
 		GasUsed:       math.U64(r.Uint64()),
 		Timestamp:     math.U64(r.Uint64()),
-		ExtraData:     bytes.Bytes(rbytes(32, r)),
+		ExtraData:     bytes.Bytes(rbytes(32)),
 		BaseFeePerGas: math.NewU256(r.Uint64()),
-		BlockHash:     common.ExecutionHash(rbytes(32, r)),
+		BlockHash:     common.ExecutionHash(rbytes(32)),
 		Transactions:  txs,
 		Withdrawals:   withdrawals,
 		BlobGasUsed:   math.U64(r.Uint64()),
 		ExcessBlobGas: math.U64(r.Uint64()),
-	}
+	}, r)
 
-	k = roll(4096, r)
+	k = roll(4096)
 	b.BlobKzgCommitments = make([]eip4844.KZGCommitment, k)
 	for i := range k {
-		b.BlobKzgCommitments[i] = eip4844.KZGCommitment(rbytes(48, r))
+		b.BlobKzgCommitments[i] = eip4844.KZGCommitment(rbytes(48))
 	}
+	b.BlobKzgCommitments = orNil[
+		[]eip4844.KZGCommitment, eip4844.KZGCommitment,
+	](b.BlobKzgCommitments, r)
 
 	return reflect.ValueOf(b)
 }
@@ -139,21 +160,50 @@ func pprint(i interface{}) string {
 	return string(s)
 }
 
+func anyNil(a *BbbDeneb) bool {
+	var r bool
+	r = a == nil
+	r = r || a.Eth1Data == nil
+	r = r || a.Deposits == nil
+	r = r || a.ExecutionPayload == nil
+	r = r || a.ExecutionPayload.Transactions == nil
+	r = r || a.ExecutionPayload.Withdrawals == nil
+	r = r || a.BlobKzgCommitments == nil
+	return r
+}
+
 func compare(a, b *BbbDeneb) bool {
 	var r bool
 	r = a.RandaoReveal == b.RandaoReveal
-	r = r && *a.Eth1Data == *b.Eth1Data
+	r = r && (a.Eth1Data != nil && *a.Eth1Data == *b.Eth1Data) ||
+		(a.Eth1Data == nil && *b.Eth1Data == types.Eth1Data{})
 	r = r && a.Graffiti == b.Graffiti
-	for i, depA := range a.Deposits {
-		r = r && *depA == *b.Deposits[i]
+	if a.Deposits != nil {
+		for i, depA := range a.Deposits {
+			r = r && *depA == *b.Deposits[i]
+		}
+	} else {
+		r = r && len(b.Deposits) == 0
 	}
-	for i, wA := range a.ExecutionPayload.Withdrawals {
-		r = r && *wA == *b.ExecutionPayload.Withdrawals[i]
+	if a.ExecutionPayload != nil {
+		for i, txA := range a.ExecutionPayload.Transactions {
+			r = r && slices.Equal(
+				txA, b.ExecutionPayload.Transactions[i])
+		}
+		for i, wA := range a.ExecutionPayload.Withdrawals {
+			r = r && *wA == *b.ExecutionPayload.Withdrawals[i]
+		}
+	} else {
+		r = r && len(b.ExecutionPayload.Transactions) == 0
+		r = r && len(b.ExecutionPayload.Withdrawals) == 0
 	}
-	for i, cA := range a.BlobKzgCommitments {
-		r = r && cA == b.BlobKzgCommitments[i]
+	if a.BlobKzgCommitments != nil {
+		for i, cA := range a.BlobKzgCommitments {
+			r = r && cA == b.BlobKzgCommitments[i]
+		}
+	} else {
+		r = r && len(b.BlobKzgCommitments) == 0
 	}
-
 	return r
 }
 
@@ -170,16 +220,17 @@ func TestSSZRoundTripBeaconBodyDeneb(t *testing.T) {
 			t.Log("Deserialize: could not deserialize back the serialized body --", err)
 			return false
 		}
-
-		if !compare(body, destBody) {
-			t.Log("Deserialized body different than former body after serialization")
+		if anyNil(destBody) {
+			t.Log("Deserialize: nil found in deserialized body")
 			t.Log(pprint(body))
 			t.Log(pprint(destBody))
 			return false
 		}
 
-		if destBody.ExecutionPayload.GetWithdrawals() == nil {
-			t.Log("Withdrawals is nil after deserialization")
+		if !compare(body, destBody) {
+			t.Log("Deserialized body different than former body after serialization")
+			t.Log(pprint(body))
+			t.Log(pprint(destBody))
 			return false
 		}
 
