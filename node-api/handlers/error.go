464ddd108a3b4d96f943f2a4b83bc161f4e020b8
nidhi-singh02 <trippin@berachain.com>
2025-02-06 17:42:14 +0530

    linting fix
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>

--- node-api/handlers/error.go
+++ node-api/handlers/error.go
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2025, Berachain Foundation. All rights reserved.
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
 package handlers
 
 // HTTPError represents an HTTP error response.
@@ -6,7 +26,7 @@ type HTTPError struct {
 	Message string `json:"message"`
 }
 
-// NewHTTPError creates a new HTTPError
+// NewHTTPError creates a new HTTPError.
 func NewHTTPError(code int, message string) *HTTPError {
 	return &HTTPError{
 		Code:    code,
@@ -19,7 +39,7 @@ func (e *HTTPError) Error() string {
 	return e.Message
 }
 
-// StatusCode returns the HTTP status code
+// StatusCode returns the HTTP status code.
 func (e *HTTPError) StatusCode() int {
 	return e.Code
 }
