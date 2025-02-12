146286495cb330189f94745b43c4480f9b10c439
Fridrik Asmundsson <fridrik01@gmail.com>
2025-02-12 16:06:11 +0000

    We still want to run go mod tidy on CI

---
+++ scripts/build/devtools.mk
@@ -0,0 +1,16 @@
+#!/usr/bin/make -f
+
+# tidy-sync-check runs `go mod tidy` and checks if go.mod/go.sum files are in sync with
+# the dependencies in the codebase. If they are not in sync, it will exit with a
+# non-zero status code.
+tidy-sync-check:
+	@{ \
+	pre_tidy_diff=$$(git diff --ignore-space-change go.mod go.sum); \
+	go mod tidy; \
+	post_tidy_diff=$$(git diff --ignore-space-change go.mod go.sum); \
+	if [ "$$pre_tidy_diff" != "$$post_tidy_diff" ]; then \
+		echo "go.mod or go.sum changed after running 'go mod tidy'"; \
+		git diff go.mod go.sum; \
+		exit 1; \
+	fi; \
+	}
