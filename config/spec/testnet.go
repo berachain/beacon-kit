ebeab873bf6c35c905f55e7bb2b4704290b81f14
Cal Bera <calbera@berachain.com>
2025-02-20 12:59:36 -0800

    chore(testnet): Activate Deneb1 for EVM inflation

--- config/spec/testnet.go
+++ config/spec/testnet.go
@@ -26,18 +26,19 @@ import (
 )
 
 // TestnetChainSpecData is the chain.SpecData for Berachain's public testnet.
-//
-// TODO: adjust values before testnet genesis.
 func TestnetChainSpecData() *chain.SpecData {
 	specData := MainnetChainSpecData()
 	specData.DepositEth1ChainID = TestnetEth1ChainID
 
-	// Unlike mainnet, testnet activates Bera minting at block 1
-	specData.EVMInflationAddressDeneb1 = common.NewExecutionAddressFromHex(mainnetEVMInflationAddressDeneb1)
+	// Genesis values of inflation are unused since Deneb1 fork is activated at block 1 (epoch 0).
 	specData.EVMInflationAddressGenesis = common.NewExecutionAddressFromHex(mainnetEVMInflationAddressDeneb1)
-
-	specData.EVMInflationPerBlockDeneb1 = mainnetEVMInflationPerBlockDeneb1
 	specData.EVMInflationPerBlockGenesis = mainnetEVMInflationPerBlockDeneb1
+
+	// Unlike mainnet, testnet activates Deneb1 (Bera minting) at block 1 (epoch 0).
+	specData.Deneb1ForkEpoch = 0
+	specData.EVMInflationAddressDeneb1 = common.NewExecutionAddressFromHex(mainnetEVMInflationAddressDeneb1)
+	specData.EVMInflationPerBlockDeneb1 = mainnetEVMInflationPerBlockDeneb1
+
 	return specData
 }
 
