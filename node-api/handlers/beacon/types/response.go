d88b59c39ef9cfceccce5323392205090c8e8295
Cal Bera <calbera@berachain.com>
2025-03-04 09:51:31 -0800

    nit comment about GenesisResponse type

--- node-api/handlers/beacon/types/response.go
+++ node-api/handlers/beacon/types/response.go
@@ -78,6 +78,10 @@ type GenesisData struct {
 	GenesisForkVersion    string      `json:"genesis_fork_version"`
 }
 
+// GenesisResponse is handled with this explicit type since "finalized"
+// and "execution_optimistic" are not part of the return value.
+//
+// https://ethereum.github.io/beacon-APIs/#/Beacon/getGenesis
 type GenesisResponse struct {
 	Data GenesisData `json:"data"`
 }
