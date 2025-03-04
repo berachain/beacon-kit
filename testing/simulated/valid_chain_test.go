8b4d5dca5ddfc0ccb12c88fb2ae3261cc0535776
Rez <rez@berachain.com>
2025-03-04 13:28:31 +1100

    Move to test valid chain test

--- testing/simulated/valid_chain_test.go
+++ testing/simulated/valid_chain_test.go
@@ -23,10 +23,19 @@
 package simulated_test
 
 import (
+	"math/big"
+	"time"
+
 	"github.com/berachain/beacon-kit/beacon/blockchain"
+	ctypes "github.com/berachain/beacon-kit/consensus-types/types"
 	"github.com/berachain/beacon-kit/consensus/cometbft/service/encoding"
+	gethprimitives "github.com/berachain/beacon-kit/geth-primitives"
+	"github.com/berachain/beacon-kit/primitives/crypto"
 	mathpkg "github.com/berachain/beacon-kit/primitives/math"
 	"github.com/berachain/beacon-kit/testing/simulated"
+	"github.com/cometbft/cometbft/abci/types"
+	gethcommon "github.com/ethereum/go-ethereum/common"
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
 )
 
 // TestFullLifecycle_ValidBlock_IsSuccessful tests that a valid block proposal is processed, finalized, and committed.
@@ -68,4 +77,114 @@ func (s *SimulatedSuite) TestFullLifecycle_ValidBlock_IsSuccessful() {
 	)
 	s.Require().NoError(err)
 	s.Require().Equal(proposedBlock.Message.GetHeader().GetBodyRoot(), stateHeader.GetBodyRoot())
+}
+
+// TestCoreLoop_InjectedTransactions_IsSuccessful effectively serves as a demonstration for how one can
+// inject custom transactions and state transitions into the core loop.
+func (s *SimulatedSuite) TestCoreLoop_InjectedTransactions_IsSuccessful() {
+	const blockHeight = 1
+	const coreLoopIterations = 1
+
+	// Initialize the chain state.
+	s.initializeChain()
+
+	// Retrieve the BLS signer and proposer address.
+	blsSigner := simulated.GetBlsSigner(s.HomeDir)
+	pubkey, err := blsSigner.GetPubKey()
+	s.Require().NoError(err)
+
+	// Go through 1 iteration of the core loop to bypass any startup specific edge cases such as sync head on startup.
+	proposals := s.moveChainToHeight(blockHeight, coreLoopIterations, blsSigner)
+	s.Require().Len(proposals, coreLoopIterations)
+
+	// Prepare a valid block proposal.
+	proposal, err := s.SimComet.Comet.PrepareProposal(s.Ctx, &types.PrepareProposalRequest{
+		Height:          blockHeight + coreLoopIterations,
+		Time:            time.Now(),
+		ProposerAddress: pubkey.Address(),
+	})
+	s.Require().NoError(err)
+	s.Require().NotEmpty(proposal)
+
+	// Unmarshal the proposal block.
+	proposedBlock, err := encoding.UnmarshalBeaconBlockFromABCIRequest(
+		proposal.Txs,
+		blockchain.BeaconBlockTxIndex,
+		s.TestNode.ChainSpec.ActiveForkVersionForSlot(blockHeight+coreLoopIterations),
+	)
+	s.Require().NoError(err)
+
+	consensusTime := time.Unix(int64(proposedBlock.GetMessage().GetTimestamp()), 0)
+
+	// Sign a valid transaction that is expected to pass
+	recipientAddress := gethcommon.HexToAddress("0x56898d1aFb10cad584961eb96AcD476C6826e41E")
+	validTx, err := gethtypes.SignNewTx(
+		simulated.GetTestKey(s.T()),
+		gethtypes.NewCancunSigner(big.NewInt(int64(s.TestNode.ChainSpec.DepositEth1ChainID()))),
+		&gethtypes.DynamicFeeTx{
+			Nonce:     0,
+			To:        &recipientAddress,
+			Value:     big.NewInt(0),
+			Gas:       21016,
+			GasTipCap: big.NewInt(765625000),
+			GasFeeCap: big.NewInt(765625000),
+			Data:      []byte{},
+		},
+	)
+
+	validTxs := []*gethprimitives.Transaction{validTx}
+	// Create a new beacon block with the valid transaction.
+	// Note: The beacon block returned here has an incorrect beacon state root, which is fixed in `ComputeAndSetStateRoot`.
+	unsignedBlock := simulated.ComputeAndSetValidExecutionBlock(s.T(), proposedBlock.GetMessage(), s.SimulationClient, s.TestNode.ChainSpec, validTxs)
+
+	proposerAddress, err := crypto.GetAddressFromPubKey(blsSigner.PublicKey())
+	s.Require().NoError(err)
+
+	// Finalize the block by applying the state transition to update its state root.
+	queryCtx, err := s.SimComet.CreateQueryContext(blockHeight+coreLoopIterations-1, false)
+	s.Require().NoError(err)
+	finalBlock, err := simulated.ComputeAndSetStateRoot(queryCtx, consensusTime, proposerAddress, s.TestNode.StateProcessor, s.TestNode.StorageBackend, unsignedBlock)
+	s.Require().NoError(err)
+
+	newSignedBlock, err := ctypes.NewSignedBeaconBlock(
+		finalBlock,
+		&ctypes.ForkData{
+			CurrentVersion:        s.TestNode.ChainSpec.ActiveForkVersionForSlot(unsignedBlock.GetSlot()),
+			GenesisValidatorsRoot: s.GenesisValidatorsRoot,
+		},
+		s.TestNode.ChainSpec,
+		blsSigner,
+	)
+	s.Require().NoError(err)
+
+	newBlockBytes, err := newSignedBlock.MarshalSSZ()
+	s.Require().NoError(err)
+
+	// Replace the old block with the new block in the proposal.
+	proposal.Txs[0] = newBlockBytes
+
+	// Reset the log buffer to discard old logs we don't care about
+	s.LogBuffer.Reset()
+	// Process the proposal containing the valid block.
+	processResp, err := s.SimComet.Comet.ProcessProposal(s.Ctx, &types.ProcessProposalRequest{
+		Txs:             proposal.Txs,
+		Height:          blockHeight + coreLoopIterations,
+		ProposerAddress: pubkey.Address(),
+		Time:            consensusTime,
+	})
+	s.Require().NoError(err)
+	s.Require().Equal(types.PROCESS_PROPOSAL_STATUS_ACCEPT, processResp.Status)
+
+	// Finalize the block.
+	finalizeResp, err := s.SimComet.Comet.FinalizeBlock(s.Ctx, &types.FinalizeBlockRequest{
+		Txs:             proposal.Txs,
+		Height:          blockHeight + coreLoopIterations,
+		ProposerAddress: pubkey.Address(),
+	})
+	s.Require().NoError(err)
+	s.Require().NotEmpty(finalizeResp)
+
+	// Commit the block.
+	_, err = s.SimComet.Comet.Commit(s.Ctx, &types.CommitRequest{})
+	s.Require().NoError(err)
 }
