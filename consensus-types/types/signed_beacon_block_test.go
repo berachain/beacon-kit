31aab0b0f4147a4d27a8a5d222a71c22db071c24
Rez <rez@berachain.com>
2025-03-18 13:35:30 +1100

    fixes

--- consensus-types/types/signed_beacon_block_test.go
+++ consensus-types/types/signed_beacon_block_test.go
@@ -56,8 +56,9 @@ func generateFakeSignedBeaconBlock(t *testing.T, version common.Version) *types.
 	blk := generateValidBeaconBlock(t, version)
 	signature := crypto.BLSSignature{}
 	return &types.SignedBeaconBlock{
-		Message:   blk,
-		Signature: signature,
+		Versionable: types.NewVersionable(version),
+		Message:     blk,
+		Signature:   signature,
 	}
 }
 
@@ -183,21 +184,33 @@ func TestSignedBeaconBlock_SizeSSZ(t *testing.T) {
 
 func TestSignedBeaconBlock_EmptySerialization(t *testing.T) {
 	t.Parallel()
-	orig := &types.SignedBeaconBlock{}
-	data, err := orig.MarshalSSZ()
-	require.NoError(t, err)
-	require.NotNil(t, data)
+	runForAllSupportedVersions(t, func(t *testing.T, v common.Version) {
+		ver := types.NewVersionable(v)
+		orig := &types.SignedBeaconBlock{
+			Message: &types.BeaconBlock{
+				Versionable: ver,
+				Body: &types.BeaconBlockBody{
+					Versionable: ver,
+					ExecutionPayload: &types.ExecutionPayload{
+						Versionable: ver,
+					},
+				},
+			},
+		}
+		data, err := orig.MarshalSSZ()
+		require.NoError(t, err)
+		require.NotNil(t, data)
 
-	var unmarshalled *types.SignedBeaconBlock
-	unmarshalled, err = unmarshalled.NewFromSSZ(data)
-	require.NoError(t, err)
-	require.NotNil(t, unmarshalled.GetMessage())
-	require.NotNil(t, unmarshalled.GetSignature())
+		unmarshalled, err := types.NewSignedBeaconBlockFromSSZ(data, v)
+		require.NoError(t, err)
+		require.NotNil(t, unmarshalled.GetMessage())
+		require.NotNil(t, unmarshalled.GetSignature())
 
-	buf := make([]byte, ssz.Size(orig))
-	err = ssz.EncodeToBytes(buf, orig)
-	require.NoError(t, err)
+		buf := make([]byte, ssz.Size(orig))
+		err = ssz.EncodeToBytes(buf, orig)
+		require.NoError(t, err)
 
-	// The two byte slices should be equal
-	require.Equal(t, data, buf)
+		// The two byte slices should be equal
+		require.Equal(t, data, buf)
+	})
 }
