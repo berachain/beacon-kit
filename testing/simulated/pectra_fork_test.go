7b12440d0d0b7abdba476cc44232ef9b2dadfd2c
Rez <rez@berachain.com>
2025-04-03 09:23:01 +1100

    Working

--- testing/simulated/pectra_fork_test.go
+++ testing/simulated/pectra_fork_test.go
@@ -41,7 +41,8 @@ import (
 type PectraForkSuite struct {
 	suite.Suite
 	// Embedded shared accessors for convenience.
-	simulated.SharedAccessors
+	Geth simulated.SharedAccessors
+	Reth simulated.SharedAccessors
 }
 
 // TestSimulatedCometComponent runs the test suite.
@@ -52,12 +53,15 @@ func TestPectraForkSuite(t *testing.T) {
 // SetupTest initializes the test environment.
 func (s *PectraForkSuite) SetupTest() {
 	// Create a cancellable context for the duration of the test.
-	s.CtxApp, s.CtxAppCancelFn = context.WithCancel(context.Background())
+	s.Geth.CtxApp, s.Geth.CtxAppCancelFn = context.WithCancel(context.Background())
+	s.Reth.CtxApp, s.Reth.CtxAppCancelFn = context.WithCancel(context.Background())
 
 	// CometBFT uses context.TODO() for all ABCI calls, so we replicate that.
-	s.CtxComet = context.TODO()
+	s.Geth.CtxComet = context.TODO()
+	s.Geth.HomeDir = s.T().TempDir()
 
-	s.HomeDir = s.T().TempDir()
+	s.Reth.CtxComet = context.TODO()
+	s.Reth.HomeDir = s.T().TempDir()
 
 	// Initialize the home directory, Comet configuration, and genesis info.
 	const elGenesisPath = "./el-genesis-files/pectra-fork-genesis.json"
@@ -65,43 +69,70 @@ func (s *PectraForkSuite) SetupTest() {
 	// Create the chainSpec.
 	chainSpec, err := chainSpecFunc()
 	s.Require().NoError(err)
-	cometConfig, genesisValidatorsRoot := simulated.InitializeHomeDir(s.T(), chainSpec, s.HomeDir, elGenesisPath)
-	s.GenesisValidatorsRoot = genesisValidatorsRoot
+	cometConfig, genesisValidatorsRoot := simulated.InitializeHomeDir(s.T(), chainSpec, s.Geth.HomeDir, elGenesisPath)
+	s.Geth.GenesisValidatorsRoot = genesisValidatorsRoot
+	s.Reth.GenesisValidatorsRoot = genesisValidatorsRoot
+
+	// Copy the home dir for the Reth Node
+	simulated.CopyHomeDir(s.T(), s.Geth.HomeDir, s.Reth.HomeDir)
 
 	// Start the EL (execution layer) Geth node.
-	elNode := execution.NewRethNode(s.HomeDir, execution.ValidRethImage())
-	elHandle, authRPC := elNode.Start(s.T(), path.Base(elGenesisPath))
-	s.ElHandle = elHandle
+	gethNode := execution.NewGethNode(s.Geth.HomeDir, execution.ValidGethImage())
+	elHandle, authRPC := gethNode.Start(s.T(), path.Base(elGenesisPath))
+	s.Geth.ElHandle = elHandle
+
+	rethNode := execution.NewRethNode(s.Reth.HomeDir, execution.ValidRethImage())
+	rethHandle, rethAuthRPC := rethNode.Start(s.T(), path.Base(elGenesisPath))
+	s.Reth.ElHandle = rethHandle
 
 	// Prepare a logger backed by a buffer to capture logs for assertions.
-	s.LogBuffer = new(bytes.Buffer)
-	logger := phuslu.NewLogger(s.LogBuffer, nil)
+	s.Geth.LogBuffer = new(bytes.Buffer)
+	logger := phuslu.NewLogger(s.Geth.LogBuffer, nil)
+
+	s.Reth.LogBuffer = new(bytes.Buffer)
+	rethLogger := phuslu.NewLogger(s.Reth.LogBuffer, nil)
 
 	// Build the Beacon node with the simulated Comet component and electra genesis chain spec
 	components := simulated.FixedComponents(s.T())
 	components = append(components, simulated.ProvideSimComet)
 	components = append(components, chainSpecFunc)
 
-	s.TestNode = simulated.NewTestNode(s.T(), simulated.TestNodeInput{
-		TempHomeDir: s.HomeDir,
+	s.Geth.TestNode = simulated.NewTestNode(s.T(), simulated.TestNodeInput{
+		TempHomeDir: s.Geth.HomeDir,
 		CometConfig: cometConfig,
 		AuthRPC:     authRPC,
 		Logger:      logger,
 		AppOpts:     viper.New(),
 		Components:  components,
 	})
+	s.Geth.SimComet = s.Geth.TestNode.SimComet
 
-	s.SimComet = s.TestNode.SimComet
+	s.Reth.TestNode = simulated.NewTestNode(s.T(), simulated.TestNodeInput{
+		TempHomeDir: s.Reth.HomeDir,
+		CometConfig: cometConfig,
+		AuthRPC:     rethAuthRPC,
+		Logger:      rethLogger,
+		AppOpts:     viper.New(),
+		Components:  components,
+	})
+	s.Reth.SimComet = s.Reth.TestNode.SimComet
 
 	// Start the Beacon node in a separate goroutine.
 	go func() {
-		_ = s.TestNode.Start(s.CtxApp)
+		_ = s.Geth.TestNode.Start(s.Geth.CtxApp)
+	}()
+	// Start the Beacon node in a separate goroutine.
+	go func() {
+		_ = s.Reth.TestNode.Start(s.Reth.CtxApp)
 	}()
 
-	s.SimulationClient = execution.NewSimulationClient(s.TestNode.EngineClient)
+	s.Geth.SimulationClient = execution.NewSimulationClient(s.Geth.TestNode.EngineClient)
+	// Reth does not have a simulation API
 	timeOut := 10 * time.Second
 	interval := 50 * time.Millisecond
-	err = simulated.WaitTillServicesStarted(s.LogBuffer, timeOut, interval)
+	err = simulated.WaitTillServicesStarted(s.Geth.LogBuffer, timeOut, interval)
+	s.Require().NoError(err)
+	err = simulated.WaitTillServicesStarted(s.Reth.LogBuffer, timeOut, interval)
 	s.Require().NoError(err)
 }
 
@@ -109,24 +140,32 @@ func (s *PectraForkSuite) SetupTest() {
 func (s *PectraForkSuite) TearDownTest() {
 	// If the test has failed, log additional information.
 	if s.T().Failed() {
-		s.T().Log(s.LogBuffer.String())
+		s.T().Log("GETH CL LOGS:")
+		s.T().Log(s.Geth.LogBuffer.String())
+		s.T().Log("RETH CL LOGS:")
+		s.T().Log(s.Reth.LogBuffer.String())
 	}
-	if err := s.ElHandle.Close(); err != nil {
-		s.T().Error("Error closing EL handle:", err)
+	if err := s.Geth.ElHandle.Close(); err != nil {
+		s.T().Error("Error closing Geth EL handle:", err)
+	}
+	if err := s.Reth.ElHandle.Close(); err != nil {
+		s.T().Error("Error closing Reth EL handle:", err)
 	}
 	// mimics the behaviour of shutdown func
-	s.CtxAppCancelFn()
-	s.TestNode.ServiceRegistry.StopAll()
+	s.Geth.CtxAppCancelFn()
+	s.Geth.TestNode.ServiceRegistry.StopAll()
+	s.Reth.CtxAppCancelFn()
+	s.Reth.TestNode.ServiceRegistry.StopAll()
 }
 
 // TestTimestampFork_ELAndCLInSync_IsSuccessful tests that we can fork successfully if EL and CL have synced timestamps
 // The forks timestamp at Unix 0, as the genesis at Unix 0, Cancun is at 10 and Prague is at 20.
 func (s *PectraForkSuite) TestTimestampFork_ELAndCLInSync_IsSuccessful() {
 	// Initialize the chain state.
-	s.InitializeChain(s.T())
+	s.Geth.InitializeChain(s.T())
 
 	// Retrieve the BLS signer and proposer address.
-	blsSigner := simulated.GetBlsSigner(s.HomeDir)
+	blsSigner := simulated.GetBlsSigner(s.Geth.HomeDir)
 
 	// Prepare a block proposal.
 	pubkey, err := blsSigner.GetPubKey()
@@ -149,7 +188,7 @@ func (s *PectraForkSuite) TestTimestampFork_ELAndCLInSync_IsSuccessful() {
 		// We set the consensus time to currentHeight * 2 to mimick a functional 2s block time.
 		// This assumption is not always true but used in a valid test.
 		consensusTime := time.Unix(currentHeight*2, 0)
-		proposal, err := s.SimComet.Comet.PrepareProposal(s.CtxComet, &types.PrepareProposalRequest{
+		proposal, err := s.Geth.SimComet.Comet.PrepareProposal(s.Geth.CtxComet, &types.PrepareProposalRequest{
 			Height:          currentHeight,
 			Time:            consensusTime,
 			ProposerAddress: pubkey.Address(),
@@ -158,7 +197,7 @@ func (s *PectraForkSuite) TestTimestampFork_ELAndCLInSync_IsSuccessful() {
 		s.Require().Len(proposal.Txs, 2)
 
 		// Process the proposal.
-		processResp, err := s.SimComet.Comet.ProcessProposal(s.CtxComet, &types.ProcessProposalRequest{
+		processResp, err := s.Geth.SimComet.Comet.ProcessProposal(s.Geth.CtxComet, &types.ProcessProposalRequest{
 			Txs:             proposal.Txs,
 			Height:          currentHeight,
 			ProposerAddress: pubkey.Address(),
@@ -168,18 +207,18 @@ func (s *PectraForkSuite) TestTimestampFork_ELAndCLInSync_IsSuccessful() {
 		s.Require().Equal(types.PROCESS_PROPOSAL_STATUS_ACCEPT, processResp.Status)
 
 		// Finalize the block.
-		s.LogBuffer.Reset()
-		finalizeResp, err := s.SimComet.Comet.FinalizeBlock(s.CtxComet, &types.FinalizeBlockRequest{
+		s.Geth.LogBuffer.Reset()
+		finalizeResp, err := s.Geth.SimComet.Comet.FinalizeBlock(s.Geth.CtxComet, &types.FinalizeBlockRequest{
 			Txs:             proposal.Txs,
 			Height:          currentHeight,
 			ProposerAddress: pubkey.Address(),
 			Time:            consensusTime,
 		})
-		s.Require().Contains(s.LogBuffer.String(), expectedMessages[expectedMessagesIdx])
+		s.Require().Contains(s.Geth.LogBuffer.String(), expectedMessages[expectedMessagesIdx])
 		s.Require().NoError(err)
 		s.Require().NotEmpty(finalizeResp)
 		// Commit the block.
-		_, err = s.SimComet.Comet.Commit(s.CtxComet, &types.CommitRequest{})
+		_, err = s.Geth.SimComet.Comet.Commit(s.Geth.CtxComet, &types.CommitRequest{})
 		s.Require().NoError(err)
 		expectedMessagesIdx++
 	}
