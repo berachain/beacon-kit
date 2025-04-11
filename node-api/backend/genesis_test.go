18aba503a4eb036b1961b12f7a09de21bfe182c4
Cal Bera <calbera@berachain.com>
2025-04-11 10:43:17 -0700

    add one more unit test

--- node-api/backend/genesis_test.go
+++ node-api/backend/genesis_test.go
@@ -31,6 +31,7 @@ import (
 	"cosmossdk.io/log"
 	storetypes "cosmossdk.io/store/types"
 	"github.com/berachain/beacon-kit/config/spec"
+	"github.com/berachain/beacon-kit/consensus-types/types"
 	"github.com/berachain/beacon-kit/log/noop"
 	"github.com/berachain/beacon-kit/node-api/backend"
 	nodemetrics "github.com/berachain/beacon-kit/node-core/components/metrics"
@@ -123,9 +124,14 @@ func TestGetGenesisData(t *testing.T) {
 	b.AttachQueryBackend(tcs)
 
 	// Set genesis data.
-	b.SetGenesisData(
-		nil,
-		common.Root{0x1, 0x2, 0x3})
+	bbh := types.BeaconBlockHeader{
+		Slot:            0,
+		ProposerIndex:   0,
+		ParentBlockRoot: common.Root{0x1, 0x2, 0x3},
+		StateRoot:       common.Root{0x1, 0x2, 0x3},
+		BodyRoot:        common.Root{0x1, 0x2, 0x3},
+	}
+	b.SetGenesisData(&bbh, common.Root{0x1, 0x2, 0x3})
 
 	// Test all genesis data.
 	genesisTime := b.GenesisTime()
@@ -136,4 +142,7 @@ func TestGetGenesisData(t *testing.T) {
 
 	genesisValidatorsRoot := b.GenesisValidatorsRoot()
 	require.Equal(t, common.Root{0x1, 0x2, 0x3}, genesisValidatorsRoot)
+
+	genesisBlockHeader := b.GenesisBlockHeader()
+	require.Equal(t, bbh, *genesisBlockHeader)
 }
