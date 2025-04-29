4d1756d9c932f35d965515bb56f25d1999613ef9
Rez <rez@berachain.com>
2025-04-29 13:56:11 +1000

    Progressing on adding deposit

--- testing/simulated/pectra_withdrawal_test.go
+++ testing/simulated/pectra_withdrawal_test.go
@@ -25,31 +25,45 @@ package simulated_test
 import (
 	"bytes"
 	"context"
+	"math/big"
 	"path"
 	"testing"
 	"time"
 
+	depositcli "github.com/berachain/beacon-kit/cli/commands/deposit"
+	consensustypes "github.com/berachain/beacon-kit/consensus-types/types"
+	"github.com/berachain/beacon-kit/geth-primitives/deposit"
 	"github.com/berachain/beacon-kit/log/phuslu"
+	"github.com/berachain/beacon-kit/node-core/components/signer"
+	"github.com/berachain/beacon-kit/primitives/common"
+	"github.com/berachain/beacon-kit/primitives/math"
 	"github.com/berachain/beacon-kit/testing/simulated"
 	"github.com/berachain/beacon-kit/testing/simulated/execution"
+	"github.com/cometbft/cometbft/crypto/bls12381"
+	"github.com/cometbft/cometbft/types"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/suite"
 )
 
-// SimulatedSuite defines our test suite for the simulated Comet component.
-type SimulatedSuite struct {
+// testPkey corresponds to address 0x56898d1aFb10cad584961eb96AcD476C6826e41E which is prefunded in genesis
+const testPkey2 = "9b9bc88a144fff869ae2f4ea8e252f2494d9b52ea1008d0b3537dad27ab489d5"
+
+// PectraWithdrawalSuite defines our test suite for Pectra related work using simulated Comet component.
+type PectraWithdrawalSuite struct {
 	suite.Suite
 	// Embedded shared accessors for convenience.
 	simulated.SharedAccessors
 }
 
 // TestSimulatedCometComponent runs the test suite.
-func TestSimulatedCometComponent(t *testing.T) {
-	suite.Run(t, new(SimulatedSuite))
+func TestPectraWithdrawalSuite(t *testing.T) {
+	suite.Run(t, new(PectraWithdrawalSuite))
 }
 
 // SetupTest initializes the test environment.
-func (s *SimulatedSuite) SetupTest() {
+func (s *PectraWithdrawalSuite) SetupTest() {
 	// Create a cancellable context for the duration of the test.
 	s.CtxApp, s.CtxAppCancelFn = context.WithCancel(context.Background())
 
@@ -59,8 +73,8 @@ func (s *SimulatedSuite) SetupTest() {
 	s.HomeDir = s.T().TempDir()
 
 	// Initialize the home directory, Comet configuration, and genesis info.
-	const elGenesisPath = "./el-genesis-files/eth-genesis.json"
-	chainSpecFunc := simulated.ProvideSimulationChainSpec
+	const elGenesisPath = "./el-genesis-files/pectra-fork-genesis.json"
+	chainSpecFunc := simulated.ProvidePectraWithdrawalTestChainSpec
 	// Create the chainSpec.
 	chainSpec, err := chainSpecFunc()
 	s.Require().NoError(err)
@@ -76,10 +90,11 @@ func (s *SimulatedSuite) SetupTest() {
 	s.LogBuffer = new(bytes.Buffer)
 	logger := phuslu.NewLogger(s.LogBuffer, nil)
 
-	// Build the Beacon node with the simulated Comet component.
+	// Build the Beacon node with the simulated Comet component and electra genesis chain spec
 	components := simulated.FixedComponents(s.T())
 	components = append(components, simulated.ProvideSimComet)
 	components = append(components, chainSpecFunc)
+
 	s.TestNode = simulated.NewTestNode(s.T(), simulated.TestNodeInput{
 		TempHomeDir: s.HomeDir,
 		CometConfig: cometConfig,
@@ -105,7 +120,7 @@ func (s *SimulatedSuite) SetupTest() {
 }
 
 // TearDownTest cleans up the test environment.
-func (s *SimulatedSuite) TearDownTest() {
+func (s *PectraWithdrawalSuite) TearDownTest() {
 	// If the test has failed, log additional information.
 	if s.T().Failed() {
 		s.T().Log(s.LogBuffer.String())
@@ -116,4 +131,65 @@ func (s *SimulatedSuite) TearDownTest() {
 	// mimics the behaviour of shutdown func
 	s.CtxAppCancelFn()
 	s.TestNode.ServiceRegistry.StopAll()
+}
+
+// TestExcessValidatorBeforeFork_CorrectlyEvicted checks that if a validator deposits and hits the validator set cap
+// before the fork, it will correctly be evicted.
+func (s *PectraWithdrawalSuite) TestExcessValidatorBeforeFork_CorrectlyEvicted() {
+	const blockHeight = 1
+	const coreLoopIterations = 10
+
+	// Initialize the chain state.
+	s.InitializeChain(s.T())
+
+	// Retrieve the BLS signer and proposer address.
+	blsSigner := simulated.GetBlsSigner(s.HomeDir)
+
+	// Hard fork occurs at t=10, so we start at t=0
+	startTime := time.Unix(0, 0)
+
+	// Go through iterations of the core loop.
+	proposals, _, _ := s.MoveChainToHeight(s.T(), blockHeight, coreLoopIterations, blsSigner, startTime)
+	s.Require().Len(proposals, coreLoopIterations)
+
+	depositContractAddress := gethcommon.Address(s.TestNode.ChainSpec.DepositContractAddress())
+	depositClient, err := deposit.NewDepositContract(depositContractAddress, s.TestNode.ContractBackend)
+	s.Require().NoError(err)
+	depositCount, err := depositClient.DepositCount(&bind.CallOpts{
+		BlockNumber: big.NewInt(0),
+	})
+	s.Require().NoError(err)
+	s.Require().Equal(uint64(1), depositCount)
+
+	creds := consensustypes.NewCredentialsFromExecutionAddress(
+		common.NewExecutionAddressFromHex("0x56898d1aFb10cad584961eb96AcD476C6826e41E"),
+	)
+	newDepositor := &signer.BLSSigner{PrivValidator: types.NewMockPVWithKeyType(bls12381.KeyType)}
+	depositAmount := math.Gwei(500_000 * 1e9) // 500K Bera
+	depositMsg, blsSig, err := depositcli.CreateDepositMessage(
+		s.TestNode.ChainSpec,
+		newDepositor,
+		s.GenesisValidatorsRoot,
+		creds,
+		depositAmount,
+	)
+	s.Require().NoError(err)
+	err = depositcli.ValidateDeposit(
+		s.TestNode.ChainSpec,
+		depositMsg.Pubkey,
+		depositMsg.Credentials,
+		depositMsg.Amount,
+		s.GenesisValidatorsRoot,
+		blsSig,
+	)
+	s.Require().NoError(err)
+
+	//tx, err = depositClient.Deposit(&bind.TransactOpts{
+	//	From:     from,
+	//	Value:    value,
+	//	Signer:   signer,
+	//	Nonce:    new(big.Int).SetUint64(nonce + i),
+	//	GasLimit: 1000000,
+	//	Context:  s.Ctx(),
+	//}, pubkey, curVal.WithdrawalCredentials[:], signature[:], operator)
 }
