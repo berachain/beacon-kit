3f58f91f387725066aaef66b32a53a9956e3fee2
abear <abear@berachain.com>
2025-06-05 13:13:39 +0200

    added finalize block handler

---
+++ consensus/cometbft/service/state/the_finalized_state.go
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2025, Berachain Foundation. All rights reserved.
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
+package state
+
+import (
+	"context"
+	"errors"
+
+	servercmtlog "github.com/berachain/beacon-kit/consensus/cometbft/service/log"
+	"github.com/berachain/beacon-kit/log/phuslu"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+var ErrNilFinalizeBlockState = errors.New("finalizeBlockState is nil")
+
+// For now, just embed finalize state and make a proper interface for it.
+// We will take care later on to cache states from processProposals
+type FinalizedStateHandler struct {
+	manager            *Manager
+	logger             *phuslu.Logger
+	finalizeBlockState *State
+}
+
+func NewFinalizeStateHandler(manager *Manager, logger *phuslu.Logger) *FinalizedStateHandler {
+	return &FinalizedStateHandler{
+		manager: manager,
+		logger:  logger,
+	}
+}
+
+func (h *FinalizedStateHandler) NewState(ctx context.Context) *State {
+	var (
+		log    = servercmtlog.WrapSDKLogger(h.logger)
+		ms     = h.manager.GetCommitMultiStore().CacheMultiStore()
+		newCtx = sdk.NewContext(ms, false, log).WithContext(ctx)
+	)
+
+	return NewState(ms, newCtx)
+}
+
+func (h *FinalizedStateHandler) NewContext() (sdk.Context, error) {
+	if h.finalizeBlockState == nil {
+		return sdk.Context{}, ErrNilFinalizeBlockState
+	}
+	newCtx, _ := h.finalizeBlockState.Context().CacheContext()
+	return newCtx, nil
+}
+
+func (h *FinalizedStateHandler) GetFinalizeState() *State {
+	return h.finalizeBlockState
+}
+
+func (h *FinalizedStateHandler) ResetFinalizeState() {
+	h.finalizeBlockState = nil
+}
