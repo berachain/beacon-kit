8c8600d0503d1fb331ab465d6bfbc4d962a3a4e7
abear <abear@berachain.com>
2025-07-15 09:44:46 +0200

    added UTs for GetBlockHeaderByID

--- node-api/handlers/beacon/header_test.go
+++ node-api/handlers/beacon/header_test.go
@@ -46,7 +46,7 @@ import (
 func TestGetBlockHeaders(t *testing.T) {
 	t.Parallel()
 
-	// a test testHeader to build test cases on top of
+	// testHeaders to build test cases on top of
 	testParentHeader := &types.BeaconBlockHeader{
 		Slot:            math.Slot(10),
 		ProposerIndex:   math.ValidatorIndex(1234),
@@ -289,6 +289,198 @@ func TestGetBlockHeaders(t *testing.T) {
 
 			// test
 			res, err := h.GetBlockHeaders(c)
+
+			// finally do checks
+			tc.check(t, res, err)
+		})
+	}
+}
+
+func TestGetBlockHeaderByID(t *testing.T) {
+	t.Parallel()
+
+	// a test testHeader to build test cases on top of
+	testHeader := &types.BeaconBlockHeader{
+		Slot:            math.Slot(1234),
+		ProposerIndex:   math.ValidatorIndex(5678),
+		ParentBlockRoot: common.Root{'p', 'a', 'r', 'e', 'n', 't', 'b', 'l', 'o', 'c', 'k', 'r', 'o', 'o', 't'},
+		StateRoot:       common.Root{'d', 'u', 'm', 'm', 'y', 's', 't', 'a', 't', 'e', 'r', 'o', 'o', 't'},
+		BodyRoot:        common.Root{'d', 'u', 'm', 'm', 'y', 'r', 'o', 'o', 't'},
+	}
+	errTestHeaderNotFound := errors.New("test header not found error")
+
+	testCases := []struct {
+		name                string
+		inputs              func() beacontypes.GetBlockHeaderRequest
+		setMockExpectations func(*mocks.Backend)
+		check               func(t *testing.T, res any, err error)
+	}{
+		{
+			name: "GetBlockHeaderByID - success - id is slot",
+			inputs: func() beacontypes.GetBlockHeaderRequest {
+				return beacontypes.GetBlockHeaderRequest{
+					BlockIDRequest: handlertypes.BlockIDRequest{
+						BlockID: testHeader.Slot.Base10(),
+					},
+				}
+			},
+			setMockExpectations: func(b *mocks.Backend) {
+				b.EXPECT().BlockHeaderAtSlot(testHeader.Slot).Return(testHeader, nil)
+			},
+			check: func(t *testing.T, res any, err error) {
+				t.Helper()
+
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.IsType(t, beacontypes.GenericResponse{}, res)
+				gr, _ := res.(beacontypes.GenericResponse)
+				require.IsType(t, &beacontypes.BlockHeaderResponse{}, gr.Data)
+				data, _ := gr.Data.(*beacontypes.BlockHeaderResponse)
+
+				require.Equal(t, testHeader.BodyRoot, data.Root)
+				expectedHeader := &beacontypes.BeaconBlockHeader{
+					Slot:          testHeader.Slot.Base10(),
+					ProposerIndex: testHeader.ProposerIndex.Base10(),
+					ParentRoot:    testHeader.ParentBlockRoot.Hex(),
+					StateRoot:     testHeader.StateRoot.Hex(),
+					BodyRoot:      testHeader.BodyRoot.Hex(),
+				}
+				require.Equal(t, expectedHeader, data.Header.Message)
+			},
+		},
+		{
+			name: "GetBlockHeaderByID - failure - invalid slot",
+			inputs: func() beacontypes.GetBlockHeaderRequest {
+				return beacontypes.GetBlockHeaderRequest{
+					BlockIDRequest: handlertypes.BlockIDRequest{
+						BlockID: "zzzz",
+					},
+				}
+			},
+			setMockExpectations: func(*mocks.Backend) {
+				// nothing to set here, slot is invalid
+			},
+			check: func(t *testing.T, _ any, err error) {
+				t.Helper()
+				require.ErrorIs(t, err, handlertypes.ErrInvalidRequest)
+			},
+		},
+		{
+			name: "GetBlockHeaderByID - failure - unindexed slot",
+			inputs: func() beacontypes.GetBlockHeaderRequest {
+				return beacontypes.GetBlockHeaderRequest{
+					BlockIDRequest: handlertypes.BlockIDRequest{
+						BlockID: testHeader.Slot.Base10(),
+					},
+				}
+			},
+			setMockExpectations: func(b *mocks.Backend) {
+				b.EXPECT().BlockHeaderAtSlot(testHeader.Slot).Return(nil, errTestHeaderNotFound)
+			},
+			check: func(t *testing.T, _ any, err error) {
+				t.Helper()
+				require.ErrorIs(t, err, errTestHeaderNotFound)
+			},
+		},
+		{
+			name: "GetBlockHeaderByID - success - id is block root",
+			inputs: func() beacontypes.GetBlockHeaderRequest {
+				return beacontypes.GetBlockHeaderRequest{
+					BlockIDRequest: handlertypes.BlockIDRequest{
+						BlockID: testHeader.BodyRoot.Hex(),
+					},
+				}
+			},
+			setMockExpectations: func(b *mocks.Backend) {
+				b.EXPECT().GetSlotByBlockRoot(testHeader.BodyRoot).Return(testHeader.Slot, nil)
+				b.EXPECT().BlockHeaderAtSlot(testHeader.Slot).Return(testHeader, nil)
+			},
+			check: func(t *testing.T, res any, err error) {
+				t.Helper()
+
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.IsType(t, beacontypes.GenericResponse{}, res)
+				gr, _ := res.(beacontypes.GenericResponse)
+				require.IsType(t, &beacontypes.BlockHeaderResponse{}, gr.Data)
+				data, _ := gr.Data.(*beacontypes.BlockHeaderResponse)
+
+				require.Equal(t, testHeader.BodyRoot, data.Root)
+				expectedHeader := &beacontypes.BeaconBlockHeader{
+					Slot:          testHeader.Slot.Base10(),
+					ProposerIndex: testHeader.ProposerIndex.Base10(),
+					ParentRoot:    testHeader.ParentBlockRoot.Hex(),
+					StateRoot:     testHeader.StateRoot.Hex(),
+					BodyRoot:      testHeader.BodyRoot.Hex(),
+				}
+				require.Equal(t, expectedHeader, data.Header.Message)
+			},
+		},
+		{
+			name: "GetBlockHeaderByID - failure - invalid block root",
+			inputs: func() beacontypes.GetBlockHeaderRequest {
+				return beacontypes.GetBlockHeaderRequest{
+					BlockIDRequest: handlertypes.BlockIDRequest{
+						BlockID: "AN-INVALID-HEX",
+					},
+				}
+			},
+			setMockExpectations: func(*mocks.Backend) {
+				// nothing to set here, slot is invalid
+			},
+			check: func(t *testing.T, _ any, err error) {
+				t.Helper()
+				require.ErrorIs(t, err, handlertypes.ErrInvalidRequest)
+			},
+		},
+		{
+			name: "GetBlockHeaderByID - failure - unindexed block",
+			inputs: func() beacontypes.GetBlockHeaderRequest {
+				return beacontypes.GetBlockHeaderRequest{
+					BlockIDRequest: handlertypes.BlockIDRequest{
+						BlockID: testHeader.BodyRoot.Hex(),
+					},
+				}
+			},
+			setMockExpectations: func(b *mocks.Backend) {
+				// Assume parent header is not known
+				b.EXPECT().GetSlotByBlockRoot(testHeader.BodyRoot).Return(0, errTestHeaderNotFound)
+			},
+			check: func(t *testing.T, _ any, err error) {
+				t.Helper()
+				require.ErrorIs(t, err, errTestHeaderNotFound)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			// setup test
+			backend := mocks.NewBackend(t)
+			h := beacon.NewHandler(backend)
+			h.SetLogger(noop.NewLogger[log.Logger]())
+			e := echo.New()
+			e.Validator = &beaconecho.CustomValidator{
+				Validator: beaconecho.ConstructValidator(),
+			}
+
+			// create API inputs
+			input := tc.inputs()
+			inputBytes, err := json.Marshal(input) //nolint:musttag //  TODO:fix
+			require.NoError(t, err)
+			body := strings.NewReader(string(inputBytes))
+			req := httptest.NewRequest(http.MethodGet, "/", body)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON) // otherwise code=415, message=Unsupported Media Type
+			c := e.NewContext(req, httptest.NewRecorder())
+
+			// set expectations
+			tc.setMockExpectations(backend)
+
+			// test
+			res, err := h.GetBlockHeaderByID(c)
 
 			// finally do checks
 			tc.check(t, res, err)
