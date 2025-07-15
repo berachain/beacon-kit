e7b777b7d3e5bde0cf8f0437b61cd835e093fc07
abear <abear@berachain.com>
2025-07-15 09:19:37 +0200

    nit

--- node-api/handlers/beacon/types/request.go
+++ node-api/handlers/beacon/types/request.go
@@ -94,9 +94,6 @@ type GetRandaoRequest struct {
 type GetBlockHeadersRequest struct {
 	SlotRequest
 
-	// TODO: unlock use. Current implementation allow block headers retrieval
-	// only by their slot. Until we complete this API we enforce ParentRoot to
-	// be empty
 	ParentRoot string `query:"parent_root" validate:"omitempty,hex"`
 }
 
