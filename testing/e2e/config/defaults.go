39cb509cedf540e3d9b07fa5ef7404ff5ecb476b
Rez <rez@berachain.com>
2025-07-22 16:43:01 +1000

    Update defaults.go

--- testing/e2e/config/defaults.go
+++ testing/e2e/config/defaults.go
@@ -151,7 +151,7 @@ func defaultConsensusSettings() ConsensusSettings {
 			TimeoutPropose:   consensus.TimeoutPropose.String(),
 			TimeoutPrevote:   consensus.TimeoutPrevote.String(),
 			TimeoutPrecommit: consensus.TimeoutPrecommit.String(),
-
+			//nolint:staticcheck // setting to zero because it's deprecated
 			TimeoutCommit:       consensus.TimeoutCommit.String(),
 			MaxNumInboundPeers:  p2p.MaxNumInboundPeers,
 			MaxNumOutboundPeers: p2p.MaxNumOutboundPeers,
