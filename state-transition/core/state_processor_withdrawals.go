10273730d3c34a9887ab6088065868cf414782e0
Camembera <camembear@berachain.com>
2025-08-29 08:09:50 -0400

    Fix/spelling and loglevels (#2633)
    Co-authored-by: Grizzly Bera <grizzlybera@berachain.com>
    Co-authored-by: Alberto Benegiamo <abear@berachain.com>

--- state-transition/core/state_processor_withdrawals.go
+++ state-transition/core/state_processor_withdrawals.go
@@ -144,7 +144,7 @@ func (sp *StateProcessor) processWithdrawals(
 		return err
 	}
 
-	sp.logger.Info(
+	sp.logger.Debug(
 		"Processed withdrawals",
 		"num_withdrawals", numWithdrawals,
 		"evm_inflation", float64(payloadWithdrawals[0].GetAmount().Unwrap())/params.GWei,
