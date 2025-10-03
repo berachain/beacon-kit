e7dd27f2c7d26931dd75be53359ffa2c17ed6eaf
Fridrik Asmundsson <fridrik01@gmail.com>
2025-10-03 15:04:34 +0000

    Add integration test for blob_fetcher, refactor helpers shared by unit/integration tests

--- beacon/blockchain/blob_executor_test.go
+++ beacon/blockchain/blob_executor_test.go
@@ -18,85 +18,26 @@
 package blockchain
 
 import (
-	"context"
 	"testing"
 
 	"cosmossdk.io/log"
-	ctypes "github.com/berachain/beacon-kit/consensus-types/types"
+	"github.com/berachain/beacon-kit/beacon/blockchain/testhelpers"
 	"github.com/berachain/beacon-kit/da/blobreactor"
 	dastore "github.com/berachain/beacon-kit/da/store"
 	datypes "github.com/berachain/beacon-kit/da/types"
 	"github.com/berachain/beacon-kit/errors"
-	"github.com/berachain/beacon-kit/primitives/common"
-	"github.com/berachain/beacon-kit/primitives/eip4844"
 	"github.com/berachain/beacon-kit/primitives/math"
-	statedb "github.com/berachain/beacon-kit/state-transition/core/state"
-	"github.com/berachain/beacon-kit/storage/block"
-	"github.com/berachain/beacon-kit/storage/deposit"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
-
-type mockBlobProcessorForExecutor struct{ mock.Mock }
-
-func (m *mockBlobProcessorForExecutor) VerifySidecars(
-	ctx context.Context,
-	sidecars datypes.BlobSidecars,
-	blkHeader *ctypes.BeaconBlockHeader,
-	kzgCommitments eip4844.KZGCommitments[common.ExecutionHash],
-) error {
-	return m.Called(ctx, sidecars, blkHeader, kzgCommitments).Error(0)
-}
-
-func (m *mockBlobProcessorForExecutor) ProcessSidecars(avs *dastore.Store, sidecars datypes.BlobSidecars) error {
-	return m.Called(avs, sidecars).Error(0)
-}
-
-type mockBlobRequesterForExecutor struct{ mock.Mock }
-
-func (m *mockBlobRequesterForExecutor) RequestBlobs(
-	ctx context.Context,
-	slot math.Slot,
-	verifier func(datypes.BlobSidecars) error,
-) ([]*datypes.BlobSidecar, error) {
-	args := m.Called(ctx, slot, verifier)
-	if args.Get(0) != nil {
-		sidecars, ok := args.Get(0).([]*datypes.BlobSidecar)
-		if !ok {
-			return nil, args.Error(1)
-		}
-		if verifier != nil {
-			if err := verifier(sidecars); err != nil {
-				return nil, err // Verifier rejected
-			}
-		}
-		return sidecars, args.Error(1)
-	}
-	return nil, args.Error(1)
-}
-
-func (m *mockBlobRequesterForExecutor) SetHeadSlot(_ math.Slot) {}
-
-type mockStorageBackendForExecutor struct {
-	availStore *dastore.Store
-}
-
-func (m *mockStorageBackendForExecutor) AvailabilityStore() *dastore.Store { return m.availStore }
-func (m *mockStorageBackendForExecutor) StateFromContext(_ context.Context) *statedb.StateDB {
-	return nil
-}
-func (m *mockStorageBackendForExecutor) DepositStore() deposit.StoreManager { return nil }
-func (m *mockStorageBackendForExecutor) BlockStore() *block.KVStore[*ctypes.BeaconBlock] {
-	return nil
-}
 
 // Test when peer sends invalid blobs (verification should reject them)
 func TestBlobFetchExecutor_ByzantineBlobs_Rejected(t *testing.T) {
 	t.Parallel()
 	ctx := t.Context()
-	mockProcessor := &mockBlobProcessorForExecutor{}
-	mockRequester := &mockBlobRequesterForExecutor{}
-	mockStorage := &mockStorageBackendForExecutor{availStore: &dastore.Store{}}
+	mockProcessor := &testhelpers.SimpleBlobProcessor{}
+	mockRequester := &testhelpers.SimpleBlobRequester{}
+	mockStorage := testhelpers.NewSimpleStorageBackend(&dastore.Store{})
 
 	executor := &blobFetchExecutor{
 		blobProcessor:  mockProcessor,
@@ -125,9 +66,9 @@ func TestBlobFetchExecutor_ByzantineBlobs_Rejected(t *testing.T) {
 func TestBlobFetchExecutor_VerifierCalled(t *testing.T) {
 	t.Parallel()
 	ctx := t.Context()
-	mockProcessor := &mockBlobProcessorForExecutor{}
-	mockRequester := &mockBlobRequesterForExecutor{}
-	mockStorage := &mockStorageBackendForExecutor{availStore: &dastore.Store{}}
+	mockProcessor := &testhelpers.SimpleBlobProcessor{}
+	mockRequester := &testhelpers.SimpleBlobRequester{}
+	mockStorage := testhelpers.NewSimpleStorageBackend(&dastore.Store{})
 
 	executor := &blobFetchExecutor{
 		blobProcessor:  mockProcessor,
@@ -144,7 +85,7 @@ func TestBlobFetchExecutor_VerifierCalled(t *testing.T) {
 		Run(func(_ mock.Arguments) { verifierCalled = true }).
 		Return(nil)
 	mockRequester.On("RequestBlobs", ctx, math.Slot(100), mock.Anything).Return(validBlobs, nil)
-	mockProcessor.On("ProcessSidecars", mockStorage.availStore, mock.Anything).Return(nil)
+	mockProcessor.On("ProcessSidecars", mockStorage.AvailabilityStore(), mock.Anything).Return(nil)
 
 	err := executor.FetchBlobsAndVerify(ctx, request)
 	require.NoError(t, err)
@@ -155,9 +96,9 @@ func TestBlobFetchExecutor_VerifierCalled(t *testing.T) {
 func TestBlobFetchExecutor_AllPeersFailed(t *testing.T) {
 	t.Parallel()
 	ctx := t.Context()
-	mockProcessor := &mockBlobProcessorForExecutor{}
-	mockRequester := &mockBlobRequesterForExecutor{}
-	mockStorage := &mockStorageBackendForExecutor{availStore: &dastore.Store{}}
+	mockProcessor := &testhelpers.SimpleBlobProcessor{}
+	mockRequester := &testhelpers.SimpleBlobRequester{}
+	mockStorage := testhelpers.NewSimpleStorageBackend(&dastore.Store{})
 
 	executor := &blobFetchExecutor{
 		blobProcessor:  mockProcessor,
