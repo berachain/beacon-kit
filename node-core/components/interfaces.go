bdaf2a7b191e2cdfbe38e9076cbd709272e80538
Alberto Benegiamo <abear@berachain.com>
2025-10-29 14:55:14 +0100

    [3/n] fix payloadID cache clearing (#2964)
    Co-authored-by: Cal Bera <calbera@berachain.com>

--- node-core/components/interfaces.go
+++ node-core/components/interfaces.go
@@ -81,11 +81,13 @@ type (
 			ctx context.Context,
 			r *builder.RequestPayloadData,
 		) (*engineprimitives.PayloadID, common.Version, error)
-		// RetrievePayload retrieves the payload for the given slot.
+		// RetrievePayload retrieves the payload for the given slot and parentBlockRoot.
+		// If returned error is nil, payload is guaranteed to have expectedForkVersion version.
 		RetrievePayload(
 			ctx context.Context,
 			slot math.Slot,
 			parentBlockRoot common.Root,
+			expectedForkVersion common.Version,
 		) (ctypes.BuiltExecutionPayloadEnv, error)
 		// RequestPayloadSync requests a payload for the given slot and
 		// blocks until the payload is delivered.
