fe10e46bb91e0bf41f9e2b221b5587d5c5bc6d23
Cal Bera <calbera@berachain.com>
2026-02-21 01:57:12 +0530

    tx unmarshalling on client side

--- testing/e2e/suite/types/json_rpc_connection.go
+++ testing/e2e/suite/types/json_rpc_connection.go
@@ -21,9 +21,19 @@
 package types
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"math/big"
 
+	"github.com/berachain/beacon-kit/errors"
+	gethtypes "github.com/berachain/beacon-kit/geth-primitives/types"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	coretypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/services"
 )
 
@@ -61,4 +71,125 @@ func NewJSONRPCConnection(
 // IsWebSocket returns true if the connection is a WebSocket.
 func (c *JSONRPCConnection) IsWebSocket() bool {
 	return c.isWebSocket
+}
+
+func (c *JSONRPCConnection) BlockByNumber(ctx context.Context, number *big.Int) (*gethtypes.Block, error) {
+	return c.getBlock(ctx, "eth_getBlockByNumber", toBlockNumArg(number), true)
+}
+
+type rpcBlock struct {
+	Hash         *common.Hash            `json:"hash"`
+	Transactions []rpcTransaction        `json:"transactions"`
+	UncleHashes  []common.Hash           `json:"uncles"`
+	Withdrawals  []*coretypes.Withdrawal `json:"withdrawals,omitempty"`
+}
+
+type rpcTransaction struct {
+	tx *gethtypes.Transaction
+	txExtraInfo
+}
+
+type txExtraInfo struct {
+	BlockNumber *string         `json:"blockNumber,omitempty"`
+	BlockHash   *common.Hash    `json:"blockHash,omitempty"`
+	From        *common.Address `json:"from,omitempty"`
+}
+
+func (tx *rpcTransaction) UnmarshalJSON(msg []byte) error {
+	if err := json.Unmarshal(msg, &tx.tx); err != nil {
+		return err
+	}
+	return json.Unmarshal(msg, &tx.txExtraInfo)
+}
+
+func (c *JSONRPCConnection) getBlock(ctx context.Context, method string, args ...interface{}) (*gethtypes.Block, error) {
+	var raw json.RawMessage
+	err := c.Client.Client().CallContext(ctx, &raw, method, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decode header and transactions.
+	var head *gethtypes.Header
+	if err := json.Unmarshal(raw, &head); err != nil {
+		return nil, err
+	}
+	// When the block is not found, the API returns JSON null.
+	if head == nil {
+		return nil, ethereum.NotFound
+	}
+
+	var body rpcBlock
+	if err := json.Unmarshal(raw, &body); err != nil {
+		return nil, err
+	}
+	// Pending blocks don't return a block hash, compute it for sender caching.
+	if body.Hash == nil {
+		tmp := head.Hash()
+		body.Hash = &tmp
+	}
+
+	// Quick-verify transaction and uncle lists. This mostly helps with debugging the server.
+	if head.UncleHash == coretypes.EmptyUncleHash && len(body.UncleHashes) > 0 {
+		return nil, errors.New("server returned non-empty uncle list but block header indicates no uncles")
+	}
+	if head.UncleHash != coretypes.EmptyUncleHash && len(body.UncleHashes) == 0 {
+		return nil, errors.New("server returned empty uncle list but block header indicates uncles")
+	}
+	if head.TxHash == coretypes.EmptyTxsHash && len(body.Transactions) > 0 {
+		return nil, errors.New("server returned non-empty transaction list but block header indicates no transactions")
+	}
+	if head.TxHash != coretypes.EmptyTxsHash && len(body.Transactions) == 0 {
+		return nil, errors.New("server returned empty transaction list but block header indicates transactions")
+	}
+	// Load uncles because they are not included in the block response.
+	var uncles []*gethtypes.Header
+	if len(body.UncleHashes) > 0 {
+		uncles = make([]*gethtypes.Header, len(body.UncleHashes))
+		reqs := make([]rpc.BatchElem, len(body.UncleHashes))
+		for i := range reqs {
+			reqs[i] = rpc.BatchElem{
+				Method: "eth_getUncleByBlockHashAndIndex",
+				Args:   []interface{}{body.Hash, hexutil.EncodeUint64(uint64(i))},
+				Result: &uncles[i],
+			}
+		}
+		if err := c.Client.Client().BatchCallContext(ctx, reqs); err != nil {
+			return nil, err
+		}
+		for i := range reqs {
+			if reqs[i].Error != nil {
+				return nil, reqs[i].Error
+			}
+			if uncles[i] == nil {
+				return nil, fmt.Errorf("got null header for uncle %d of block %x", i, body.Hash[:])
+			}
+		}
+	}
+	txs := make([]*gethtypes.Transaction, len(body.Transactions))
+	for i, tx := range body.Transactions {
+		txs[i] = tx.tx
+	}
+
+	return gethtypes.NewBlockWithHeader(head).WithBody(
+		gethtypes.Body{
+			Transactions: txs,
+			Uncles:       uncles,
+			Withdrawals:  body.Withdrawals,
+		}), nil
+}
+
+func toBlockNumArg(number *big.Int) string {
+	if number == nil {
+		return "latest"
+	}
+	if number.Sign() >= 0 {
+		return hexutil.EncodeBig(number)
+	}
+	// It's negative.
+	if number.IsInt64() {
+		return rpc.BlockNumber(number.Int64()).String()
+	}
+	// It's negative and large, which is invalid.
+	return fmt.Sprintf("<invalid %d>", number)
 }
