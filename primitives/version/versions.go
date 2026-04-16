75c2286ee4573c3c970c60bf74eba5552df35bad
Cal Bera <calbera@berachain.com>
2026-04-16 16:45:40 +0530

    nit

--- primitives/version/versions.go
+++ primitives/version/versions.go
@@ -44,9 +44,7 @@ var (
 	electra = common.Version{0x05, 0x00, 0x00, 0x00}
 	// electra1 is the first hardfork of Electra on Berachain mainnet.
 	electra1 = common.Version{0x05, 0x01, 0x00, 0x00}
-	// TBD if used but kept as an example.
-	electra2 = common.Version{0x05, 0x02, 0x00, 0x00}
-	// fulu is the Fulu hardfork of Berachain mainnet (Fusaka CL fork).
+	// fulu is the first version of the Fulu hardfork on Berachain mainnet.
 	fulu = common.Version{0x06, 0x00, 0x00, 0x00}
 )
 
@@ -89,11 +87,6 @@ func Electra() common.Version {
 // Electra1 returns electra1 as a common.Version.
 func Electra1() common.Version {
 	return electra1
-}
-
-// Electra2 returns electra2 as a common.Version.
-func Electra2() common.Version {
-	return electra2
 }
 
 // Fulu returns fulu as a common.Version. Fulu is the CL component of the
